8bb5c50eb65785460b45828c90997de209b82223
Laszlo Fogas <laszlo@laszlo.cloud>
2019-11-13 11:25:21 +0100

    ARM is not widely used, making the project leaner

--- .drone.sh
+++ .drone.sh
@@ -13,5 +13,3 @@ echo "Building $VERSION"
 
 go build -ldflags '-extldflags "-static" -X github.com/laszlocph/woodpecker/version.Version='${VERSION} -o release/drone-server github.com/laszlocph/woodpecker/cmd/drone-server
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0         go build -ldflags '-X github.com/laszlocph/woodpecker/version.Version='${VERSION} -o release/drone-agent             github.com/laszlocph/woodpecker/cmd/drone-agent
-GOOS=linux GOARCH=arm64 CGO_ENABLED=0         go build -ldflags '-X github.com/laszlocph/woodpecker/version.Version='${VERSION} -o release/linux/arm64/drone-agent github.com/laszlocph/woodpecker/cmd/drone-agent
-GOOS=linux GOARCH=arm   CGO_ENABLED=0 GOARM=7 go build -ldflags '-X github.com/laszlocph/woodpecker/version.Version='${VERSION} -o release/linux/arm/drone-agent   github.com/laszlocph/woodpecker/cmd/drone-agent
