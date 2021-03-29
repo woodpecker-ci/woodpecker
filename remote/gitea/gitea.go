76d1a4a3aad4932a52f570d2eb45b32c29fe4392
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2021-03-29 14:17:11 +0200

    Company name fixed
    Company name fixed.
    
    Fixes: 38888383fd150bb2077e95bf11b038e10df2f4a6
    Author-Change-Id: IB#1107569

--- remote/gitea/gitea.go
+++ remote/gitea/gitea.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// This file has been modified by Informatyka Boguslawski sp. z o.o.
+// This file has been modified by Informatyka Boguslawski sp. z o.o. sp.k.
 
 package gitea
 
