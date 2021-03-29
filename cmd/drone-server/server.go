woodpecker-ci/woodpecker

commit 66d8ff7f9425a4e4ad1e49dc4dee322586002dfa

Reverse proxy authentication to Woodpeck and Gitea
This mod adds option to authenticate user using HTTP header set by
reverse proxy. It forwards specified HTTP header with authenticated
username in requests to Gitea.

Requirements:

* Gitea must be configured for reverse proxy authentication and
  must accept HTTP header auth in API calls (Gitea fix IB#1107572).

* Woodpecker must use its external host address in status URL-s
  sent to Gitea (Woodpecker fix IB#1107596).

To enable set the following variables in woodpecker server
environment (woodpecker running behind reverse proxy):

* internal woodpecker server URL, i.e.:

  WOODPECKER_HOST_INTERNAL=http://192.168.1.100:8000

* enable reverse proxy auth in woodpecker and forwarding auth
  header to gitea:

  WOODPECKER_GITEA_REV_PROXY_AUTH=true

* set name of header with authenticated username (set by
  reverse proxy), i.e.:

  WOODPECKER_GITEA_REV_PROXY_AUTH_HEADER=X-Forward-Username

Author-Change-Id: IB#1107569

cmd/drone-server/server.go
@@ -61,6 +61,11 @@ var flags = []cli.Flag{
 		Usage:  "server fully qualified url (<scheme>://<host>)",
 	},
 	cli.StringFlag{
+		EnvVar: "WOODPECKER_HOST_INTERNAL",
+		Name:   "server-host-internal",
+		Usage:  "server internal fully qualified url (<scheme>://<host>)",
+	},
+	cli.StringFlag{
 		EnvVar: "DRONE_SERVER_ADDR,WOODPECKER_SERVER_ADDR",
 		Name:   "server-addr",
 		Usage:  "server address",
@@ -513,6 +518,16 @@ var flags = []cli.Flag{
 		Name:   "keepalive-min-time",
 		Usage:  "server-side enforcement policy on the minimum amount of time a client should wait before sending a keepalive ping.",
 	},
+	cli.BoolFlag{
+		EnvVar: "WOODPECKER_GITEA_REV_PROXY_AUTH",
+		Name:   "gitea-rev-proxy-auth",
+		Usage:  "enable gitea authentication using HTTP header specified in WOODPECKER_GITEA_REV_PROXY_AUTH_HEADER",
+	},
+	cli.StringFlag{
+		EnvVar: "WOODPECKER_GITEA_REV_PROXY_AUTH_HEADER",
+		Name:   "gitea-rev-proxy-auth-header",
+		Usage:  "HTTP header with gitea authenticated user login",
+	},
 }
 
 func server(c *cli.Context) error {
@@ -538,6 +553,18 @@ func server(c *cli.Context) error {
 	if strings.HasSuffix(c.String("server-host"), "/") {
 		logrus.Fatalln(
 			"DRONE_HOST/DRONE_SERVER_HOST must not have trailing slash",
+		)
+	}
+
+	if c.String("server-host-internal") != "" && !strings.Contains(c.String("server-host-internal"), "://") {
+		logrus.Fatalln(
+			"WOODPECKER_HOST_INTERNAL must be <scheme>://<hostname> format",
+		)
+	}
+
+	if c.String("server-host-internal") != "" && strings.HasSuffix(c.String("server-host-internal"), "/") {
+		logrus.Fatalln(
+			"WOODPECKER_HOST_INTERNAL must not have trailing slash",
 		)
 	}
 
@@ -691,6 +718,7 @@ func setupEvilGlobals(c *cli.Context, v store.Store, r remote.Remote) {
 	droneserver.Config.Server.Key = c.String("server-key")
 	droneserver.Config.Server.Pass = c.String("agent-secret")
 	droneserver.Config.Server.Host = strings.TrimRight(c.String("server-host"), "/")
+	droneserver.Config.Server.HostInternal = strings.TrimRight(c.String("server-host-internal"), "/")
 	droneserver.Config.Server.Port = c.String("server-addr")
 	droneserver.Config.Server.RepoConfig = c.String("repo-config")
 	droneserver.Config.Server.SessionExpires = c.Duration("session-expires")

cmd/drone-server/setup.go
@@ -122,12 +122,14 @@ func setupGogs(c *cli.Context) (remote.Remote, error) {
 // helper function to setup the Gitea remote from the CLI arguments.
 func setupGitea(c *cli.Context) (remote.Remote, error) {
 	return gitea.New(gitea.Opts{
-		URL:         c.String("gitea-server"),
-		Context:     c.String("gitea-context"),
-		Username:    c.String("gitea-git-username"),
-		Password:    c.String("gitea-git-password"),
-		PrivateMode: c.Bool("gitea-private-mode"),
-		SkipVerify:  c.Bool("gitea-skip-verify"),
+		URL:                c.String("gitea-server"),
+		Context:            c.String("gitea-context"),
+		Username:           c.String("gitea-git-username"),
+		Password:           c.String("gitea-git-password"),
+		PrivateMode:        c.Bool("gitea-private-mode"),
+		SkipVerify:         c.Bool("gitea-skip-verify"),
+		RevProxyAuth:       c.Bool("gitea-rev-proxy-auth"),
+		RevProxyAuthHeader: c.String("gitea-rev-proxy-auth-header"),
 	})
 }
 

remote/gitea/gitea.go
@@ -28,22 +28,27 @@ import (
 
 // Opts defines configuration options.
 type Opts struct {
-	URL         string // Gitea server url.
-	Context     string // Context to display in status check
-	Username    string // Optional machine account username.
-	Password    string // Optional machine account password.
-	PrivateMode bool   // Gitea is running in private mode.
-	SkipVerify  bool   // Skip ssl verification.
+	URL                string // Gitea server url.
+	Context            string // Context to display in status check
+	Username           string // Optional machine account username.
+	Password           string // Optional machine account password.
+	PrivateMode        bool   // Gitea is running in private mode.
+	SkipVerify         bool   // Skip ssl verification.
+	RevProxyAuth       bool   // Enable reverse proxy authentication using RevProxyAuthHeader.
+	RevProxyAuthHeader string // Name of HTTP header with username for reverse proxy authentication.
 }
 
 type client struct {
-	URL         string
-	Context     string
-	Machine     string
-	Username    string
-	Password    string
-	PrivateMode bool
-	SkipVerify  bool
+	URL                     string
+	Context                 string
+	Machine                 string
+	Username                string
+	Password                string
+	PrivateMode             bool
+	SkipVerify              bool
+	RevProxyAuth            bool
+	RevProxyAuthHeader      string
+	RevProxyAuthHeaderValue string
 }
 
 const (
@@ -112,26 +117,36 @@ func New(opts Opts) (remote.Remote, error) {
 		url.Host = host
 	}
 	return &client{
-		URL:         opts.URL,
-		Context:     opts.Context,
-		Machine:     url.Host,
-		Username:    opts.Username,
-		Password:    opts.Password,
-		PrivateMode: opts.PrivateMode,
-		SkipVerify:  opts.SkipVerify,
+		URL:                opts.URL,
+		Context:            opts.Context,
+		Machine:            url.Host,
+		Username:           opts.Username,
+		Password:           opts.Password,
+		PrivateMode:        opts.PrivateMode,
+		SkipVerify:         opts.SkipVerify,
+		RevProxyAuth:       opts.RevProxyAuth,
+		RevProxyAuthHeader: opts.RevProxyAuthHeader,
 	}, nil
 }
 
 // Login authenticates an account with Gitea using basic authentication. The
 // Gitea account details are returned when the user is successfully authenticated.
 func (c *client) Login(res http.ResponseWriter, req *http.Request) (*model.User, error) {
-	var (
+
+	var username string
+	var password string
+
+	if c.RevProxyAuth {
+		username = req.Header.Get(c.RevProxyAuthHeader)
+		c.RevProxyAuthHeaderValue = username
+		password = ""
+	} else {
 		username = req.FormValue("username")
 		password = req.FormValue("password")
-	)
+	}
 
-	// if the username or password is empty we re-direct to the login screen.
-	if len(username) == 0 || len(password) == 0 {
+	// Redirect to the login screen if user credentials are incomplete.
+	if len(username) == 0 || (!c.RevProxyAuth && len(password) == 0) {
 		http.Redirect(res, req, "/login/form", http.StatusSeeOther)
 		return nil, nil
 	}
@@ -363,12 +378,37 @@ func (c *client) Hook(r *http.Request) (*model.Repo, *model.Build, error) {
 	return parseHook(r)
 }
 
+// authTransport forwards authentication HTTP header to gitea.
+type authTransport struct {
+	headerName          string
+	headerValue         string
+	underlyingTransport http.RoundTripper
+}
+
+func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add(t.headerName, t.headerValue)
+	if t.underlyingTransport != nil {
+		return t.underlyingTransport.RoundTrip(req)
+	} else {
+		return http.DefaultTransport.RoundTrip(req)
+	}
+}
+
 // helper function to return the Gitea client
 func (c *client) newClientToken(token string) (*gitea.Client, error) {
 	httpClient := &http.Client{}
 	if c.SkipVerify {
 		httpClient.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+	}
+	// Forward authentication header in every HTTP request to Gitea
+	// in reverse proxy authentication mode.
+	if c.RevProxyAuth {
+		httpClient.Transport = &authTransport{
+			headerName:          c.RevProxyAuthHeader,
+			headerValue:         c.RevProxyAuthHeaderValue,
+			underlyingTransport: httpClient.Transport,
 		}
 	}
 	return gitea.NewClient(c.URL, gitea.SetToken(token), gitea.SetHTTPClient(httpClient))

server/repo.go
@@ -73,9 +73,14 @@ func PostRepo(c *gin.Context) {
 		return
 	}
 
+	host := httputil.GetURL(c.Request)
+	if Config.Server.HostInternal != "" {
+		host = Config.Server.HostInternal
+	}
+
 	link := fmt.Sprintf(
 		"%s/hook?access_token=%s",
-		httputil.GetURL(c.Request),
+		host,
 		sig,
 	)
 
@@ -203,7 +208,12 @@ func DeleteRepo(c *gin.Context) {
 		}
 	}
 
-	remote.Deactivate(user, repo, httputil.GetURL(c.Request))
+	host := httputil.GetURL(c.Request)
+	if Config.Server.HostInternal != "" {
+		host = Config.Server.HostInternal
+	}
+
+	remote.Deactivate(user, repo, host)
 	c.JSON(200, repo)
 }
 
@@ -222,6 +232,10 @@ func RepairRepo(c *gin.Context) {
 
 	// reconstruct the link
 	host := httputil.GetURL(c.Request)
+	if Config.Server.HostInternal != "" {
+		host = Config.Server.HostInternal
+	}
+
 	link := fmt.Sprintf(
 		"%s/hook?access_token=%s",
 		host,
@@ -308,6 +322,10 @@ func MoveRepo(c *gin.Context) {
 
 	// reconstruct the link
 	host := httputil.GetURL(c.Request)
+	if Config.Server.HostInternal != "" {
+		host = Config.Server.HostInternal
+	}
+
 	link := fmt.Sprintf(
 		"%s/hook?access_token=%s",
 		host,

server/rpc.go
@@ -68,6 +68,7 @@ var Config = struct {
 		Key            string
 		Cert           string
 		Host           string
+		HostInternal   string
 		Port           string
 		Pass           string
 		RepoConfig     string

shared/token/token.go
@@ -53,12 +53,15 @@ func Parse(raw string, fn SecretFunc) (*Token, error) {
 func ParseRequest(r *http.Request, fn SecretFunc) (*Token, error) {
 	var token = r.Header.Get("Authorization")
 
-	// first we attempt to get the token from the
+	// First we attempt to get the token from the
 	// authorization header.
 	if len(token) != 0 {
 		token = r.Header.Get("Authorization")
 		fmt.Sscanf(token, "Bearer %s", &token)
-		return Parse(token, fn)
+
+		if parsedToken, err := Parse(token, fn); err == nil {
+			return parsedToken, nil
+		}
 	}
 
 	// then we attempt to get the token from the

commit 38888383fd150bb2077e95bf11b038e10df2f4a6

Missing information about file changes added
Missing information about file changes added.

Fixes: 66d8ff7f9425a4e4ad1e49dc4dee322586002dfa
Author-Change-Id: IB#1107569

cmd/drone-server/server.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package main
 

cmd/drone-server/setup.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package main
 

remote/gitea/gitea.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package gitea
 

server/repo.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package server
 

server/rpc.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package server
 

shared/token/token.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Drone.IO Inc.
+// Copyright 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified by Informatyka Boguslawski sp. z o.o.
 
 package token
 