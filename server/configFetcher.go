0f78921f88f7e08c149aee43afeb0e6a9e27598a
Anton Bracke <anton@ju60.de>
2021-08-30 18:46:25 +0200

    revert: undo wrong merge

--- server/configFetcher.go
+++ server/configFetcher.go
@@ -28,53 +28,35 @@ func (cf *configFetcher) Fetch() ([]*remote.FileMeta, error) {
 	for i := 0; i < 5; i++ {
 		select {
 		case <-time.After(time.Second * time.Duration(i)):
-			var fileerr error
-
 			// either a file
-			if !strings.HasSuffix(cf.repo.Config, "/") {
-				file, fileerr := cf.remote_.File(cf.user, cf.repo, cf.build, cf.repo.Config)
-				if fileerr == nil {
-					return []*remote.FileMeta{{
-						Name: cf.repo.Config,
-						Data: file,
-					}}, nil
-				}
+			file, fileerr := cf.remote_.File(cf.user, cf.repo, cf.build, cf.repo.Config)
+			if fileerr == nil {
+				return []*remote.FileMeta{{
+					Name: cf.repo.Config,
+					Data: file,
+				}}, nil
 			}
 
 			// or a folder
-			if strings.HasSuffix(cf.repo.Config, "/") {
-				files, fileerr := cf.remote_.Dir(cf.user, cf.repo, cf.build, strings.TrimSuffix(cf.repo.Config, "/"))
-				if fileerr == nil {
-					return filterPipelineFiles(files), nil
-				}
+			dir, direrr := cf.remote_.Dir(cf.user, cf.repo, cf.build, strings.TrimSuffix(cf.repo.Config, "/"))
+
+			if direrr == nil {
+				return dir, nil
+			} else if !cf.repo.Fallback {
+				return nil, direrr
 			}
 
 			// or fallback
-			if cf.repo.Fallback {
-				file, fileerr := cf.remote_.File(cf.user, cf.repo, cf.build, ".drone.yml")
-				if fileerr == nil {
-					return []*remote.FileMeta{{
-						Name: ".drone.yml",
-						Data: file,
-					}}, nil
-				}
+			file, fileerr = cf.remote_.File(cf.user, cf.repo, cf.build, ".drone.yml")
+			if fileerr != nil {
+				return nil, fileerr
 			}
 
-			return nil, fileerr
+			return []*remote.FileMeta{{
+				Name: cf.repo.Config,
+				Data: file,
+			}}, nil
 		}
 	}
-
 	return []*remote.FileMeta{}, nil
-}
-
-func filterPipelineFiles(files []*remote.FileMeta) []*remote.FileMeta {
-	var res []*remote.FileMeta
-
-	for _, file := range files {
-		if strings.HasSuffix(file.Name, ".yml") || strings.HasSuffix(file.Name, ".yaml") {
-			res = append(res, file)
-		}
-	}
-
-	return res
 }
