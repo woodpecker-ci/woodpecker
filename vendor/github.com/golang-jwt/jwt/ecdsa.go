ac46ee93455e90ac9639c657a386f1a95206b8dc
6543 <6543@obermui.de>
2021-09-21 12:55:25 +0200

    Migrate jwt token lib (#332)
    * migrate from github.com/dgrijalva/jwt-go to github.com/golang-jwt/jwt
    
    * migrate

--- vendor/github.com/golang-jwt/jwt/ecdsa.go
+++ vendor/github.com/golang-jwt/jwt/ecdsa.go
@@ -4,7 +4,6 @@ import (
 	"crypto"
 	"crypto/ecdsa"
 	"crypto/rand"
-	"encoding/asn1"
 	"errors"
 	"math/big"
 )
@@ -15,15 +14,12 @@ var (
 )
 
 // Implements the ECDSA family of signing methods signing methods
+// Expects *ecdsa.PrivateKey for signing and *ecdsa.PublicKey for verification
 type SigningMethodECDSA struct {
-	Name string
-	Hash crypto.Hash
-}
-
-// Marshalling structure for r, s EC point
-type ECPoint struct {
-	R *big.Int
-	S *big.Int
+	Name      string
+	Hash      crypto.Hash
+	KeySize   int
+	CurveBits int
 }
 
 // Specific instances for EC256 and company
@@ -35,19 +31,19 @@ var (
 
 func init() {
 	// ES256
-	SigningMethodES256 = &SigningMethodECDSA{"ES256", crypto.SHA256}
+	SigningMethodES256 = &SigningMethodECDSA{"ES256", crypto.SHA256, 32, 256}
 	RegisterSigningMethod(SigningMethodES256.Alg(), func() SigningMethod {
 		return SigningMethodES256
 	})
 
 	// ES384
-	SigningMethodES384 = &SigningMethodECDSA{"ES384", crypto.SHA384}
+	SigningMethodES384 = &SigningMethodECDSA{"ES384", crypto.SHA384, 48, 384}
 	RegisterSigningMethod(SigningMethodES384.Alg(), func() SigningMethod {
 		return SigningMethodES384
 	})
 
 	// ES512
-	SigningMethodES512 = &SigningMethodECDSA{"ES512", crypto.SHA512}
+	SigningMethodES512 = &SigningMethodECDSA{"ES512", crypto.SHA512, 66, 521}
 	RegisterSigningMethod(SigningMethodES512.Alg(), func() SigningMethod {
 		return SigningMethodES512
 	})
@@ -74,14 +70,15 @@ func (m *SigningMethodECDSA) Verify(signingString, signature string, key interfa
 	case *ecdsa.PublicKey:
 		ecdsaKey = k
 	default:
-		return ErrInvalidKey
+		return ErrInvalidKeyType
 	}
 
-	// Unmarshal asn1 ECPoint
-	var ecpoint = new(ECPoint)
-	if _, err := asn1.Unmarshal(sig, ecpoint); err != nil {
-		return err
+	if len(sig) != 2*m.KeySize {
+		return ErrECDSAVerification
 	}
+
+	r := big.NewInt(0).SetBytes(sig[:m.KeySize])
+	s := big.NewInt(0).SetBytes(sig[m.KeySize:])
 
 	// Create hasher
 	if !m.Hash.Available() {
@@ -91,11 +88,11 @@ func (m *SigningMethodECDSA) Verify(signingString, signature string, key interfa
 	hasher.Write([]byte(signingString))
 
 	// Verify the signature
-	if verifystatus := ecdsa.Verify(ecdsaKey, hasher.Sum(nil), ecpoint.R, ecpoint.S); verifystatus == true {
+	if verifystatus := ecdsa.Verify(ecdsaKey, hasher.Sum(nil), r, s); verifystatus {
 		return nil
-	} else {
-		return ErrECDSAVerification
 	}
+
+	return ErrECDSAVerification
 }
 
 // Implements the Sign method from SigningMethod
@@ -107,7 +104,7 @@ func (m *SigningMethodECDSA) Sign(signingString string, key interface{}) (string
 	case *ecdsa.PrivateKey:
 		ecdsaKey = k
 	default:
-		return "", ErrInvalidKey
+		return "", ErrInvalidKeyType
 	}
 
 	// Create the hasher
@@ -120,16 +117,25 @@ func (m *SigningMethodECDSA) Sign(signingString string, key interface{}) (string
 
 	// Sign the string and return r, s
 	if r, s, err := ecdsa.Sign(rand.Reader, ecdsaKey, hasher.Sum(nil)); err == nil {
-		// asn1 marhsal r, s using ecPoint as the structure
-		var ecpoint = new(ECPoint)
-		ecpoint.R = r
-		ecpoint.S = s
+		curveBits := ecdsaKey.Curve.Params().BitSize
 
-		if signature, err := asn1.Marshal(*ecpoint); err != nil {
-			return "", err
-		} else {
-			return EncodeSegment(signature), nil
+		if m.CurveBits != curveBits {
+			return "", ErrInvalidKey
 		}
+
+		keyBytes := curveBits / 8
+		if curveBits%8 > 0 {
+			keyBytes += 1
+		}
+
+		// We serialize the outputs (r and s) into big-endian byte arrays
+		// padded with zeros on the left to make sure the sizes work out.
+		// Output must be 2*keyBytes long.
+		out := make([]byte, 2*keyBytes)
+		r.FillBytes(out[0:keyBytes]) // r is assigned to the first half of output.
+		s.FillBytes(out[keyBytes:])  // s is assigned to the second half of output.
+
+		return EncodeSegment(out), nil
 	} else {
 		return "", err
 	}
