b34107181eefd6ac2fd130f02007e5fe3a9913a3
6543 <6543@obermui.de>
2021-09-24 15:54:04 +0200

    revert unrelated change

--- server/shared/configFetcher.go
+++ server/shared/configFetcher.go
@@ -26,7 +26,6 @@ func NewConfigFetcher(remote remote.Remote, user *model.User, repo *model.Repo, 
 
 func (cf *configFetcher) Fetch() (files []*remote.FileMeta, err error) {
 	var file []byte
-	cf.repo.Config = strings.TrimSpace(cf.repo.Config)
 
 	for i := 0; i < 5; i++ {
 		select {
@@ -44,9 +43,11 @@ func (cf *configFetcher) Fetch() (files []*remote.FileMeta, err error) {
 				}
 
 				// or a folder
-				files, err = cf.remote_.Dir(cf.user, cf.repo, cf.build, strings.TrimSuffix(cf.repo.Config, "/"))
-				if err == nil {
-					return filterPipelineFiles(files), nil
+				if strings.HasSuffix(cf.repo.Config, "/") {
+					files, err = cf.remote_.Dir(cf.user, cf.repo, cf.build, strings.TrimSuffix(cf.repo.Config, "/"))
+					if err == nil {
+						return filterPipelineFiles(files), nil
+					}
 				}
 			} else {
 				// no user defined config so try .woodpecker/*.yml -> .woodpecker.yml -> .drone.yml
