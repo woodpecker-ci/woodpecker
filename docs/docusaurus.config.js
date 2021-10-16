257e08ce7fd9ca795eababe02619bd7157d985d5
Anton Bracke <anton@ju60.de>
2021-10-16 18:25:02 +0200

    improve docusaurus layout

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,5 +1,6 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const path = require("path");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
