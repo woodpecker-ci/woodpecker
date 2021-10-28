91d37be1da48ec654f3973fabfc5259442c433bf
6543 <6543@obermui.de>
2021-10-28 12:11:52 +0200

    Update Dependencies (#486)
    * github.com/Microsoft/go-winio
    
    * github.com/bradrydzewski/togo
    
    * github.com/containerd/containerd
    
    * github.com/docker/cli
    
    * github.com/docker/docker
    
    * github.com/docker/docker-credential-helpers
    
    * github.com/franela/goblin
    
    * github.com/google/go-github/v39
    
    * github.com/joho/godotenv
    
    * github.com/lib/pq
    
    * github.com/moby/moby
    
    * github.com/prometheus/client_golang
    
    * github.com/tevino/abool
    
    * github.com/woodpecker-ci/togo
    
    * github.com/xanzy/go-gitlab
    
    * github.com/xeipuuv/gojsonschema
    
    * github.com/mattn/go-sqlite3

--- vendor/github.com/franela/goblin/goblin.go
+++ vendor/github.com/franela/goblin/goblin.go
@@ -83,12 +83,12 @@ func (d *Describe) runJustBeforeEach() {
 
 func (d *Describe) runAfterEach() {
 
-	if d.parent != nil {
-		d.parent.runAfterEach()
-	}
-
 	for _, a := range d.afterEach {
 		a()
+	}
+
+	if d.parent != nil {
+		d.parent.runAfterEach()
 	}
 }
 
@@ -124,12 +124,13 @@ type Failure struct {
 }
 
 type It struct {
-	h        interface{}
-	name     string
-	parent   *Describe
-	failure  *Failure
-	reporter Reporter
-	isAsync  bool
+	h         interface{}
+	name      string
+	parent    *Describe
+	failure   *Failure
+	failureMu sync.RWMutex
+	reporter  Reporter
+	isAsync   bool
 }
 
 func (it *It) run(g *G) bool {
@@ -139,19 +140,15 @@ func (it *It) run(g *G) bool {
 		g.reporter.ItIsPending(it.name)
 		return false
 	}
-	//TODO: should handle errors for beforeEach
-	it.parent.runBeforeEach()
 
-	it.parent.runJustBeforeEach()
-
-	runIt(g, it.h)
-
-	it.parent.runAfterEach()
+	runIt(g, it)
 
 	failed := false
+	it.failureMu.RLock()
 	if it.failure != nil {
 		failed = true
 	}
+	it.failureMu.RUnlock()
 
 	if failed {
 		g.reporter.ItFailed(it.name)
@@ -163,6 +160,8 @@ func (it *It) run(g *G) bool {
 }
 
 func (it *It) failed(msg string, stack []string) {
+	it.failureMu.Lock()
+	defer it.failureMu.Unlock()
 	it.failure = &Failure{Stack: stack, Message: msg, TestName: it.parent.name + " " + it.name}
 }
 
@@ -196,13 +195,17 @@ func parseFlags() {
 	}
 }
 
+var doParseOnce sync.Once
 var timeout = flag.Duration("goblin.timeout", 5*time.Second, "Sets default timeouts for all tests")
 var isTty = flag.Bool("goblin.tty", true, "Sets the default output format (color / monochrome)")
 var regexParam = flag.String("goblin.run", "", "Runs only tests which match the supplied regex")
 var runRegex *regexp.Regexp
 
 func Goblin(t *testing.T, arguments ...string) *G {
-	parseFlags()
+	doParseOnce.Do(func() {
+		parseFlags()
+	})
+
 	g := &G{t: t, timeout: *timeout}
 	var fancy TextFancier
 	if *isTty {
@@ -215,29 +218,39 @@ func Goblin(t *testing.T, arguments ...string) *G {
 	return g
 }
 
-func runIt(g *G, h interface{}) {
-	defer timeTrack(time.Now(), g)
+func runIt(g *G, it *It) {
 	g.mutex.Lock()
 	g.timedOut = false
 	g.mutex.Unlock()
 	g.timer = time.NewTimer(g.timeout)
 	g.shouldContinue = make(chan bool)
-	if call, ok := h.(func()); ok {
+	if call, ok := it.h.(func()); ok {
 		// the test is synchronous
-		go func(c chan bool) { call(); c <- true }(g.shouldContinue)
-	} else if call, ok := h.(func(Done)); ok {
+		go func(c chan bool) {
+			it.parent.runBeforeEach()
+			it.parent.runJustBeforeEach()
+			timeTrack(g, func() { call() })
+			it.parent.runAfterEach()
+			c <- true
+		}(g.shouldContinue)
+	} else if call, ok := it.h.(func(Done)); ok {
 		doneCalled := 0
 		go func(c chan bool) {
-			call(func(msg ...interface{}) {
-				if len(msg) > 0 {
-					g.Fail(msg)
-				} else {
-					doneCalled++
-					if doneCalled > 1 {
-						g.Fail("Done called multiple times")
+			it.parent.runBeforeEach()
+			it.parent.runJustBeforeEach()
+			timeTrack(g, func() {
+				call(func(msg ...interface{}) {
+					if len(msg) > 0 {
+						g.Fail(msg)
+					} else {
+						doneCalled++
+						if doneCalled > 1 {
+							g.Fail("Done called multiple times")
+						}
+						it.parent.runAfterEach()
+						c <- true
 					}
-					c <- true
-				}
+				})
 			})
 		}(g.shouldContinue)
 	} else {
@@ -334,25 +347,57 @@ func (g *G) Assert(src interface{}) *Assertion {
 	return &Assertion{src: src, fail: g.Fail}
 }
 
-func timeTrack(start time.Time, g *G) {
-	g.reporter.ItTook(time.Since(start))
+func timeTrack(g *G, call func()) {
+	t := time.Now()
+	defer func() {
+		g.reporter.ItTook(time.Since(t))
+	}()
+	call()
 }
 
-func (g *G) Fail(error interface{}) {
-	//Skips 7 stacks due to the functions between the stack and the test
-	stack := ResolveStack(7)
-	message := fmt.Sprintf("%v", error)
+func (g *G) errorCommon(msg string, fatal bool) {
 	if g.currentIt == nil {
 		panic("Asserts should be written inside an It() block.")
 	}
-	g.currentIt.failed(message, stack)
+	g.currentIt.failed(msg, ResolveStack(9))
 	if g.shouldContinue != nil {
 		g.shouldContinue <- true
 	}
-	g.mutex.Lock()
-	defer g.mutex.Unlock()
-	if !g.timedOut {
-		//Stop test function execution
-		runtime.Goexit()
+
+	if fatal {
+		g.mutex.Lock()
+		defer g.mutex.Unlock()
+		if !g.timedOut {
+			//Stop test function execution
+			runtime.Goexit()
+		}
 	}
+}
+
+func (g *G) Fail(error interface{}) {
+	message := fmt.Sprintf("%v", error)
+	g.errorCommon(message, true)
+}
+
+func (g *G) FailNow() {
+	g.t.FailNow()
+}
+
+func (g *G) Failf(format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
+	g.errorCommon(message, true)
+}
+
+func (g *G) Fatalf(format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
+	g.errorCommon(message, true)
+}
+
+func (g *G) Errorf(format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
+	g.errorCommon(message, false)
+}
+
+func (g *G) Helper() {
+	g.t.Helper()
 }
