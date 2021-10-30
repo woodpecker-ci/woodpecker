3f8a8cc38e2e86102b2a959bb2127824f13e2214
Anton Bracke <anton@ju60.de>
2021-10-30 16:01:10 +0200

    change image for testing

--- pipeline/frontend/yaml/compiler/compiler.go
+++ pipeline/frontend/yaml/compiler/compiler.go
@@ -110,18 +110,19 @@ func (c *Compiler) Compile(conf *yaml.Config) *backend.Config {
 	// add default clone step
 	if !c.local && len(conf.Clone.Containers) == 0 && !conf.SkipClone {
 		container := &yaml.Container{
-			Name:        "clone",
-			Image:       "woodpeckerci/plugin-git:latest",
+			Name: "clone",
+			// Image:       "woodpeckerci/plugin-git:latest",
+			Image:       "woodpeckerci/plugin-git:next",
 			Vargs:       map[string]interface{}{"depth": "0"},
 			Environment: c.cloneEnv,
 		}
-		// TODO: migrate to woodpeckerci/plugin-git:latest (multi arch)
-		switch c.metadata.Sys.Arch {
-		case "linux/arm":
-			container.Image = "plugins/git:linux-arm"
-		case "linux/arm64":
-			container.Image = "plugins/git:linux-arm64"
-		}
+		// // TODO: migrate to woodpeckerci/plugin-git:latest (multi arch)
+		// switch c.metadata.Sys.Arch {
+		// case "linux/arm":
+		// 	container.Image = "plugins/git:linux-arm"
+		// case "linux/arm64":
+		// 	container.Image = "plugins/git:linux-arm64"
+		// }
 		name := fmt.Sprintf("%s_clone", c.prefix)
 		step := c.createProcess(name, container, "clone")
 
