d36fe01ca6a559952f6b7f8b359a0cc31e306778
6543 <6543@obermui.de>
2021-11-04 02:20:10 +0100

    todo

---
+++ server/model/agent.go
@@ -0,0 +1,18 @@
+package model
+
+type Agent struct{}
+
+// TODO: check if it is actauly used or just some relict from the past
+
+/*
+CREATE TABLE IF NOT EXISTS agents (
+ agent_id       INTEGER PRIMARY KEY AUTO_INCREMENT
+,agent_addr     VARCHAR(250)
+,agent_platform VARCHAR(500)
+,agent_capacity INTEGER
+,agent_created  INTEGER
+,agent_updated  INTEGER
+
+,UNIQUE(agent_addr)
+);
+*/
