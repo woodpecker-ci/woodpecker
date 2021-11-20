4847c77186ae79f8fbc2a041f3b2ad2eacc28399
6543 <6543@obermui.de>
2021-11-20 22:10:13 +0100

    finish review

--- pipeline/frontend/yaml/constraint.go
+++ pipeline/frontend/yaml/constraint.go
@@ -164,8 +164,8 @@ func (c *ConstraintMap) UnmarshalYAML(unmarshal func(interface{}) error) error {
 
 	out2 := map[string]string{}
 
-	_ = unmarshal(&out1)
-	_ = unmarshal(&out2)
+	_ = unmarshal(&out1) // it contains include and exclude statement
+	_ = unmarshal(&out2) // it contains no include/exclude statement, assume include as default
 
 	c.Include = out1.Include
 	c.Exclude = out1.Exclude
