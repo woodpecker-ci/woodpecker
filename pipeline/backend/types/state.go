b4b4d21885a8c273e7764ac7cf814f8d5378c01f
6543 <6543@obermui.de>
2021-11-26 03:09:58 +0100

    rm

--- pipeline/backend/types/state.go
+++ pipeline/backend/types/state.go
@@ -9,22 +9,3 @@ type State struct {
 	// Container is oom killed, true or false
 	OOMKilled bool `json:"oom_killed"`
 }
-
-// // State defines the pipeline and process state.
-// type State struct {
-// 	Pipeline struct {
-// 		// Current pipeline step
-// 		Step *Step `json:"step"`
-// 		// Current pipeline error state
-// 		Error error `json:"error"`
-// 	}
-//
-// 	Process struct {
-// 		// Container exit code
-// 		ExitCode int `json:"exit_code"`
-// 		// Container exited, true or false
-// 		Exited bool `json:"exited"`
-// 		// Container is oom killed, true or false
-// 		OOMKilled bool `json:"oom_killed"`
-// 	}
-// }
