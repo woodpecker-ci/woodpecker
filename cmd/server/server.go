244fd707d9d66cb83febfcbec61c66fc91a35100
Anton Bracke <anton@ju60.de>
2021-12-09 22:53:05 +0100

    apply suggestion

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -287,7 +287,7 @@ func setupEvilGlobals(c *cli.Context, v store.Store, r remote.Remote) {
 	server.Config.Server.Key = c.String("server-key")
 	server.Config.Server.Pass = c.String("agent-secret")
 	server.Config.Server.Host = c.String("server-host")
-	if len(c.String("server-dev-oauth-host")) > 0 {
+	if c.IsSet("server-dev-oauth-host") {
 		server.Config.Server.OAuthHost = c.String("server-dev-oauth-host")
 	} else {
 		server.Config.Server.OAuthHost = c.String("server-host")
