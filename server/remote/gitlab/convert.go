6cc9d6af54a2c76190e1a125d458f68c472c26a5
Anton Bracke <anton@ju60.de>
2022-01-03 12:55:18 +0100

    fix gitlab sub repo listing

--- server/remote/gitlab/convert.go
+++ server/remote/gitlab/convert.go
@@ -28,9 +28,8 @@ import (
 
 func (g *Gitlab) convertGitlabRepo(_repo *gitlab.Project) (*model.Repo, error) {
 	parts := strings.Split(_repo.PathWithNamespace, "/")
-	// TODO: save repo id (support nested repos)
-	var owner = parts[0]
-	var name = parts[1]
+	var owner = strings.Join(parts[:len(parts)-1], "/")
+	var name = parts[len(parts)-1]
 	repo := &model.Repo{
 		Owner:      owner,
 		Name:       name,
