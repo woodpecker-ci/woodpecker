8489b5bd3718692e61672c736a03ac7c6534c559
6543 <6543@obermui.de>
2022-01-08 22:16:07 +0100

    Gitea: push webhook notice changed files of all related commits (#675)
    * getChangedFilesFromPushHook: use all commits
    
    * add test

--- server/remote/gitea/helper_test.go
+++ server/remote/gitea/helper_test.go
@@ -106,6 +106,7 @@ func Test_parse(t *testing.T) {
 			g.Assert(build.Message).Equal(hook.Commits[0].Message)
 			g.Assert(build.Avatar).Equal("http://1.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87")
 			g.Assert(build.Author).Equal(hook.Sender.Login)
+			g.Assert(build.ChangedFiles).Equal([]string{"CHANGELOG.md", "app/controller/application.rb"})
 		})
 
 		g.It("Should return a Repo struct from a push hook", func() {
