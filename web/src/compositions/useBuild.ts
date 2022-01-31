2a5159f7febc73057e46866f77ff72ce45df8154
6543 <6543@obermui.de>
2022-01-31 15:38:39 +0100

    Fix pipeline timestamps (#730)
    * only calculate time on running builds
    
    * Add updated timestamp into database and use it in frontend
    
    * add more trace logging
    
    * refactor (move grpc unrelated func into related package)
    
    * fix xorm schema
    
    * add todo

--- web/src/compositions/useBuild.ts
+++ web/src/compositions/useBuild.ts
@@ -40,13 +40,17 @@ export default (build: Ref<Build | undefined>) => {
     }
 
     const start = build.value.started_at || 0;
-    const end = build.value.finished_at || 0;
+    const end = build.value.finished_at || build.value.updated_at || 0;
 
     if (start === 0) {
       return 0;
     }
 
     if (end === 0) {
+      // only calculate time on running builds
+      if (build.value.status !== 'running') {
+        return 0;
+      }
       return Date.now() - start * 1000;
     }
 
