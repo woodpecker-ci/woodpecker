61bcce4867d12171fe9d1fcf005c525ed124e1c7
6543 <6543@obermui.de>
2022-02-21 18:24:15 +0100

    refactor: rename to match what it does & log ignore case

--- server/plugins/environments/filesystem.go
+++ server/plugins/environments/filesystem.go
@@ -3,6 +3,8 @@ package environments
 import (
 	"strings"
 
+	"github.com/rs/zerolog/log"
+
 	"github.com/woodpecker-ci/woodpecker/server/model"
 )
 
@@ -10,14 +12,15 @@ type builtin struct {
 	globals []*model.Environ
 }
 
-// Filesystem returns a new local registry service.
-func Filesystem(params []string) model.EnvironService {
+// Parse returns a EnvironService based on a string slice where key and value are separated by a ":" delimeter.
+func Parse(params []string) model.EnvironService {
 	var globals []*model.Environ
 
 	for _, item := range params {
 		kvPair := strings.SplitN(item, ":", 2)
 		if len(kvPair) != 2 {
 			// ignore items only containing a key and no value
+			log.Warn().Msgf("key '%s' has no value, will be ignored", kvPair[0])
 			continue
 		}
 		globals = append(globals, &model.Environ{Name: kvPair[0], Value: kvPair[1]})
