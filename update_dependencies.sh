f6f68b5a0e6c3b97b14076f54a02c01f346224e0
6543 <6543@obermui.de>
2022-04-30 22:24:36 +0200

    update module

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -2,7 +2,8 @@
 
 grep 'git' go.mod | grep '\.com' | grep -v indirect | grep -v replace | cut -f 2 | cut -d ' ' -f 1 | while read line; do
   go get -u "$line"
-  make vendor
+  go mod tidy
+  go mod vendor
   git add .
   git commit -m "update $line"
 done
