7de8ea975fd67774f73c827ece58704011727493
6543 <6543@obermui.de>
2022-05-01 09:43:29 +0200

    clean

--- update_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-grep 'git' go.mod | grep '\.com' | grep -v indirect | grep -v replace | cut -f 2 | cut -d ' ' -f 1 | while read line; do
-  go get -u "$line"
-  go mod tidy
-  go mod vendor
-  git add .
-  git commit -m "update $line"
-done
