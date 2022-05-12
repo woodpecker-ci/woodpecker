5240cdc7b6185a62d9d24fcba95b1ff9793ff31c
qwerty287 <ndev@web.de>
2022-05-12 18:25:38 +0200

    Update test

--- server/remote/common/status_test.go
+++ server/remote/common/status_test.go
@@ -10,10 +10,10 @@ import (
 )
 
 func TestGetBuildStatusContext(t *testing.T) {
-	origFormatr := server.Config.Server.StatusContextFormat
+	origFormat := server.Config.Server.StatusContextFormat
 	origCtx := server.Config.Server.StatusContext
 	defer func() {
-		server.Config.Server.StatusContextFormat = origFormatr
+		server.Config.Server.StatusContextFormat = origFormat
 		server.Config.Server.StatusContext = origCtx
 	}()
 
@@ -24,12 +24,12 @@ func TestGetBuildStatusContext(t *testing.T) {
 	assert.EqualValues(t, "", GetBuildStatusContext(repo, build, proc))
 
 	server.Config.Server.StatusContext = "ci/woodpecker"
-	server.Config.Server.StatusContextFormat = "%context/%event/%pipeline"
+	server.Config.Server.StatusContextFormat = "{{ .context }}/{{ .event }}/{{ .pipeline }}"
 	assert.EqualValues(t, "ci/woodpecker/pr/lint", GetBuildStatusContext(repo, build, proc))
 	build.Event = model.EventPush
 	assert.EqualValues(t, "ci/woodpecker/push/lint", GetBuildStatusContext(repo, build, proc))
 
 	server.Config.Server.StatusContext = "ci"
-	server.Config.Server.StatusContextFormat = "%context:%owner/%repo:%event:%pipeline"
+	server.Config.Server.StatusContextFormat = "{{ .context }}:{{ .owner }}/{{ .repo }}:{{ .event }}:{{ .pipeline }}"
 	assert.EqualValues(t, "ci:user1/repo1:push:lint", GetBuildStatusContext(repo, build, proc))
 }
