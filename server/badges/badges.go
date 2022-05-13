abb9fe7842a3030bb0514915f5b4f8f066f96b69
6543 <6543@obermui.de>
2022-05-13 19:33:19 +0200

    Update server/badges/badges.go
    Co-authored-by: qwerty287 <80460567+qwerty287@users.noreply.github.com>

--- server/badges/badges.go
+++ server/badges/badges.go
@@ -24,7 +24,7 @@ var (
 	badgeNone    = `<svg xmlns="http://www.w3.org/2000/svg" width="75" height="20"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><rect rx="3" width="75" height="20" fill="#555"/><rect rx="3" x="37" width="38" height="20" fill="#9f9f9f"/><path fill="#9f9f9f" d="M37 0h4v20h-4z"/><rect rx="3" width="75" height="20" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="19.5" y="15" fill="#010101" fill-opacity=".3">build</text><text x="19.5" y="14">build</text><text x="55" y="15" fill="#010101" fill-opacity=".3">none</text><text x="55" y="14">none</text></g></svg>`
 )
 
-// Generate a svg badge based on an build
+// Generate an SVG badge based on a build
 func Generate(build *model.Build) string {
 	switch build.Status {
 	case model.StatusSuccess:
