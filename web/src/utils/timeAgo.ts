5617f6e0114b3e7f298309edd6140e92ecbd5cb6
6543 <6543@obermui.de>
2022-06-20 19:08:27 +0200

    Add German Translation

--- web/src/utils/timeAgo.ts
+++ web/src/utils/timeAgo.ts
@@ -1,11 +1,13 @@
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en.json';
 import lv from 'javascript-time-ago/locale/lv.json';
+import de from 'javascript-time-ago/locale/de.json';
 
 import { getUserLanguage } from '~/utils/locale';
 
 TimeAgo.addDefaultLocale(en);
 TimeAgo.addLocale(lv);
+TimeAgo.addLocale(de);
 
 const timeAgo = new TimeAgo(getUserLanguage());
 
