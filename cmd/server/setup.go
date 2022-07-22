3a0e55e7baab5f06b71a1ce17dfadcb04301912f
Paweł Bogusławski <pawel.boguslawski@ib.pl>
2022-07-22 16:16:59 +0200

    Update setup.go

--- cmd/server/setup.go
+++ cmd/server/setup.go
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// This file has been modified by Informatyka Boguslawski sp. z o.o. sp.k.
 
 package main
 
