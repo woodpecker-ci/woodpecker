16e5ac160d294b7336c692c48041cb933d71d30f
Paweł Bogusławski <pawel.boguslawski@ib.pl>
2022-07-22 16:18:25 +0200

    Update token.go

--- shared/token/token.go
+++ shared/token/token.go
@@ -1,5 +1,4 @@
 // Copyright 2018 Drone.IO Inc.
-// Copyright 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// This file has been modified by Informatyka Boguslawski sp. z o.o. sp.k.
 
 package token
 
