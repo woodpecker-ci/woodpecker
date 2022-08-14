238a877e7976a6387e7d8e32e5e3e7510fa5a98d
Lauris BH <lauris@nix.lv>
2022-08-14 14:28:39 +0300

    Fix lint warning

--- web/src/components/atomic/Warning.vue
+++ web/src/components/atomic/Warning.vue
@@ -1,15 +1,6 @@
 <template>
   <div
-    class="
-      text-sm text-gray-600
-      font-bold
-      rounded-md
-      border border-solid
-      p-2
-      border-yellow-500
-      bg-yellow-200
-      dark:bg-yellow-600 dark:border-yellow-800 dark:text-light-100
-    "
+    class="text-sm text-gray-600 font-bold rounded-md border border-solid p-2 border-yellow-500 bg-yellow-200 dark:bg-yellow-600 dark:border-yellow-800 dark:text-light-100"
   >
     ⚠ {{ text }}
   </div>
