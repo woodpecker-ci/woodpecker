843bd5e6308130667f1912737b116f621c6297b3
qwerty287 <ndev@web.de>
2022-08-20 09:46:55 +0200

    Remote unique constraint

--- server/store/datastore/migration/007_repo_add_remote_id.go
+++ server/store/datastore/migration/007_repo_add_remote_id.go
@@ -20,7 +20,7 @@ import (
 )
 
 type RepoV007 struct {
-	RemoteID string `json:"-"                               xorm:"UNIQUE 'remote_id'"`
+	RemoteID string `json:"-" xorm:"'remote_id'"`
 }
 
 // TableName return database table name for xorm
