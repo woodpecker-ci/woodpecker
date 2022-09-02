8159743ce9e7fab3dc4abdf7a3537ad3a27ac020
Anbraten <anton@ju60.de>
2022-09-02 10:10:10 +0000

    update docs and set certificates storage location

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -350,11 +350,3 @@ func (a *authorizer) authorize(ctx context.Context) error {
 	}
 	return errors.New("missing agent token")
 }
-
-func cacheDir() string {
-	const base = "golang-autocert"
-	if xdg := os.Getenv("XDG_CACHE_HOME"); xdg != "" {
-		return filepath.Join(xdg, base)
-	}
-	return filepath.Join(os.Getenv("HOME"), ".cache", base)
-}
