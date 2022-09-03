49aec957f807462e240cc7a5e3ce6ac2d103ca67
Anbraten <anton@ju60.de>
2022-09-03 08:09:18 +0000

    change foramt

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -175,7 +175,10 @@ func loop(c *cli.Context) error {
 		}()
 	}
 
-	log.Info().Msgf("Starting Woodpecker agent with version '%s' and backend '%s' running up to %d pipelines in parallel", version.String(), engine.Name(), parallel)
+	log.Info().
+		Msgf(
+			"Starting Woodpecker agent with version '%s' and backend '%s' running up to %d pipelines in parallel",
+			version.String(), engine.Name(), parallel)
 
 	wg.Wait()
 	return nil
