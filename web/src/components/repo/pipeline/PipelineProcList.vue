631daa8a7d6ed579b5eec988e958487d9e8f19fb
Lukas Hass <lukas@slucky.de>
2022-10-21 19:42:38 +0000

    fix gap between buttons

--- web/src/components/repo/pipeline/PipelineProcList.vue
+++ web/src/components/repo/pipeline/PipelineProcList.vue
@@ -82,7 +82,7 @@
             v-for="job in proc.children"
             :key="job.pid"
             type="button"
-            class="flex not-last:mb-1 p-2 border-2 border-transparent rounded-md items-center hover:bg-black hover:bg-opacity-10 dark:hover:bg-white dark:hover:bg-opacity-5 w-full"
+            class="flex mt-1 p-2 border-2 border-transparent rounded-md items-center hover:bg-black hover:bg-opacity-10 dark:hover:bg-white dark:hover:bg-opacity-5 w-full"
             :class="{
               'bg-black bg-opacity-10 dark:bg-white dark:bg-opacity-5': selectedProcId && selectedProcId === job.pid,
             }"
