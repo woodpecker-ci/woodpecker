2e600c298aed05d77b295e24a95f06185562904e
qwerty287 <ndev@web.de>
2022-10-22 14:37:24 +0200

    Fix typecheck

--- web/src/components/repo/pipeline/PipelineProcList.vue
+++ web/src/components/repo/pipeline/PipelineProcList.vue
@@ -90,7 +90,7 @@
             class="flex p-2 border-2 border-transparent rounded-md items-center hover:bg-black hover:bg-opacity-10 dark:hover:bg-white dark:hover:bg-opacity-5 w-full"
             :class="{
               'bg-black bg-opacity-10 dark:bg-white dark:bg-opacity-5': selectedProcId && selectedProcId === job.pid,
-              'mt-1': (pipeline.procs && pipeline.procs.length > 1) || job.pid !== proc.children[0].pid,
+              'mt-1': (pipeline.procs && pipeline.procs.length > 1) || (proc.children && job.pid !== proc.children[0].pid),
             }"
             @click="$emit('update:selected-proc-id', job.pid)"
           >
