87c4c61b72da561eaa1580a21c5ae5daeef56d77
qwerty287 <ndev@web.de>
2022-10-26 16:32:21 +0200

    fix import

--- server/remote/gitea/gitea.go
+++ server/remote/gitea/gitea.go
@@ -33,13 +33,13 @@ import (
 
 	"code.gitea.io/sdk/gitea"
 	"github.com/rs/zerolog/log"
-	"github.com/woodpecker-ci/woodpecker/server/store"
 	"golang.org/x/oauth2"
 
 	"github.com/woodpecker-ci/woodpecker/server"
 	"github.com/woodpecker-ci/woodpecker/server/model"
 	"github.com/woodpecker-ci/woodpecker/server/remote"
 	"github.com/woodpecker-ci/woodpecker/server/remote/common"
+	"github.com/woodpecker-ci/woodpecker/server/store"
 )
 
 const (
