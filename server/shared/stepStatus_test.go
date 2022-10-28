91a09fc2f8d817dd0daaac2b5d02be59a85d2a02
6543 <6543@obermui.de>
2022-10-28 15:43:19 +0200

    nit

--- server/shared/stepStatus_test.go
+++ server/shared/stepStatus_test.go
@@ -1,4 +1,5 @@
-// Copyright 2019 mhmxs.
+// Copyright 2022 Woodpecker Authors
+// Copyright 2019 mhmxs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
