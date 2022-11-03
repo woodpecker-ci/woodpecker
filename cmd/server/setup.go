db92ebff10790835607434e2b8bb2ee188452fcf
Kuzmin Ilya <ilia.kuzmin@indrive.com>
2022-11-03 09:38:58 +0600

    bugfix

--- cmd/server/setup.go
+++ cmd/server/setup.go
@@ -171,19 +171,17 @@ func setupSecretService(c *cli.Context, s store.Store) model.SecretService {
 	decryptionKeysetProvided := c.IsSet("secrets-encryption-decrypt-all-keyset")
 	keysetProvided := c.IsSet("secrets-encryption-keyset")
 
-	if encryptionEnabled || keysetProvided {
-		if keysetProvided && decryptionKeysetProvided {
-			log.Fatal().Msg("Secret service config conflict: you should specify either encryption or decryption keyset, " +
-				"not the both ones")
-		} else if decryptionKeysetProvided {
-			encrypted_secrets.DecryptAll(c, s)
-			return secrets.New(c.Context, s)
-		} else if keysetProvided {
-			return encrypted_secrets.New(c, s)
-		}
+	if keysetProvided && decryptionKeysetProvided {
+		log.Fatal().Msg("Secret service config conflict: you should specify either encryption or decryption keyset, " +
+			"not the both ones")
+	} else if keysetProvided {
+		return encrypted_secrets.New(c, s)
+	} else if encryptionEnabled && decryptionKeysetProvided {
+		encrypted_secrets.DecryptAll(c, s)
+		return secrets.New(c.Context, s)
+	} else if encryptionEnabled && !keysetProvided {
 		log.Fatal().Msg("Failed to initialize secret service: secrets are encrypted but no encryption key provided")
 	}
-
 	return secrets.New(c.Context, s)
 }
 
