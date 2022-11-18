32cc4f7530aa7a7c4d82ad85f673bf154a5c1be8
smainz <stefan@mainz.ws>
2022-11-18 16:17:00 +0100

    Refresh token when running cron
    Before accessing a repo during a cron job the users token is
    refreshed like it is done for e.g. create pipeline. User in this context means the creator of the cron job.
    
    If the scheduled delay between two executions of a cron job is larger than the exiration time of the oauth2 refresh token (provided by the forge) this will fail and the cron job will not be run. But this is the best one can do.

--- server/cron/cron.go
+++ server/cron/cron.go
@@ -22,6 +22,7 @@ import (
 	"github.com/robfig/cron"
 	"github.com/rs/zerolog/log"
 
+	"github.com/woodpecker-ci/woodpecker/server"
 	"github.com/woodpecker-ci/woodpecker/server/forge"
 	"github.com/woodpecker-ci/woodpecker/server/model"
 	"github.com/woodpecker-ci/woodpecker/server/pipeline"
@@ -105,7 +106,7 @@ func runCron(store store.Store, forge forge.Forge, cron *model.Cron, now time.Ti
 	return err
 }
 
-func CreatePipeline(ctx context.Context, store store.Store, forge forge.Forge, cron *model.Cron) (*model.Repo, *model.Pipeline, error) {
+func CreatePipeline(ctx context.Context, store store.Store, f forge.Forge, cron *model.Cron) (*model.Repo, *model.Pipeline, error) {
 	repo, err := store.GetRepo(cron.RepoID)
 	if err != nil {
 		return nil, nil, err
@@ -121,7 +122,23 @@ func CreatePipeline(ctx context.Context, store store.Store, forge forge.Forge, c
 		return nil, nil, err
 	}
 
-	commit, err := forge.BranchHead(ctx, creator, repo, cron.Branch)
+	// if the forge has a refresh token, the current access token
+	// may be stale. Therefore, we should refresh prior to dispatching
+	// the pipeline.
+	if refresher, ok := server.Config.Services.Forge.(forge.Refresher); ok {
+		refreshed, err := refresher.Refresh(ctx, creator)
+		log.Debug().Msgf("token refreshed: %s", refreshed)
+		if err != nil {
+			log.Error().Err(err).Msgf("failed to refresh oauth2 token for creator: %s", creator.Login)
+		} else if refreshed {
+			if err := store.UpdateUser(creator); err != nil {
+				log.Error().Err(err).Msgf("error while updating creator: %s", creator.Login)
+				// move forward
+			}
+		}
+	}
+
+	commit, err := f.BranchHead(ctx, creator, repo, cron.Branch)
 	if err != nil {
 		return nil, nil, err
 	}
