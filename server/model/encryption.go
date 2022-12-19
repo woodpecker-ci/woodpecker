cb85219c6e80f682afb4ebde869c15a74ca75c1e
Kuzmin Ilya <ilia.kuzmin@indrive.com>
2022-12-19 23:49:28 +0600

    encryption refactored

--- server/model/encryption.go
+++ server/model/encryption.go
@@ -6,20 +6,17 @@ type EncryptionBuilder interface {
 	Build()
 }
 
-// EncryptionServiceBuilder should be used only in encryption configuration process
 type EncryptionServiceBuilder interface {
 	WithClients(clients []EncryptionClient) EncryptionServiceBuilder
 	Build() EncryptionService
 }
 
-// EncryptionService defines a service for encryption.
 type EncryptionService interface {
 	Encrypt(plaintext string, associatedData string) string
 	Decrypt(ciphertext string, associatedData string) string
 	Disable()
 }
 
-// EncryptionClient should be used only in encryption configuration process
 type EncryptionClient interface {
 	// InitEncryption should be available only once
 	InitEncryption(encryption EncryptionService)
