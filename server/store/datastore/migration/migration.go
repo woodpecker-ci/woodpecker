eb1080b0fb7962682ac2cd5fdd3453c37a68876a
Anbraten <anton@ju60.de>
2023-03-16 19:18:08 +0100

    typo

--- server/store/datastore/migration/migration.go
+++ server/store/datastore/migration/migration.go
@@ -25,7 +25,7 @@ import (
 )
 
 // APPEND NEW MIGRATIONS
-// they are executed in order and if one fails woodpecker will try to rollback and quit
+// they are executed in order and if one fails woodpecker will try to rollback and quits
 var migrationTasks = []*task{
 	&legacy2Xorm,
 	&alterTableReposDropFallback,
