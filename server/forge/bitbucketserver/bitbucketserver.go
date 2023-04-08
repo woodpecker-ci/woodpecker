9849add8e4ac920d489f52ffe5df4e52a62cab45
qwerty287 <ndev@web.de>
2023-04-08 10:43:08 +0200

    Pagination on BB branches

--- server/forge/bitbucketserver/bitbucketserver.go
+++ server/forge/bitbucketserver/bitbucketserver.go
@@ -229,8 +229,8 @@ func (c *Config) Activate(ctx context.Context, u *model.User, r *model.Repo, lin
 }
 
 // Branches returns the names of all branches for the named repository.
-func (c *Config) Branches(ctx context.Context, u *model.User, r *model.Repo, _ *model.ListOptions) ([]string, error) {
-	bitbucketBranches, err := internal.NewClientWithToken(ctx, c.URL, c.Consumer, u.Token).ListBranches(r.Owner, r.Name)
+func (c *Config) Branches(ctx context.Context, u *model.User, r *model.Repo, p *model.ListOptions) ([]string, error) {
+	bitbucketBranches, err := internal.NewClientWithToken(ctx, c.URL, c.Consumer, u.Token).ListBranches(r.Owner, r.Name, p.Page, p.PerPage)
 	if err != nil {
 		return nil, err
 	}
