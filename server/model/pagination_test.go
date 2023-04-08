a79c04e295d54d868b277dbcb0e15b8078d07d6f
qwerty287 <ndev@web.de>
2023-04-08 13:12:48 +0200

    Better test and fix func

--- server/model/pagination_test.go
+++ server/model/pagination_test.go
@@ -13,7 +13,7 @@ func TestApplyPagination(t *testing.T) {
 
 	assert.Equal(t, ApplyPagination(&ListOptions{All: true}, example), example)
 	assert.Equal(t, ApplyPagination(&ListOptions{Page: 1, PerPage: 1}, example), []int{0})
-	assert.Equal(t, ApplyPagination(&ListOptions{Page: 1, PerPage: 2}, example), []int{0, 1})
+	assert.Equal(t, ApplyPagination(&ListOptions{Page: 2, PerPage: 2}, example), []int{2})
 	assert.Equal(t, ApplyPagination(&ListOptions{Page: 3, PerPage: 1}, example), []int{2})
 	assert.Equal(t, ApplyPagination(&ListOptions{Page: 4, PerPage: 1}, example), []int{})
 	assert.Equal(t, ApplyPagination(&ListOptions{Page: 5, PerPage: 1}, example), []int{})
