14f8b237846222a689eff19575257d00bbfb1fce
qwerty287 <ndev@web.de>
2023-04-08 12:24:41 +0200

    Add test and fix out of buonds

--- server/model/pagination.go
+++ server/model/pagination.go
@@ -10,5 +10,8 @@ func ApplyPagination[T any](d *ListOptions, slice []T) []T {
 	if d.All {
 		return slice
 	}
+	if d.PerPage*(d.Page-1) > len(slice) || d.PerPage*(d.Page) > len(slice) {
+		return []T{}
+	}
 	return slice[d.PerPage*(d.Page-1) : d.PerPage*(d.Page)]
 }
