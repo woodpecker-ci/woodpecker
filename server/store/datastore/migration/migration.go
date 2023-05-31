f91ee5d23a7531fbd32eecfce753cbba4fedfa29
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2023-05-31 21:27:57 +0200

    Remove MSSQL specific code and cleanups (#1796)
    We don't have MSSQL support
    
    ---------
    
    Co-authored-by: Anbraten <anton@ju60.de>

--- server/store/datastore/migration/migration.go
+++ server/store/datastore/migration/migration.go
@@ -92,7 +92,7 @@ func initNew(sess *xorm.Session) error {
 }
 
 func Migrate(e *xorm.Engine) error {
-	if err := e.Sync2(new(migrations)); err != nil {
+	if err := e.Sync(new(migrations)); err != nil {
 		return err
 	}
 
@@ -171,13 +171,13 @@ func runTasks(sess *xorm.Session, tasks []*task) error {
 }
 
 type syncEngine interface {
-	Sync2(beans ...interface{}) error
+	Sync(beans ...interface{}) error
 }
 
 func syncAll(sess syncEngine) error {
 	for _, bean := range allBeans {
-		if err := sess.Sync2(bean); err != nil {
-			return fmt.Errorf("sync2 error '%s': %w", reflect.TypeOf(bean), err)
+		if err := sess.Sync(bean); err != nil {
+			return fmt.Errorf("Sync error '%s': %w", reflect.TypeOf(bean), err)
 		}
 	}
 	return nil
