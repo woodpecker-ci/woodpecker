f444938cc557d005fbf7b9e65148792d5c8b5ab1
6543 <6543@obermui.de>
2023-06-04 12:25:38 +0200

    wordings
    Co-authored-by: Anbraten <anton@ju60.de>

--- server/pipeline/filter.go
+++ server/pipeline/filter.go
@@ -61,12 +61,12 @@ func checkIfFiltered(repo *model.Repo, p *model.Pipeline, forgeYamlConfigs []*fo
 	for _, forgeYamlConfig := range forgeYamlConfigs {
 		substitutedConfigData, err := frontend.EnvVarSubst(string(forgeYamlConfig.Data), matchMetadata.Environ())
 		if err != nil {
-			log.Trace().Err(err).Msgf("fail to substitute config '%s'", forgeYamlConfig.Name)
+			log.Trace().Err(err).Msgf("failed to substitute config '%s'", forgeYamlConfig.Name)
 			return false, err
 		}
 		parsedPipelineConfig, err := yaml.ParseString(substitutedConfigData)
 		if err != nil {
-			log.Trace().Err(err).Msgf("fail to parse config '%s'", forgeYamlConfig.Name)
+			log.Trace().Err(err).Msgf("failed to parse config '%s'", forgeYamlConfig.Name)
 			return false, err
 		}
 		log.Trace().Msgf("config '%s': %#v", forgeYamlConfig.Name, parsedPipelineConfig)
