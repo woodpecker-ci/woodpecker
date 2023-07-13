a778b5fb90247c1c3cfa55868f2e3ee5d532d2ed
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2023-07-13 15:57:50 +0200

    Do not use `window.WOODPECKER_*` variables directly (#1992)
    use the config helper instead

--- web/src/components/layout/header/Navbar.vue
+++ web/src/components/layout/header/Navbar.vue
@@ -69,7 +69,7 @@ export default defineComponent({
     const route = useRoute();
     const authentication = useAuthentication();
     const { darkMode } = useDarkMode();
-    const docsUrl = window.WOODPECKER_DOCS;
+    const docsUrl = config.docs || undefined;
 
     function doLogin() {
       authentication.authenticate(route.fullPath);
