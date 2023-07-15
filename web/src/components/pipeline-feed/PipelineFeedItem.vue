3ac1c4dd0f4d655b93a3b5fd63ceafced2e04d7c
pat-s <patrick.schratz@gmail.com>
2023-07-15 13:50:02 +0200

    .

--- web/src/components/pipeline-feed/PipelineFeedItem.vue
+++ web/src/components/pipeline-feed/PipelineFeedItem.vue
@@ -32,9 +32,9 @@
 import { Tooltip } from 'floating-vue';
 import { computed, toRef } from 'vue';
 
-import useConfig from '~/compositions/useConfig';
 import Icon from '~/components/atomic/Icon.vue';
 import PipelineStatusIcon from '~/components/repo/pipeline/PipelineStatusIcon.vue';
+import useConfig from '~/compositions/useConfig';
 import usePipeline from '~/compositions/usePipeline';
 import { PipelineFeed } from '~/lib/api/types';
 import { useRepoStore } from '~/store/repos';
