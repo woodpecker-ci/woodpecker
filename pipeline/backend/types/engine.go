6d5216c21513135fb06ecad25ee91854b8a9b7dd
6543 <6543@obermui.de>
2023-07-20 06:12:00 +0200

    add TODOs

--- pipeline/backend/types/engine.go
+++ pipeline/backend/types/engine.go
@@ -19,22 +19,27 @@ type Engine interface {
 
 	// Setup the workflow environment.
 	// TODO: rename to "SetupWorkflow"
+	// TODO: pass a task UUID
 	Setup(context.Context, *Config) error
 
 	// Exec start the workflow step.
 	// TODO: rename to "StartStep" to make
+	// TODO: pass a task UUID
 	Exec(context.Context, *Step) error
 
 	// Wait for the workflow step to complete and returns
 	// the completion results.
 	// TODO: rename to "WaitStep" to make
+	// TODO: pass a task UUID
 	Wait(context.Context, *Step) (*State, error)
 
 	// Tail the workflow step logs.
 	// TODO: rename to "TailStep" to make
+	// TODO: pass a task UUID
 	Tail(context.Context, *Step) (io.ReadCloser, error)
 
 	// Destroy the workflow environment.
 	// TODO: rename to "DestroyWorkflow" to make
+	// TODO: pass a task UUID
 	Destroy(context.Context, *Config) error
 }
