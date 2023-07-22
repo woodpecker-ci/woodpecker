d02bbe680561f4488a422770a67ee65aaa99adcd
Robert Kaussow <mail@thegeeklab.de>
2023-07-22 17:44:50 +0200

    fix pipeline feed

--- web/src/components/pipeline-feed/PipelineFeedSidebar.vue
+++ web/src/components/pipeline-feed/PipelineFeedSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <aside
     v-if="isOpen"
-    class="flex flex-col z-50 overflow-y-auto items-center bg-white dark:bg-wp-darkgray-800 dark:border-wp-dark-500"
+    class="flex flex-col z-50 overflow-y-auto items-center bg-wp-background-100 dark:bg-wp-background-200 border-wp-background-400"
     :aria-label="$t('pipeline_feed')"
   >
     <router-link
@@ -11,7 +11,7 @@
         name: 'repo-pipeline',
         params: { repoId: pipeline.repo_id, pipelineId: pipeline.number },
       }"
-      class="flex border-b py-4 px-2 w-full hover:bg-light-300 dark:hover:bg-wp-darkgray-900 dark:border-wp-darkgray-600 hover:shadow-sm"
+      class="flex border-b border-wp-background-400 py-4 px-2 w-full hover:bg-wp-background-300 dark:hover:bg-wp-background-400 hover:shadow-sm"
     >
       <PipelineFeedItem :pipeline="pipeline" />
     </router-link>
