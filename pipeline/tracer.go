e92b179f87b11e70efa9683accb548a2588203f3
6543 <6543@obermui.de>
2023-07-27 23:55:02 +0200

    remove CI_BUILD_STATUS

--- pipeline/tracer.go
+++ pipeline/tracer.go
@@ -38,7 +38,6 @@ var DefaultTracer = TraceFunc(func(state *State) error {
 	state.Pipeline.Step.Environment["CI_STEP_FINISHED"] = strconv.FormatInt(time.Now().Unix(), 10)
 
 	// DEPRECATED
-	state.Pipeline.Step.Environment["CI_BUILD_STATUS"] = "success"
 	state.Pipeline.Step.Environment["CI_BUILD_STARTED"] = strconv.FormatInt(state.Pipeline.Time, 10)
 	state.Pipeline.Step.Environment["CI_BUILD_FINISHED"] = strconv.FormatInt(time.Now().Unix(), 10)
 	state.Pipeline.Step.Environment["CI_JOB_STATUS"] = "success"
@@ -49,7 +48,6 @@ var DefaultTracer = TraceFunc(func(state *State) error {
 		state.Pipeline.Step.Environment["CI_PIPELINE_STATUS"] = "failure"
 		state.Pipeline.Step.Environment["CI_STEP_STATUS"] = "failure"
 		// DEPRECATED
-		state.Pipeline.Step.Environment["CI_BUILD_STATUS"] = "failure"
 		state.Pipeline.Step.Environment["CI_JOB_STATUS"] = "failure"
 	}
 	return nil
