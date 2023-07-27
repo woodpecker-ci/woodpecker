fbe5aa897750a1dfb174aaaa44d3d9f38f9a2479
6543 <6543@obermui.de>
2023-07-27 13:52:38 +0200

    format code

--- server/api/badge.go
+++ server/api/badge.go
@@ -112,12 +112,10 @@ func GetCC(c *gin.Context) {
 	}
 
 	pipelines, err := _store.GetPipelineList(repo, &model.ListOptions{Page: 1, PerPage: 1})
-	if err != nil {
-		if !errors.Is(err, types.RecordNotExist) {
-			log.Warn().Err(err).Msg("could not get pipeline list")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
+	if err != nil && !errors.Is(err, types.RecordNotExist) {
+		log.Warn().Err(err).Msg("could not get pipeline list")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
 	}
 	if len(pipelines) == 0 {
 		c.AbortWithStatus(http.StatusNotFound)
