fa75eb691508f3ff36263058438f0f749bcae073
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2023-08-03 16:59:52 +0200

    Update useTimeAgo.ts

--- web/src/compositions/useTimeAgo.ts
+++ web/src/compositions/useTimeAgo.ts
@@ -10,7 +10,7 @@ const addedLocales = ['en'];
 export default () => new TimeAgo(getUserLanguage());
 export async function loadTimeAgoLocale(locale: string) {
   if (!addedLocales.includes(locale)) {
-    const { default: timeAgoLocale } = await import(`~/assets/timeAgoLocales/${locale}.json.js`);
+    const { default: timeAgoLocale } = await import(`~/assets/timeAgoLocales/${locale}.js`);
     TimeAgo.addLocale(timeAgoLocale);
     addedLocales.push(locale);
   }
