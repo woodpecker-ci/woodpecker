3bad2aa567c997035d272a5c7bfc9c089275d4da
6543 <6543@obermui.de>
2023-08-04 02:47:08 +0200

    code comment and refactor

--- pipeline/backend/local/clone.go
+++ pipeline/backend/local/clone.go
@@ -31,11 +31,13 @@ import (
 	"github.com/woodpecker-ci/woodpecker/shared/constant"
 )
 
+// checkGitCloneCap check if we have the git binary on hand
 func checkGitCloneCap() error {
 	_, err := exec.LookPath("git")
 	return err
 }
 
+// loadClone on backend start determine if there is a global plugin-git binary
 func (e *local) loadClone() {
 	binary, err := exec.LookPath("plugin-git")
 	if err != nil || binary == "" {
@@ -45,6 +47,7 @@ func (e *local) loadClone() {
 	e.pluginGitBinary = binary
 }
 
+// setupClone prepare the clone environment before exec
 func (e *local) setupClone(state *workflowState) error {
 	if e.pluginGitBinary != "" {
 		state.pluginGitBinary = e.pluginGitBinary
@@ -52,66 +55,14 @@ func (e *local) setupClone(state *workflowState) error {
 	}
 
 	log.Info().Msg("no global 'plugin-git' installed, try to download for current workflow")
-
-	type asset struct {
-		Name               string
-		BrowserDownloadURL string `json:"browser_download_url"`
+	state.pluginGitBinary = filepath.Join(state.homeDir, "plugin-git")
+	if runtime.GOOS == "windows" {
+		state.pluginGitBinary += ".exe"
 	}
-
-	type release struct {
-		Assets []asset
-	}
-
-	// get latest release
-	req, _ := http.NewRequest(http.MethodGet, "https://api.github.com/repos/woodpecker-ci/plugin-git/releases/latest", nil)
-	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("could not get latest release: %w", err)
-	}
-	raw, _ := io.ReadAll(resp.Body)
-	_ = resp.Body.Close()
-	var rel release
-	_ = json.Unmarshal(raw, &rel)
-
-	downloaded := false
-	for _, at := range rel.Assets {
-		if strings.Contains(at.Name, runtime.GOOS) && strings.Contains(at.Name, runtime.GOARCH) {
-			resp2, err := http.Get(at.BrowserDownloadURL)
-			if err != nil {
-				return fmt.Errorf("could not download plugin-git: %w", err)
-			}
-			defer resp2.Body.Close()
-
-			state.pluginGitBinary = filepath.Join(state.homeDir, "plugin-git")
-			if runtime.GOOS == "windows" {
-				state.pluginGitBinary += ".exe"
-			}
-
-			file, err := os.Create(state.pluginGitBinary)
-			if err != nil {
-				return fmt.Errorf("could not create plugin-git: %w", err)
-			}
-			defer file.Close()
-
-			if _, err := io.Copy(file, resp2.Body); err != nil {
-				return fmt.Errorf("could not download plugin-git: %w", err)
-			}
-			if err := os.Chmod(state.pluginGitBinary, 0o755); err != nil {
-				return err
-			}
-			downloaded = true
-			break
-		}
-	}
-
-	if downloaded {
-		return fmt.Errorf("could not download plugin-git, binary for this os/arch not found")
-	}
-	return nil
+	return downloadLatestGitPluginBinary(state.pluginGitBinary)
 }
 
+// execClone executes a clone-step locally
 func (e *local) execClone(ctx context.Context, step *types.Step, state *workflowState, env []string) error {
 	if err := e.setupClone(state); err != nil {
 		return fmt.Errorf("setup clone step failed: %w", err)
@@ -156,6 +107,7 @@ func (e *local) execClone(ctx context.Context, step *types.Step, state *workflow
 	return cmd.Start()
 }
 
+// writeNetRC write a netrc file into the home dir of a given workflow state
 func writeNetRC(step *types.Step, state *workflowState) (string, error) {
 	if step.Environment["CI_NETRC_MACHINE"] == "" {
 		return "", nil
@@ -174,4 +126,60 @@ func writeNetRC(step *types.Step, state *workflowState) (string, error) {
 		step.Environment["CI_NETRC_USERNAME"],
 		step.Environment["CI_NETRC_PASSWORD"],
 	)), 0o600)
+}
+
+// downloadLatestGitPluginBinary download the latest plugin-git binary based on runtime OS and Arch
+// and saves it to dest
+func downloadLatestGitPluginBinary(dest string) error {
+	type asset struct {
+		Name               string
+		BrowserDownloadURL string `json:"browser_download_url"`
+	}
+
+	type release struct {
+		Assets []asset
+	}
+
+	// get latest release
+	req, _ := http.NewRequest(http.MethodGet, "https://api.github.com/repos/woodpecker-ci/plugin-git/releases/latest", nil)
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("could not get latest release: %w", err)
+	}
+	raw, _ := io.ReadAll(resp.Body)
+	_ = resp.Body.Close()
+	var rel release
+	if err := json.Unmarshal(raw, &rel); err != nil {
+		return fmt.Errorf("could not unmarshal github response: %w", err)
+	}
+
+	for _, at := range rel.Assets {
+		if strings.Contains(at.Name, runtime.GOOS) && strings.Contains(at.Name, runtime.GOARCH) {
+			resp2, err := http.Get(at.BrowserDownloadURL)
+			if err != nil {
+				return fmt.Errorf("could not download plugin-git: %w", err)
+			}
+			defer resp2.Body.Close()
+
+			file, err := os.Create(dest)
+			if err != nil {
+				return fmt.Errorf("could not create plugin-git: %w", err)
+			}
+			defer file.Close()
+
+			if _, err := io.Copy(file, resp2.Body); err != nil {
+				return fmt.Errorf("could not download plugin-git: %w", err)
+			}
+			if err := os.Chmod(dest, 0o755); err != nil {
+				return err
+			}
+
+			// download successful
+			return nil
+		}
+	}
+
+	return fmt.Errorf("could not download plugin-git, binary for this os/arch not found")
 }
