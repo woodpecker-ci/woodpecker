729fc34efba048eb869c7f51b532f09de1eefece
6543 <6543@obermui.de>
2023-08-07 19:54:52 +0200

    Update cmd/common/common.go
    Co-authored-by: qwerty287 <80460567+qwerty287@users.noreply.github.com>

--- cmd/common/common.go
+++ cmd/common/common.go
@@ -20,7 +20,7 @@ import (
 	"golang.org/x/term"
 )
 
-// IsInteractive checks if the output is piped, but NOT if the session is run interactively..
+// IsInteractive checks if the output is piped, but NOT if the session is run interactively.
 func IsInteractive() bool {
 	return term.IsTerminal(int(os.Stdout.Fd()))
 }
