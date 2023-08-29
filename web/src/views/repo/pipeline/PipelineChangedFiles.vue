76b83523c82be2dab1727b3d4e3e889ac586c813
Robert Kaussow <mail@thegeeklab.de>
2023-08-29 09:44:40 +0200

    hide changed files tab if no files were changed

--- web/src/views/repo/pipeline/PipelineChangedFiles.vue
+++ web/src/views/repo/pipeline/PipelineChangedFiles.vue
@@ -1,9 +1,6 @@
 <template>
-  <Panel v-if="pipeline">
-    <div v-if="pipeline.changed_files === undefined || pipeline.changed_files.length < 1" class="w-full">
-      <div class="text-wp-text-alt-100 text-center">{{ $t('repo.pipeline.no_files') }}</div>
-    </div>
-    <ul v-else class="list-disc list-inside w-full">
+  <Panel>
+    <ul class="list-disc list-inside w-full">
       <li v-for="file in pipeline.changed_files" :key="file">{{ file }}</li>
     </ul>
   </Panel>
