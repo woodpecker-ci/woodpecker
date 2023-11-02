c1faa95d8fbbdfcc582c0cd7d57840e0e4891f59
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2023-11-02 15:31:49 +0100

    Revert breaking pipeline changes (#2677)
    Revert #2180 and #2480 so we can release v2.0 without breaking changes
    in pipeline config.
    
    After merging #2476 we should apply these changes to a new v2.
    
    After merging this, we should be ready for the 2.0 release.
    
    ---------
    
    Co-authored-by: Anbraten <anton@ju60.de>

--- pipeline/frontend/yaml/parse.go
+++ pipeline/frontend/yaml/parse.go
@@ -15,9 +15,13 @@
 package yaml
 
 import (
+	"fmt"
+
 	"codeberg.org/6543/xyaml"
 
+	"github.com/woodpecker-ci/woodpecker/pipeline/frontend/yaml/constraint"
 	"github.com/woodpecker-ci/woodpecker/pipeline/frontend/yaml/types"
+	"github.com/woodpecker-ci/woodpecker/pipeline/frontend/yaml/types/base"
 )
 
 // ParseBytes parses the configuration from bytes b.
@@ -27,6 +31,35 @@ func ParseBytes(b []byte) (*types.Workflow, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// support deprecated branch filter
+	if out.BranchesDontUseIt != nil {
+		if out.When.Constraints == nil {
+			out.When.Constraints = []constraint.Constraint{{Branch: *out.BranchesDontUseIt}}
+		} else if len(out.When.Constraints) == 1 && out.When.Constraints[0].Branch.IsEmpty() {
+			out.When.Constraints[0].Branch = *out.BranchesDontUseIt
+		} else {
+			return nil, fmt.Errorf("could not apply deprecated branches filter into global when filter")
+		}
+		out.BranchesDontUseIt = nil
+	}
+
+	// support deprecated pipeline keyword
+	if len(out.PipelineDontUseIt.ContainerList) != 0 && len(out.Steps.ContainerList) == 0 {
+		out.Steps.ContainerList = out.PipelineDontUseIt.ContainerList
+	}
+
+	// support deprecated platform filter
+	if out.PlatformDontUseIt != "" {
+		if out.Labels == nil {
+			out.Labels = make(base.SliceOrMap)
+		}
+		if _, set := out.Labels["platform"]; !set {
+			out.Labels["platform"] = out.PlatformDontUseIt
+		}
+		out.PlatformDontUseIt = ""
+	}
+	out.PipelineDontUseIt.ContainerList = nil
 
 	return out, nil
 }
