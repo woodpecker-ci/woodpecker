a46b85c7a3b66b7c76c5cd870e33e4d0a0990a36
Anbraten <anton@ju60.de>
2023-11-04 10:36:14 +0100

    update comment

--- server/model/forge.go
+++ server/model/forge.go
@@ -30,27 +30,11 @@ type Forge struct {
 // Client: c.String("bitbucket-client"),
 // Secret: c.String("bitbucket-secret"),
 
-// gogs
-// URL:         c.String("gogs-server"),
-// ### Username:    c.String("gogs-git-username"),
-// ### Password:    c.String("gogs-git-password"),
-// ### PrivateMode: c.Bool("gogs-private-mode"),
-// SkipVerify:  c.Bool("gogs-skip-verify"),
-
 // gitea
 // URL:        strings.TrimRight(server.String(), "/"),
 // Client:     c.String("gitea-client"),
 // Secret:     c.String("gitea-secret"),
 // SkipVerify: c.Bool("gitea-skip-verify"),
-
-// stash
-// URL:               c.String("stash-server"),
-// SkipVerify:        c.Bool("stash-skip-verify"),
-// ### Username:          c.String("stash-git-username"),
-// ### Password:          c.String("stash-git-password"),
-// ### ConsumerKey:       c.String("stash-consumer-key"),
-// ### ConsumerRSA:       c.String("stash-consumer-rsa"),
-// ### ConsumerRSAString: c.String("stash-consumer-rsa-string"),
 
 // gitlab
 // URL:          c.String("gitlab-server"),
@@ -64,12 +48,3 @@ type Forge struct {
 // Secret:     c.String("github-secret"),
 // SkipVerify: c.Bool("github-skip-verify"),
 // ### MergeRef:   c.Bool("github-merge-ref"),
-
-// coding
-// URL:        c.String("coding-server"),
-// Client:     c.String("coding-client"),
-// Secret:     c.String("coding-secret"),
-// SkipVerify: c.Bool("coding-skip-verify"),
-// ### Scopes:     c.StringSlice("coding-scope"),
-// ### Username:   c.String("coding-git-username"),
-// ### Password:   c.String("coding-git-password"),
