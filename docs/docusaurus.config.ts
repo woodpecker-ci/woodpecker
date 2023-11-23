d19057277611655dc6e3d43d3255a0b897888bfc
qwerty287 <ndev@web.de>
2023-11-23 13:18:34 +0100

    fix build

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
             },
             {
               label: 'Server setup',
-              to: '/docs/administration/setup',
+              to: '/docs/administration/deployment/overview',
             },
             {
               label: 'FAQ',
