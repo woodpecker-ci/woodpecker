65f3f7a4dffe67a9a914cc968b90b2cbdba46806
6543 <6543@obermui.de>
2023-12-24 12:04:56 +0100

    add more dag tests

--- pipeline/frontend/yaml/compiler/dag.go
+++ pipeline/frontend/yaml/compiler/dag.go
@@ -96,6 +96,9 @@ func dfsVisit(steps map[string]*dagCompilerStep, name string, visited map[string
 	path = append(path, name)
 
 	for _, dep := range steps[name].dependsOn {
+		if dep == "" {
+			continue
+		}
 		if err := dfsVisit(steps, dep, visited, path); err != nil {
 			return err
 		}
@@ -111,6 +114,9 @@ func convertDAGToStages(steps map[string]*dagCompilerStep, prefix string) ([]*ba
 	for name, step := range steps {
 		// check if all depends_on are valid
 		for _, dep := range step.dependsOn {
+			if dep == "" {
+				continue
+			}
 			if _, ok := steps[dep]; !ok {
 				return nil, &ErrStepMissingDependency{name: name, dep: dep}
 			}
@@ -150,6 +156,9 @@ func convertDAGToStages(steps map[string]*dagCompilerStep, prefix string) ([]*ba
 
 func allDependenciesSatisfied(step *dagCompilerStep, addedSteps map[string]struct{}) bool {
 	for _, childName := range step.dependsOn {
+		if childName == "" {
+			continue
+		}
 		_, ok := addedSteps[childName]
 		if !ok {
 			return false
