b77f2b0e0c1e472451e63a788fd839e22faa525f
qwerty287 <ndev@web.de>
2023-12-27 09:56:25 +0100

    Add some tests

---
+++ server/plugins/environments/parse_test.go
@@ -0,0 +1,26 @@
+package environments
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	service := Parse([]string{})
+	env, err := service.EnvironList(nil)
+	assert.NoError(t, err)
+	assert.Empty(t, env)
+
+	service = Parse([]string{"ENV:value"})
+	env, err = service.EnvironList(nil)
+	assert.NoError(t, err)
+	assert.Len(t, env, 1)
+	assert.Equal(t, env[0].Name, "ENV")
+	assert.Equal(t, env[0].Value, "value")
+
+	service = Parse([]string{"ENV:value", "ENV2:value2"})
+	env, err = service.EnvironList(nil)
+	assert.NoError(t, err)
+	assert.Len(t, env, 2)
+}
