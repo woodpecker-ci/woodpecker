be7b23d60753c5994e24da502b8c96bbdb747f44
6543 <6543@obermui.de>
2023-12-30 22:32:07 +0100

    more code and code comments

--- server/model/secret.go
+++ server/model/secret.go
@@ -105,14 +105,21 @@ func (s Secret) IsRepository() bool {
 // Match returns true if an image and event match the restricted list.
 // Note that EventPullClosed are treated as EventPull.
 func (s *Secret) Match(event WebhookEvent) bool {
+	// if there is no filter set secret matches all webhook events
+	if len(s.Events) == 0 {
+		return true
+	}
+	// tread all pull events the same way
 	if event == EventPullClosed {
 		event = EventPull
 	}
+	// one match is enough
 	for _, e := range s.Events {
 		if e == event {
 			return true
 		}
 	}
+	// a filter is set but the webhook did not match it
 	return false
 }
 
