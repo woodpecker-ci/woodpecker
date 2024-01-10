woodpecker-ci/woodpecker

commit 768fd71841ff348518c2988bb2d0278f59dc690e

Enable some linters (#3129)
Mostly those that did not require much work.

From #2960

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

cmd/server/server.go
@@ -87,10 +87,7 @@ func run(c *cli.Context) error {
 		log.Fatal().Err(err).Msg("can't setup forge")
 	}
 
-	_store, err := setupStore(c)
-	if err != nil {
-		log.Fatal().Err(err).Msg("can't setup database store")
-	}
+	_store := setupStore(c)
 	defer func() {
 		if err := _store.Close(); err != nil {
 			log.Error().Err(err).Msg("could not close store")
@@ -317,7 +314,7 @@ func setupEvilGlobals(c *cli.Context, v store.Store, f forge.Forge) error {
 
 	// Execution
 	_events := c.StringSlice("default-cancel-previous-pipeline-events")
-	events := make([]model.WebhookEvent, len(_events))
+	events := make([]model.WebhookEvent, len(_events), 0)
 	for _, v := range _events {
 		events = append(events, model.WebhookEvent(v))
 	}

server/grpc/server.go
@@ -74,17 +74,14 @@ func (s *WoodpeckerServer) Next(c context.Context, req *proto.NextRequest) (*pro
 
 	res := new(proto.NextResponse)
 	pipeline, err := s.peer.Next(c, filter)
-	if err != nil {
-		return res, err
-	}
-	if pipeline == nil {
+	if err != nil || pipeline == nil {
 		return res, err
 	}
 
 	res.Workflow = new(proto.Workflow)
 	res.Workflow.Id = pipeline.ID
 	res.Workflow.Timeout = pipeline.Timeout
-	res.Workflow.Payload, _ = json.Marshal(pipeline.Config)
+	res.Workflow.Payload, err = json.Marshal(pipeline.Config)
 
 	return res, err
 }

.golangci.yml
@@ -102,6 +102,24 @@ linters-settings:
           - pkg: 'go.woodpecker-ci.org/woodpecker/v2/cmd/agent'
           - pkg: 'go.woodpecker-ci.org/woodpecker/v2/cmd/cli'
           - pkg: 'go.woodpecker-ci.org/woodpecker/v2/woodpecker-go/woodpecker'
+  gci:
+    sections:
+      - standard
+      - default
+      - prefix(go.woodpecker-ci.org/woodpecker)
+  gomnd:
+    ignored-numbers:
+      - '0o600'
+      - '0o660'
+      - '0o644'
+      - '0o755'
+      - '0o700'
+    ignored-functions:
+      - make
+      - time.*
+      - strings.Split
+      - callerName
+      - securecookie.GenerateRandomKey
 
 linters:
   disable-all: true
@@ -124,6 +142,25 @@ linters:
     - forbidigo
     - zerologlint
     - depguard
+    - asciicheck
+    - bodyclose
+    - dogsled
+    - durationcheck
+    - errchkjson
+    - gochecknoinits
+    - goheader
+    - gomoddirectives
+    - gomodguard
+    - goprintffuncname
+    - importas
+    - makezero
+    - rowserrcheck
+    - sqlclosecheck
+    - tenv
+    - unconvert
+    - unparam
+    - wastedassign
+    - whitespace
 
 run:
   timeout: 15m
@@ -140,7 +177,12 @@ issues:
     - path: 'cmd/*|cli/*'
       linters:
         - forbidigo
+
     # allow some setup functions to use log.Fatal()
     - path: 'server/web/web.go|server/plugins/encryption/tink_keyset_watcher.go|shared/logger/logger.go'
       linters:
         - forbidigo
+
+    - path: '_test.go'
+      linters:
+        - forcetypeassert

agent/state.go
@@ -74,8 +74,11 @@ func (s *State) Healthy() bool {
 
 func (s *State) WriteTo(w io.Writer) (int64, error) {
 	s.Lock()
-	out, _ := json.Marshal(s)
+	out, err := json.Marshal(s)
 	s.Unlock()
+	if err != nil {
+		return 0, err
+	}
 	ret, err := w.Write(out)
 	return int64(ret), err
 }

cmd/agent/agent.go
@@ -271,6 +271,8 @@ func getBackendEngine(backendCtx context.Context, backendName string, addons []s
 }
 
 func runWithRetry(context *cli.Context) error {
+	initHealth()
+
 	retryCount := context.Int("connect-retry-count")
 	retryDelay := context.Duration("connect-retry-delay")
 	var err error

cmd/agent/health.go
@@ -31,7 +31,7 @@ import (
 // following specification:
 //   https://github.com/mozilla-services/Dockerflow
 
-func init() {
+func initHealth() {
 	http.HandleFunc("/varz", handleStats)
 	http.HandleFunc("/healthz", handleHeartbeat)
 	http.HandleFunc("/version", handleVersion)
@@ -48,10 +48,13 @@ func handleHeartbeat(w http.ResponseWriter, _ *http.Request) {
 func handleVersion(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(200)
 	w.Header().Add("Content-Type", "text/json")
-	_ = json.NewEncoder(w).Encode(versionResp{
+	err := json.NewEncoder(w).Encode(versionResp{
 		Source:  "https://github.com/woodpecker-ci/woodpecker",
 		Version: version.String(),
 	})
+	if err != nil {
+		log.Error().Err(err).Msg("handleVersion")
+	}
 }
 
 func handleStats(w http.ResponseWriter, _ *http.Request) {

cmd/server/setup.go
@@ -54,7 +54,7 @@ import (
 	addonTypes "go.woodpecker-ci.org/woodpecker/v2/shared/addon/types"
 )
 
-func setupStore(c *cli.Context) (store.Store, error) {
+func setupStore(c *cli.Context) store.Store {
 	datasource := c.String("datasource")
 	driver := c.String("driver")
 	xorm := store.XORM{
@@ -95,7 +95,7 @@ func setupStore(c *cli.Context) (store.Store, error) {
 		log.Fatal().Err(err).Msg("could not migrate datastore")
 	}
 
-	return store, nil
+	return store
 }
 
 func checkSqliteFileExist(path string) error {

pipeline/backend/kubernetes/service.go
@@ -26,7 +26,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func mkService(namespace, name string, ports []uint16, selector map[string]string) (*v1.Service, error) {
+func mkService(namespace, name string, ports []uint16, selector map[string]string) *v1.Service {
 	log.Trace().Str("name", name).Interface("selector", selector).Interface("ports", ports).Msg("Creating service")
 
 	var svcPorts []v1.ServicePort
@@ -48,7 +48,7 @@ func mkService(namespace, name string, ports []uint16, selector map[string]strin
 			Selector: selector,
 			Ports:    svcPorts,
 		},
-	}, nil
+	}
 }
 
 func serviceName(step *types.Step) (string, error) {
@@ -69,10 +69,7 @@ func startService(ctx context.Context, engine *kube, step *types.Step) (*v1.Serv
 		StepLabel: podName,
 	}
 
-	svc, err := mkService(engine.config.Namespace, name, step.Ports, selector)
-	if err != nil {
-		return nil, err
-	}
+	svc := mkService(engine.config.Namespace, name, step.Ports, selector)
 
 	return engine.client.CoreV1().Services(engine.config.Namespace).Create(ctx, svc, metav1.CreateOptions{})
 }

commit f3420716ab12d3bac8a31f5ddbe796a004e35205

Fix startup panic (#3160)
introduced by #3129

cmd/server/server.go
@@ -314,7 +314,7 @@ func setupEvilGlobals(c *cli.Context, v store.Store, f forge.Forge) error {
 
 	// Execution
 	_events := c.StringSlice("default-cancel-previous-pipeline-events")
-	events := make([]model.WebhookEvent, len(_events), 0)
+	events := make([]model.WebhookEvent, 0, len(_events))
 	for _, v := range _events {
 		events = append(events, model.WebhookEvent(v))
 	}