0ca7f7c759f02d8f0ef10d08eee20aea26a8ccfd
6543 <6543@obermui.de>
2024-01-18 23:42:23 +0100

    dont lower test coverage

--- pipeline/backend/common/script_test.go
+++ pipeline/backend/common/script_test.go
@@ -31,4 +31,6 @@ func TestGenerateNetRCCommand(t *testing.T) {
 	assert.EqualValues(t, []string{"echo CmlmIFsgLW4gIiRDSV9ORVRSQ19NQUNISU5FIiBdOyB0aGVuCmNhdCA8PEVPRiA+ICRIT01FLy5uZXRyYwptYWNoaW5lICRDSV9ORVRSQ19NQUNISU5FCmxvZ2luICRDSV9ORVRSQ19VU0VSTkFNRQpwYXNzd29yZCAkQ0lfTkVUUkNfUEFTU1dPUkQKRU9GCmNobW9kIDA2MDAgJEhPTUUvLm5ldHJjCmZpCnVuc2V0IENJX05FVFJDX1VTRVJOQU1FCnVuc2V0IENJX05FVFJDX1BBU1NXT1JECnVuc2V0IENJX1NDUklQVAo= | base64 -d | /bin/sh -e"}, posix)
 	windows := GenerateNetRCCommand(DefaultWindowsEntry)
 	assert.EqualValues(t, []string{"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(\"JEVycm9yQWN0aW9uUHJlZmVyZW5jZSA9ICdTdG9wJzsKJmNtZCAvYyAibWtkaXIgYzpccm9vdCI7CmlmICgkRW52OkNJX05FVFJDX01BQ0hJTkUpIHsKJG5ldHJjPVtzdHJpbmddOjpGb3JtYXQoInswfVxfbmV0cmMiLCRFbnY6SE9NRSk7CiJtYWNoaW5lICRFbnY6Q0lfTkVUUkNfTUFDSElORSIgPj4gJG5ldHJjOwoibG9naW4gJEVudjpDSV9ORVRSQ19VU0VSTkFNRSIgPj4gJG5ldHJjOwoicGFzc3dvcmQgJEVudjpDSV9ORVRSQ19QQVNTV09SRCIgPj4gJG5ldHJjOwp9OwpbRW52aXJvbm1lbnRdOjpTZXRFbnZpcm9ubWVudFZhcmlhYmxlKCJDSV9ORVRSQ19QQVNTV09SRCIsJG51bGwpOwpbRW52aXJvbm1lbnRdOjpTZXRFbnZpcm9ubWVudFZhcmlhYmxlKCJDSV9TQ1JJUFQiLCRudWxsKTs=\")) | iex"}, windows)
+	busybox := GenerateNetRCCommand([]string{"/bin/busybox", "-c"})
+	assert.Len(t, busybox, 0)
 }
