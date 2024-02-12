630d2f8d04f412af17b8bf890c7439e26e76895c
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-13 00:42:55 +0100

    enable background update check

--- cli/common/before.go
+++ cli/common/before.go
@@ -1,7 +1,10 @@
 package common
 
 import (
+	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
+
+	"go.woodpecker-ci.org/woodpecker/v2/cli/update"
 )
 
 func Before(c *cli.Context) error {
@@ -10,21 +13,21 @@ func Before(c *cli.Context) error {
 	}
 
 	// TODO: background update check
-	// go func() {
-	// 	log.Debug().Msg("Checking for updates ...")
+	go func() {
+		log.Debug().Msg("Checking for updates ...")
 
-	// 	newVersion, err := update.CheckForUpdate(c, false)
-	// 	if err != nil {
-	// 		log.Printf("Failed to check for updates: %s", err)
-	// 		return
-	// 	}
+		newVersion, err := update.CheckForUpdate(c, false)
+		if err != nil {
+			log.Error().Err(err).Msgf("Failed to check for updates")
+			return
+		}
 
-	// 	if newVersion != nil {
-	// 		log.Info().Msgf("A new version of woodpecker-cli is available: %s", newVersion)
-	// 	} else {
-	// 		log.Debug().Msgf("You are using the latest version of woodpecker-cli")
-	// 	}
-	// }()
+		if newVersion != nil {
+			log.Warn().Msgf("A new version of woodpecker-cli is available: %s", newVersion)
+		} else {
+			log.Debug().Msgf("You are using the latest version of woodpecker-cli")
+		}
+	}()
 
 	return nil
 }
