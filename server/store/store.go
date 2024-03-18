e57a09a4046bb1cc1d284845943158b422bd7f2d
6543 <m.huber@kithara.com>
2024-03-18 20:07:45 +0100

    Update pipeline state on server as a whole on approval (#3504)
    We can not just update some records for steps, as we want the pipeline
    engine as single source of truth but not manage the state.
    And the server should only manage the state but not how pipelines work.
    
    We can match the pipeline but neither workflows or steps 1:1, so we
    "update" them as a whole by deleting existing workflow and step data and
    insert the new info from engine.
    
    close   #3494
    close  #3472
    
    ---------
    *Sponsored by Kithara Software GmbH*
    
    ---------
    
    Co-authored-by: Robert Kaussow <xoxys@rknet.org>

--- server/store/store.go
+++ server/store/store.go
@@ -171,6 +171,7 @@ type Store interface {
 	// Workflow
 	WorkflowGetTree(*model.Pipeline) ([]*model.Workflow, error)
 	WorkflowsCreate([]*model.Workflow) error
+	WorkflowsReplace(*model.Pipeline, []*model.Workflow) error
 	WorkflowLoad(int64) (*model.Workflow, error)
 	WorkflowUpdate(*model.Workflow) error
 
