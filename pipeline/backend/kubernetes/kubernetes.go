82881f136964d4c78817bb46798a504f35acf940
elias.souza <elias.souza@quintoandar.com.br>
2024-04-01 15:47:33 -0300

    fix: pipeline freezing with pod satus ImagePullBackOff

--- pipeline/backend/kubernetes/kubernetes.go
+++ pipeline/backend/kubernetes/kubernetes.go
@@ -318,7 +318,7 @@ func (e *kube) TailStep(ctx context.Context, step *types.Step, taskUUID string) 
 
 		if pod.Name == podName {
 			if isImagePullBackOffState(pod) {
-				up <- false
+				up <- true
 			}
 			switch pod.Status.Phase {
 			case v1.PodRunning, v1.PodSucceeded, v1.PodFailed:
