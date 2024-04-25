e08869716a5eb44d03140e7d140017ecefe09099
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-04-25 12:45:05 +0200

    add tests

--- agent/logger/buf.go
+++ agent/logger/buf.go
@@ -3,23 +3,25 @@ package logger
 import (
 	"bufio"
 	"io"
+	"sync"
 	"time"
 )
 
 type LogBuffer struct {
+	*sync.Mutex
 	buffer        *bufio.Writer
 	flushInterval time.Duration
 	timer         *time.Timer
-	flushChan     chan struct{}
 	closeChan     chan struct{}
 }
 
 func NewLogBuffer(writer io.Writer, bufferSize int, flushInterval time.Duration) *LogBuffer {
 	lb := &LogBuffer{
+		Mutex:         &sync.Mutex{},
 		buffer:        bufio.NewWriterSize(writer, bufferSize),
 		flushInterval: flushInterval,
 		timer:         time.NewTimer(flushInterval),
-		flushChan:     make(chan struct{}, 1),
+		closeChan:     make(chan struct{}),
 	}
 	go lb.start()
 	return lb
@@ -49,17 +51,16 @@ func (lb *LogBuffer) start() {
 }
 
 func (lb *LogBuffer) waitForFlush() bool {
-	lb.timer.Stop()
-	defer lb.timer.Reset(lb.flushInterval)
-
 	// Wait for either a timeout or a manual flush signal
 	select {
 	case <-lb.timer.C:
 		// Time limit reached, flush the buffer
-		lb.buffer.Flush()
-	case <-lb.flushChan:
-		// Manual flush signal received
-		lb.buffer.Flush()
+		lb.Lock()
+		defer lb.Unlock()
+		err := lb.buffer.Flush()
+		if err != nil {
+			return false
+		}
 	case <-lb.closeChan:
 		// Close signal received
 		return false
@@ -69,12 +70,13 @@ func (lb *LogBuffer) waitForFlush() bool {
 }
 
 func (lb *LogBuffer) Flush() error {
-	lb.flushChan <- struct{}{}
-	return nil
+	lb.Lock()
+	defer lb.Unlock()
+	return lb.buffer.Flush()
 }
 
 func (lb *LogBuffer) Close() error {
 	lb.timer.Stop()
-	close(lb.flushChan)
+	close(lb.closeChan)
 	return lb.buffer.Flush()
 }
