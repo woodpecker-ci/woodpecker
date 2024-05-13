5067210dd1709cab9582fa7893317f3ce806194c
6543 <6543@obermui.de>
2024-05-13 17:48:07 +0200

    Add nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  # Override nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      systems,
+    }:
+    flake-utils.lib.eachSystem (import systems) (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # generic
+            gnumake
+            gnutar
+
+            # frontend
+            nodejs
+            nodePackages.pnpm
+
+            # backend
+            go
+            golangci-lint
+            gofumpt
+            go-mockery
+            protobuf
+          ];
+        };
+      }
+    );
+}
