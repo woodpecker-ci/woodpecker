8059e65b5253043e9ea3068526986548423e4dec
6543 <6543@obermui.de>
2024-05-14 04:43:21 +0200

    finish for now

--- pipeline/backend/types/step.go
+++ pipeline/backend/types/step.go
@@ -34,7 +34,7 @@ type Step struct {
 	Networks       []Conn            `json:"networks,omitempty"`
 	DNS            []string          `json:"dns,omitempty"`
 	DNSSearch      []string          `json:"dns_search,omitempty"`
-	MemSwapLimit   int64             `json:"memswap_limit,omitempty"` // cspell:disable-line
+	MemSwapLimit   int64             `json:"memswap_limit,omitempty"`
 	MemLimit       int64             `json:"mem_limit,omitempty"`
 	ShmSize        int64             `json:"shm_size,omitempty"`
 	CPUQuota       int64             `json:"cpu_quota,omitempty"`
