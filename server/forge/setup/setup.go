399d229635c3fd160a8bcc5b413f4984b59d61f6
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2024-05-14 07:23:25 +0200

    Update server/forge/setup/setup.go
    Co-authored-by: John Olheiser <john.olheiser@gmail.com>

--- server/forge/setup/setup.go
+++ server/forge/setup/setup.go
@@ -94,7 +94,7 @@ func setupForgejo(forge *model.Forge) (forge.Forge, error) {
 	if len(opts.URL) == 0 {
 		return nil, fmt.Errorf("WOODPECKER_GITEA_URL must be set")
 	}
-	log.Trace().Msgf("Forge (gitea) opts: %#v", opts)
+	log.Trace().Msgf("Forge (forgejo) opts: %#v", opts)
 	return forgejo.New(opts)
 }
 
