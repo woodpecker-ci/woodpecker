bac7e34e9450e13b3dce6e1f34059ddfe7fa17ec
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-05-28 10:20:48 +0200

    update config

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,199 +1,127 @@
 // cSpell:ignore TSES
 // @ts-check
 
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-//import eslintImport from 'eslint-plugin-import';
-//import eslintTypescript from '@typescript-eslint/eslint-plugin';
-import vueParser from 'vue-eslint-parser';
-import globals from 'globals';
-import js from '@eslint/js';
-import airBnbBase from 'eslint-config-airbnb-base';
-//import airBnbTS from 'eslint-config-airbnb-typescript/base';
-import eslintVue from 'eslint-plugin-vue';
-import eslintVueScopedCSS from 'eslint-plugin-vue-scoped-css';
-import eslintPrettier from 'eslint-plugin-prettier/recommended';
-import eslintPromise from 'eslint-plugin-promise';
-import tseslint from 'typescript-eslint';
+import vueI18n from '@intlify/eslint-plugin-vue-i18n';
+import antfu from '@antfu/eslint-config';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+export default antfu(
+  // eslint.configs.recommended,
+  // js.configs.recommended,
+  // eslintPromise.configs.recommended,
 
-// TODO check eslint-env
-/* eslint-env node */
-
-export default tseslint.config(
   {
-    ignores: [
-      'dist/**',
-      'package.json',
-      'tsconfig.eslint.json',
-      'tsconfig.json',
-      'src/assets/locales/**',
-      'src/assets/dayjsLocales/**',
-      'components.d.ts',
-    ],
+    stylistic: false,
+
+    typescript: {
+      tsconfigPath: './tsconfig.eslint.json',
+    },
+    vue: true,
+
+    // Disable jsonc and yaml support
+    jsonc: false,
+    yaml: false,
   },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
 
-  //airBnbBase,
-  //airBnbTS,
-  //eslintImport, used these 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'
-  ...eslintVue.configs['flat/recommended'],
-  ...eslintVueScopedCSS.configs['flat/recommended'],
-  eslintPrettier,
-  //eslintPromise.configs.recommended,
+  /// TypeScript
+  // ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommendedTypeChecked,
+  // ...tseslint.configs.strictTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+
+  /// Vue
+  // ...pluginVue.configs['flat/recommended'],
+  // {
+  //   languageOptions: {
+  //     parser: vueParser,
+  //     parserOptions: {
+  //       ecmaVersion: 'latest',
+  //       project: ['./tsconfig.eslint.json'],
+  //       parser: tsParser,
+  //       sourceType: 'module',
+  //       extraFileExtensions: ['.vue'],
+  //       ecmaFeatures: {
+  //         jsx: true,
+  //       },
+  //     },
+  //   },
+  // },
+
   {
-    files: ['**/*.js', '**/*.ts', '**/*.vue'],
-
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        project: ['./tsconfig.eslint.json'],
-        tsconfigRootDir: path.dirname(fileURLToPath(import.meta.url)),
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore see https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
-        parser: tseslint.parser,
-        extraFileExtensions: ['.vue', '.json'],
-      },
-      sourceType: 'module',
-      globals: globals.browser,
-    },
-
-    linterOptions: {
-      reportUnusedDisableDirectives: 'warn',
-    },
-
-    plugins: {
-      //TODO 'import': eslintImport,
-      'simple-import-sort': simpleImportSort,
-      //TODO promise: eslintPromise,
-      vue: eslintVue,
-      'vue-scoped-css': eslintVueScopedCSS,
-    },
-
+    files: ['**/*.vue'],
     rules: {
-      // enable scope analysis rules
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': 'error',
-      'no-shadow': 'off',
-      '@typescript-eslint/no-shadow': 'error',
-      'no-redeclare': 'off',
-      '@typescript-eslint/no-redeclare': 'error',
-
-      // make typescript eslint rules even more strict
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      // SOURCE: https://github.com/iamturns/eslint-config-airbnb-typescript/blob/4aec5702be5b4e74e0e2f40bc78b4bc961681de1/lib/shared.js#L41
-      '@typescript-eslint/naming-convention': [
-        'error',
-        // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
-        {
-          selector: 'variable',
-          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-          leadingUnderscore: 'allow',
-        },
-        // Allow camelCase functions (23.2), and PascalCase functions (23.8)
-        {
-          selector: 'function',
-          format: ['camelCase', 'PascalCase'],
-        },
-        // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
-        {
-          selector: 'typeLike',
-          format: ['PascalCase'],
-        },
-      ],
-
-      //'import/no-unresolved': 'off', // disable as this is handled by tsc itself
-      //'import/first': 'error',
-      //'import/newline-after-import': 'error',
-      //'import/no-cycle': 'error',
-      //'import/no-relative-parent-imports': 'error',
-      //'import/no-duplicates': 'error',
-      //'import/no-extraneous-dependencies': 'error',
-      //'import/extensions': 'off',
-      //'import/prefer-default-export': 'off',
-
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-
-      //'promise/prefer-await-to-then': 'error',
-      //'promise/prefer-await-to-callbacks': 'error',
-
-      'no-underscore-dangle': 'off',
-      'no-else-return': ['error', { allowElseIf: false }],
-      'no-return-assign': ['error', 'always'],
-      'no-return-await': 'error',
-      'no-useless-return': 'error',
-      'no-restricted-imports': [
-        'error',
-        {
-          patterns: ['src', 'dist'],
-        },
-      ],
-      'no-console': 'warn',
-      'no-useless-concat': 'error',
-      'prefer-const': 'error',
-      'spaced-comment': ['error', 'always'],
-      'object-shorthand': ['error', 'always'],
-      'no-useless-rename': 'error',
-      eqeqeq: 'error',
-
-      'vue/attribute-hyphenation': 'error',
-      // enable in accordance with https://github.com/prettier/eslint-config-prettier#vuehtml-self-closing
+      'vue/multi-word-component-names': 'off',
       'vue/html-self-closing': [
         'error',
         {
           html: {
-            void: 'any',
+            void: 'always',
+            normal: 'always',
+            component: 'always',
+          },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+      'vue/block-order': [
+        'error',
+        {
+          order: ['template', 'script', 'style'],
+        },
+      ],
+      'vue/singleline-html-element-content-newline': ['off'],
+    },
+  },
+
+  /// Vue I18n
+  ...vueI18n.configs['flat/recommended'],
+  {
+    rules: {
+      '@intlify/vue-i18n/no-raw-text': [
+        'error',
+        {
+          attributes: {
+            '/.+/': ['label'],
           },
         },
       ],
-      'vue/no-static-inline-styles': 'error',
-      'vue/v-on-function-call': 'error',
-      'vue/no-useless-v-bind': 'error',
-      'vue/no-useless-mustaches': 'error',
-      'vue/no-useless-concat': 'error',
-      'vue/no-boolean-default': 'error',
-      'vue/html-button-has-type': 'error',
-      'vue/component-name-in-template-casing': 'error',
-      'vue/match-component-file-name': [
-        'error',
-        {
-          extensions: ['vue'],
-          shouldMatchCase: true,
-        },
-      ],
-      'vue/require-name-property': 'error',
-      'vue/v-for-delimiter-style': 'error',
-      'vue/no-empty-component-block': 'error',
-      'vue/no-duplicate-attr-inheritance': 'error',
-      'vue/no-unused-properties': [
-        'error',
-        {
-          groups: ['props', 'data', 'computed', 'methods', 'setup'],
-        },
-      ],
-      'vue/new-line-between-multi-line-property': 'error',
-      'vue/padding-line-between-blocks': 'error',
-      'vue/multi-word-component-names': 'off',
-      'vue/no-reserved-component-names': 'off',
-
-      // css rules
-      'vue-scoped-css/no-unused-selector': 'error',
-      'vue-scoped-css/no-parsing-error': 'error',
-      'vue-scoped-css/require-scoped': 'error',
-
-      // enable in accordance with https://github.com/prettier/eslint-config-prettier#curly
-      curly: ['error', 'all'],
-
-      // risky because of https://github.com/prettier/eslint-plugin-prettier#arrow-body-style-and-prefer-arrow-callback-issue
-      'arrow-body-style': 'error',
-      'prefer-arrow-callback': 'error',
+      '@intlify/vue-i18n/key-format-style': ['error', 'snake_case'],
+      '@intlify/vue-i18n/no-duplicate-keys-in-locale': 'error',
+      '@intlify/vue-i18n/no-dynamic-keys': 'error',
+      '@intlify/vue-i18n/no-deprecated-i18n-component': 'error',
+      '@intlify/vue-i18n/no-deprecated-tc': 'error',
+      '@intlify/vue-i18n/no-i18n-t-path-prop': 'error',
+      // '@intlify/vue-i18n/no-missing-keys-in-other-locales': 'error',
+      '@intlify/vue-i18n/valid-message-syntax': 'error',
+      '@intlify/vue-i18n/no-missing-keys': 'error',
+      '@intlify/vue-i18n/no-unknown-locale': 'error',
+      '@intlify/vue-i18n/no-unused-keys': ['error', { extensions: ['.ts', '.vue'] }],
+      '@intlify/vue-i18n/prefer-sfc-lang-attr': 'error',
+      '@intlify/vue-i18n/no-html-messages': 'error',
+      '@intlify/vue-i18n/prefer-linked-key-with-paren': 'error',
+      '@intlify/vue-i18n/sfc-locale-attr': 'error',
     },
+    settings: {
+      'vue-i18n': {
+        localeDir: './src/assets/locales/en.json',
+        // Specify the version of `vue-i18n` you are using.
+        // If not specified, the message will be parsed twice.
+        messageSyntaxVersion: '^9.0.0',
+      },
+    },
+  },
+
+  /// Ignore list
+  {
+    ignores: [
+      // # don't lint build output (make sure it's set to your correct build folder name)
+      'dist',
+      'coverage/',
+      'package.json',
+      'tsconfig.eslint.json',
+      'tsconfig.json',
+      // 'src/assets/locales/',
+      'src/assets/dayjsLocales/',
+      'components.d.ts',
+    ],
   },
 );
