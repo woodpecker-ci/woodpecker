c7c69d9bf8e4462e6ac8fe4a353fd411df4583a0
qwerty287 <qwerty287@posteo.de>
2024-06-01 12:57:22 +0200

    fix lint

--- web/src/shims-vue.d.ts
+++ web/src/shims-vue.d.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
+
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
