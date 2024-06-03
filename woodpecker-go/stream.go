bbe45334988d3725e805704274a305bea7fa519a
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-06-03 18:28:00 +0200

    add streaming

--- woodpecker-go/stream.go
+++ woodpecker-go/stream.go
@@ -1,16 +1,22 @@
 package woodpeckergo
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/r3labs/sse/v2"
 )
 
-func (c *Client) StreamPipelineStepLog(repoID, pipelineNumber, stepID int64) error {
-	events := make(chan *sse.Event)
+func (c *Client) StreamPipelineStepLog(ctx context.Context, repoID, pipelineNumber, stepID int64) error {
+	logs := make(chan *sse.Event)
 
 	client := sse.NewClient(fmt.Sprintf("%s/api/stream/logs/%d/%d/%d", c.uri, repoID, pipelineNumber, stepID))
 	client.Connection.Transport = c.transport.Transport
 
-	return client.SubscribeChan("logs", events)
+	client.SubscribeChan("logs", logs)
+	defer client.Close()
+	<-ctx.Done()
+	client.Unsubscribe(logs)
+
+	return nil
 }
