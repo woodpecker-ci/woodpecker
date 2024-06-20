19e2a27f37c1256a36e9e2112e787bcbb3e17549
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-06-20 17:07:39 +0200

    improve tests

--- server/api/login_test.go
+++ server/api/login_test.go
@@ -13,9 +13,9 @@ import (
 
 	"go.woodpecker-ci.org/woodpecker/v2/server"
 	"go.woodpecker-ci.org/woodpecker/v2/server/api"
+
 	mocks_forge "go.woodpecker-ci.org/woodpecker/v2/server/forge/mocks"
-	"go.woodpecker-ci.org/woodpecker/v2/server/model"
-	mocks_manager "go.woodpecker-ci.org/woodpecker/v2/server/services/mocks"
+	mocks_services "go.woodpecker-ci.org/woodpecker/v2/server/services/mocks"
 	mocks_store "go.woodpecker-ci.org/woodpecker/v2/server/store/mocks"
 )
 
@@ -24,44 +24,86 @@ func TestHandleAuth(t *testing.T) {
 
 	g := goblin.Goblin(t)
 	g.Describe("Login", func() {
-		g.It("should redirect to forge login", func() {
-			_manager := mocks_manager.NewManager(t)
-			_forge := mocks_forge.NewForge(t)
-			_store := mocks_store.NewStore(t)
-
-			_forge.On("Login", mock.Anything, mock.Anything).Return(&model.User{}, "", nil)
-			_manager.On("ForgeMain").Return(_forge, nil)
-			server.Config.Services.Manager = _manager
-
-			w := httptest.NewRecorder()
-			c, _ := gin.CreateTestContext(w)
-			c.Set("store", _store)
-
-			api.HandleAuth(c)
-
-			// mockStore.AssertCalled(t, "GetPipelineList", mock.Anything, mock.Anything, mock.Anything)
-			assert.Equal(t, http.StatusSeeOther, c.Writer.Status())
-		})
-
 		g.It("should handle the error", func() {
-			_manager := mocks_manager.NewManager(t)
-			_forge := mocks_forge.NewForge(t)
-			_store := mocks_store.NewStore(t)
-
-			_forge.On("Login", mock.Anything, mock.Anything).Return(&model.User{}, "", nil)
-			_manager.On("ForgeMain").Return(_forge, nil)
-			server.Config.Services.Manager = _manager
-
 			w := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(w)
-			c.Set("store", _store)
-			c.Request.URL, _ = url.Parse("/authorize?error=access_denied")
+			c.Request = &http.Request{
+				Header: make(http.Header),
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/authorize",
+					RawQuery: "error=access_denied",
+				},
+			}
 
 			api.HandleAuth(c)
 
 			// check if we get redirected to /login?error=access_denied
 			assert.Equal(t, http.StatusSeeOther, c.Writer.Status())
 			assert.Equal(t, "/login?error=access_denied", c.Writer.Header().Get("Location"))
+		})
+
+		g.It("should fail if a code was provided, but no state", func() {
+			// TODO
+		})
+
+		g.It("should fail if a code was provided, but the state is wrong", func() {
+			// TODO
+		})
+
+		g.It("should fail if a code was provided, but the state is wrong", func() {
+			// TODO
+		})
+
+		g.It("should redirect to forge login page", func() {
+			_manager := mocks_services.NewManager(t)
+			_forge := mocks_forge.NewForge(t)
+			_store := mocks_store.NewStore(t)
+
+			forgeRedirectURL := "https://my-awesome-forge.com/oauth/authorize?client_id=client-id"
+
+			_forge.On("Login", mock.Anything, mock.Anything).Return(nil, forgeRedirectURL, nil)
+			_manager.On("ForgeMain").Return(_forge, nil)
+			server.Config.Services.Manager = _manager
+
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			c.Set("store", _store)
+			c.Request = &http.Request{
+				Header: make(http.Header),
+			}
+
+			api.HandleAuth(c)
+
+			assert.Equal(t, http.StatusSeeOther, c.Writer.Status())
+			assert.Equal(t, forgeRedirectURL, c.Writer.Header().Get("Location"))
+		})
+
+		g.It("should handle the callback and register a new user", func() {
+			_manager := mocks_services.NewManager(t)
+			_forge := mocks_forge.NewForge(t)
+			_store := mocks_store.NewStore(t)
+
+			_forge.On("Callback", mock.Anything, mock.Anything).Return(nil, nil)
+			_manager.On("ForgeMain").Return(_forge, nil)
+			server.Config.Services.Manager = _manager
+
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			c.Set("store", _store)
+			c.Request = &http.Request{
+				Header: make(http.Header),
+				Method: http.MethodGet,
+				URL:    &url.URL{},
+			}
+		})
+
+		g.It("should handle the callback and login an existing user", func() {
+			// TODO: implement
+		})
+
+		g.It("should handle the callback and deny a new user to register", func() {
+			// TODO: implement
 		})
 	})
 }
