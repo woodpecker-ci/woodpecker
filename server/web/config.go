fbb96ff8f583167abed072641660a4c1cbdb8c8e
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-06-20 13:08:54 +0200

    Add api for forges (#3733)

--- server/web/config.go
+++ server/web/config.go
@@ -39,20 +39,11 @@ func Config(c *gin.Context) {
 		csrf, _ = t.Sign(user.Hash)
 	}
 
-	// TODO: remove this and use the forge type from the corresponding repo
-	mainForge, err := server.Config.Services.Manager.ForgeMain()
-	if err != nil {
-		log.Error().Err(err).Msg("could not get main forge")
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-
 	configData := map[string]any{
 		"user":               user,
 		"csrf":               csrf,
 		"version":            version.String(),
 		"skip_version_check": server.Config.WebUI.SkipVersionCheck,
-		"forge":              mainForge.Name(),
 		"root_path":          server.Config.Server.RootPath,
 		"enable_swagger":     server.Config.WebUI.EnableSwagger,
 	}
@@ -85,7 +76,6 @@ const configTemplate = `
 window.WOODPECKER_USER = {{ json .user }};
 window.WOODPECKER_CSRF = "{{ .csrf }}";
 window.WOODPECKER_VERSION = "{{ .version }}";
-window.WOODPECKER_FORGE = "{{ .forge }}";
 window.WOODPECKER_ROOT_PATH = "{{ .root_path }}";
 window.WOODPECKER_ENABLE_SWAGGER = {{ .enable_swagger }};
 window.WOODPECKER_SKIP_VERSION_CHECK = {{ .skip_version_check }}
