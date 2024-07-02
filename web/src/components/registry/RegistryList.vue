7c01d274d2f8bba99fa8c8f38ef3dd0fb316b02d
Lauris BH <lauris@nix.lv>
2024-07-03 01:29:08 +0300

    Refactor registry list and edit form to components

---
+++ web/src/components/registry/RegistryList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="space-y-4 text-wp-text-100">
+    <ListItem
+      v-for="registry in registries"
+      :key="registry.id"
+      class="items-center !bg-wp-background-200 !dark:bg-wp-background-100"
+    >
+      <span>{{ registry.address }}</span>
+      <IconButton
+        icon="edit"
+        class="ml-auto w-8 h-8"
+        :title="$t('registries.edit')"
+        @click="editRegistry(registry)"
+      />
+      <IconButton
+        icon="trash"
+        class="w-8 h-8 hover:text-wp-control-error-100"
+        :is-loading="isDeleting"
+        :title="$t('registries.delete')"
+        @click="deleteRegistry(registry)"
+      />
+    </ListItem>
+
+    <div v-if="registries?.length === 0" class="ml-2">{{ $t('registries.none') }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toRef } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import IconButton from '~/components/atomic/IconButton.vue';
+import ListItem from '~/components/atomic/ListItem.vue';
+import type { Registry } from '~/lib/api/types';
+
+const props = defineProps<{
+  modelValue: (Registry & { edit?: boolean })[];
+  isDeleting: boolean;
+}>();
+
+const emit = defineEmits<{
+  (event: 'edit', registry: Registry): void;
+  (event: 'delete', registry: Registry): void;
+}>();
+
+const i18n = useI18n();
+
+const registries = toRef(props, 'modelValue');
+
+function editRegistry(registry: Registry) {
+  emit('edit', registry);
+}
+
+function deleteRegistry(registry: Registry) {
+  // TODO: use proper dialog
+  // eslint-disable-next-line no-alert
+  if (!confirm(i18n.t('registries.delete_confirm'))) {
+    return;
+  }
+  emit('delete', registry);
+}
+</script>
