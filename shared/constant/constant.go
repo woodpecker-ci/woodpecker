3995ebd5145ee8e8680d6965efb3b125ca42b178
Patrick Schratz <patrick.schratz@gmail.com>
2024-07-13 14:30:44 +0200

    Update constant.go

--- shared/constant/constant.go
+++ shared/constant/constant.go
@@ -34,7 +34,7 @@ var DefaultConfigOrder = [...]string{
 const (
 	// DefaultCloneImage can be changed by 'WOODPECKER_DEFAULT_CLONE_IMAGE' at runtime.
 	// renovate: datasource=docker depName=woodpeckerci/plugin-git
-	DefaultCloneImage = "docker.io/woodpeckerci/plugin-git:2.5.0"
+	DefaultCloneImage = "docker.io/woodpeckerci/plugin-git:2.5.1"
 )
 
 var TrustedCloneImages = []string{
