67bb314b286e67cd6c9aee638f240a545a738cd9
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-09-25 07:08:52 +0200

    Update cli/exec/flags.go

--- cli/exec/flags.go
+++ cli/exec/flags.go
@@ -35,7 +35,7 @@ var flags = []cli.Flag{
 	&cli.StringFlag{
 		Sources: cli.EnvVars("WOODPECKER_METADATA_FILE"),
 		Name:    "metadata-file",
-		Usage:   "path to metadata file who has stored the pipeline environment to emulate (can be downloaded from an existing pipeline), infos can be overwritten.",
+		Usage:   "path to pipeline metadata file (normally downloaded from UI). Parameters can be adjusted by applying additional cli flags",
 	},
 	&cli.DurationFlag{
 		Sources: cli.EnvVars("WOODPECKER_TIMEOUT"),
