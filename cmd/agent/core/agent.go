972e45daafaabc3448990c70265ca13b9f95179b
6543 <6543@obermui.de>
2024-10-03 14:02:40 +0100

    Print agent labels in debug mode (#4155) (#4156)

--- cmd/agent/core/agent.go
+++ cmd/agent/core/agent.go
@@ -238,6 +238,8 @@ func run(ctx context.Context, c *cli.Command, backends []types.Backend) error {
 		return err
 	}
 
+	log.Debug().Any("labels", labels).Msgf("agent configured with labels")
+
 	filter := rpc.Filter{
 		Labels: labels,
 	}
