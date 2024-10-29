42f0d68ddb1db6212b6ba5955bec55fa96cf8ae0
Patrick Schratz <patrick.schratz@gmail.com>
2024-10-29 15:50:17 +0100

    correct spelling

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -167,7 +167,7 @@ var flags = append([]cli.Flag{
 	&cli.StringSliceFlag{
 		Sources: cli.EnvVars("WOODPECKER_PLUGINS_TRUSTED_CLONE"),
 		Name:    "plugins-trusted-clone",
-		Usage:   "Plugins witch are trusted to handle the netrc info in clone steps",
+		Usage:   "Plugins which are trusted to handle the netrc info in clone steps",
 		Value:   constant.TrustedClonePlugins,
 	},
 	&cli.StringSliceFlag{
