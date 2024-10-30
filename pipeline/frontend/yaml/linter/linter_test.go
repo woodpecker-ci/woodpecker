383bfbb6de27ae01c76d7869728d36028aee58f2
Robert Kaussow <mail@thegeeklab.de>
2024-10-30 22:14:12 +0100

    Fix wording for privileged plugins linter error (#4280)

--- pipeline/frontend/yaml/linter/linter_test.go
+++ pipeline/frontend/yaml/linter/linter_test.go
@@ -167,7 +167,7 @@ func TestLintErrors(t *testing.T) {
 		},
 		{
 			from: "{steps: { build: { image: plugins/docker, settings: { test: 'true' } } }, when: { branch: main, event: push } } }",
-			want: "Cannot use once by default privileged plugin 'plugins/docker', if needed add it too WOODPECKER_PLUGINS_PRIVILEGED",
+			want: "The formerly privileged plugin 'plugins/docker' is no longer privileged by default, if required, add it to WOODPECKER_PLUGINS_PRIVILEGED",
 		},
 		{
 			from: "{steps: { build: { image: golang, settings: { test: 'true' } } }, when: { branch: main, event: push }, clone: { git: { image: some-other/plugin-git:v1.1.0 } } }",
