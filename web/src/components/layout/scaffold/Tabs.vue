e6bfeb745d8893c307185bd932bfd015e92ff505
qwerty287 <qwerty287@posteo.de>
2024-11-18 17:46:28 +0200

    fix chevron

--- web/src/components/layout/scaffold/Tabs.vue
+++ web/src/components/layout/scaffold/Tabs.vue
@@ -3,13 +3,13 @@
     <router-link
       v-for="tab in tabs"
       :key="tab.title"
-      v-slot="{ isActive }"
+      v-slot="{ isActive, isExactActive }"
       :to="tab.to"
       class="border-transparent w-full py-1 md:py-2 md:w-auto md:px-6 flex cursor-pointer md:border-b-2 text-wp-text-100 hover:text-wp-text-200 items-center"
       :active-class="tab.matchChildren ? '!border-wp-text-100' : ''"
       :exact-active-class="tab.matchChildren ? '' : '!border-wp-text-100'"
     >
-      <Icon v-if="isActive" name="chevron-right" class="md:hidden" />
+      <Icon v-if="isExactActive || (isActive && tab.matchChildren)" name="chevron-right" class="md:hidden" />
       <Icon v-else name="blank" class="md:hidden" />
       <span class="flex gap-2 items-center flex-row-reverse md:flex-row">
         <Icon v-if="tab.icon" :name="tab.icon" :class="tab.iconClass" />
