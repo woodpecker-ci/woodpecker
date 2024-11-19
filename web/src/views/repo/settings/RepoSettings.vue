879c1e2d7d3fdfc298706ff3ea6630a22af55e5c
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2024-11-19 15:04:50 +0200

    Use separate routes instead of anchors (#4285)
    Co-authored-by: Anbraten <anton@ju60.de>

--- web/src/views/repo/settings/RepoSettings.vue
+++ web/src/views/repo/settings/RepoSettings.vue
@@ -16,24 +16,14 @@
       </span>
     </template>
 
-    <Tab id="general" :title="$t('repo.settings.general.general')">
-      <GeneralTab />
-    </Tab>
-    <Tab id="secrets" :title="$t('secrets.secrets')">
-      <SecretsTab />
-    </Tab>
-    <Tab id="registries" :title="$t('registries.registries')">
-      <RegistriesTab />
-    </Tab>
-    <Tab id="crons" :title="$t('repo.settings.crons.crons')">
-      <CronTab />
-    </Tab>
-    <Tab id="badge" :title="$t('repo.settings.badge.badge')">
-      <BadgeTab />
-    </Tab>
-    <Tab id="actions" :title="$t('repo.settings.actions.actions')">
-      <ActionsTab />
-    </Tab>
+    <Tab :to="{ name: 'repo-settings' }" :title="$t('repo.settings.general.general')" />
+    <Tab :to="{ name: 'repo-settings-secrets' }" :title="$t('secrets.secrets')" />
+    <Tab :to="{ name: 'repo-settings-registries' }" :title="$t('registries.registries')" />
+    <Tab :to="{ name: 'repo-settings-crons' }" :title="$t('repo.settings.crons.crons')" />
+    <Tab :to="{ name: 'repo-settings-badge' }" :title="$t('repo.settings.badge.badge')" />
+    <Tab :to="{ name: 'repo-settings-actions' }" :title="$t('repo.settings.actions.actions')" />
+
+    <router-view />
   </Scaffold>
 </template>
 
@@ -44,12 +34,6 @@ import { useRouter } from 'vue-router';
 
 import Scaffold from '~/components/layout/scaffold/Scaffold.vue';
 import Tab from '~/components/layout/scaffold/Tab.vue';
-import ActionsTab from '~/components/repo/settings/ActionsTab.vue';
-import BadgeTab from '~/components/repo/settings/BadgeTab.vue';
-import CronTab from '~/components/repo/settings/CronTab.vue';
-import GeneralTab from '~/components/repo/settings/GeneralTab.vue';
-import RegistriesTab from '~/components/repo/settings/RegistriesTab.vue';
-import SecretsTab from '~/components/repo/settings/SecretsTab.vue';
 import useNotifications from '~/compositions/useNotifications';
 import { useRouteBack } from '~/compositions/useRouteBack';
 import type { Repo, RepoPermissions } from '~/lib/api/types';
