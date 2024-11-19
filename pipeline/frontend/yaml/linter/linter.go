6caa9b48a64c9ad87115216b172713da21bc085c
6543 <6543@obermui.de>
2024-11-19 16:09:14 +0100

    Update pipeline/frontend/yaml/linter/linter.go
    Co-authored-by: qwerty287 <80460567+qwerty287@users.noreply.github.com>

--- pipeline/frontend/yaml/linter/linter.go
+++ pipeline/frontend/yaml/linter/linter.go
@@ -209,7 +209,7 @@ func (l *Linter) lintContainerDeprecations(config *WorkflowConfig, c *types.Cont
 	if len(c.Secrets) != 0 {
 		err = multierr.Append(err, &errorTypes.PipelineError{
 			Type:    errorTypes.PipelineErrorTypeDeprecation,
-			Message: "Secrets are un-supported now, use environment with from_secret",
+			Message: "Secrets are unsupported now, use environment with from_secret",
 			Data: errors.DeprecationErrorData{
 				File:  config.File,
 				Field: fmt.Sprintf("%s.%s.secrets", field, c.Name),
