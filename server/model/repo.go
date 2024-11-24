5d5f092b9e9a382471108404c384102ceffeb8af
qwerty287 <qwerty287@posteo.de>
2024-11-24 16:43:37 +0200

    fmt

--- server/model/repo.go
+++ server/model/repo.go
@@ -71,7 +71,7 @@ type Repo struct {
 	Hash                         string               `json:"-"                               xorm:"varchar(500) 'hash'"`
 	Perm                         *Perm                `json:"-"                               xorm:"-"`
 	CancelPreviousPipelineEvents []WebhookEvent       `json:"cancel_previous_pipeline_events" xorm:"json 'cancel_previous_pipeline_events'"`
-	NetrcTrustedPlugins                 []string             `json:"netrc_trusted" xorm:"json 'netrc_trusted'"`
+	NetrcTrustedPlugins          []string             `json:"netrc_trusted" xorm:"json 'netrc_trusted'"`
 } //	@name Repo
 
 // TableName return database table name for xorm.
