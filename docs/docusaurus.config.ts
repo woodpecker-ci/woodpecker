0303ccf58527adb248410dad63789852e5087a93
6543 <6543@obermui.de>
2024-11-27 15:57:59 +0100

    prettier2

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -238,7 +238,9 @@ const config: Config = {
           includeCurrentVersion: true,
           lastVersion: '2.7',
           onlyIncludeVersions:
-            process.env.NODE_ENV === 'development' ? ['current', '2.8'] : ['current', '2.8', '2.7', '2.6', '2.5', '1.0'],
+            process.env.NODE_ENV === 'development'
+              ? ['current', '2.8']
+              : ['current', '2.8', '2.7', '2.6', '2.5', '1.0'],
           versions: {
             current: {
               label: 'Next 🚧',
