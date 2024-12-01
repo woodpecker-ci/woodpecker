5f047485b90d54ed28ae55f70cf5a7735480e416
6543 <6543@obermui.de>
2024-12-02 00:54:17 +0100

    Update server/api/repo.go
    Co-authored-by: Patrick Schratz <patrick.schratz@gmail.com>

--- server/api/repo.go
+++ server/api/repo.go
@@ -260,7 +260,7 @@ func PatchRepo(c *gin.Context) {
 			return
 		}
 	} else if in.IsGated != nil {
-		c.String(http.StatusBadRequest, "'gated' option was used, use 'require_approval'")
+		c.String(http.StatusBadRequest, "'gated' option has been set in version 2.8, use 'require-approval' in >= 3.0")
 		return
 	}
 	if in.Timeout != nil {
