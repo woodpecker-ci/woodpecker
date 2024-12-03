a7f37c71d67612d146fb488d544f3f0b5af90390
6543 <6543@obermui.de>
2024-12-03 17:53:09 +0100

    frontport

--- server/store/datastore/migration/019_gated_to_require_approval.go
+++ server/store/datastore/migration/019_gated_to_require_approval.go
@@ -26,8 +26,8 @@ var gatedToRequireApproval = xormigrate.Migration{
 	ID: "gated-to-require-approval",
 	MigrateSession: func(sess *xorm.Session) (err error) {
 		const (
-			RequireApprovalOldNotGated string = "old_not_gated"
-			RequireApprovalAllEvents   string = "all_events"
+			requireApprovalOldNotGated string = "old_not_gated"
+			requireApprovalAllEvents   string = "all_events"
 		)
 
 		type repos struct {
@@ -43,15 +43,15 @@ var gatedToRequireApproval = xormigrate.Migration{
 
 		// migrate gated repos
 		if _, err := sess.Exec(
-			builder.Update(builder.Eq{"require_approval": RequireApprovalAllEvents}).
+			builder.Update(builder.Eq{"require_approval": requireApprovalAllEvents}).
 				From("repos").
 				Where(builder.Eq{"gated": true})); err != nil {
 			return err
 		}
 
-		// migrate non gated to not set to migrate to new defaults with next migration
+		// migrate non gated repos to old_not_gated (no approval required)
 		if _, err := sess.Exec(
-			builder.Update(builder.Eq{"require_approval": RequireApprovalOldNotGated}).
+			builder.Update(builder.Eq{"require_approval": requireApprovalOldNotGated}).
 				From("repos").
 				Where(builder.Eq{"gated": false})); err != nil {
 			return err
