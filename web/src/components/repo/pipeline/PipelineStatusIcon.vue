828cdfcbc3f310cfc5e6d4a9403b1df9899693bc
pat-s <patrick.schratz@gmail.com>
2024-12-16 11:35:51 +0100

    chore: increase status icon size

--- web/src/components/repo/pipeline/PipelineStatusIcon.vue
+++ web/src/components/repo/pipeline/PipelineStatusIcon.vue
@@ -5,7 +5,7 @@
   >
     <Icon
       :name="service ? 'settings' : `status-${status}`"
-      size="1em"
+      size="1.5em"
       :class="{
         'text-wp-state-error-100': pipelineStatusColors[status] === 'red',
         'text-wp-state-neutral-100': pipelineStatusColors[status] === 'gray',
