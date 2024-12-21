b8918a8bf11f29c0ff1429f86915aac85f2eae84
pat-s <patrick.schratz@gmail.com>
2024-12-21 14:17:39 +0100

    adjust code-box colors

--- web/src/components/atomic/CountBadge.vue
+++ web/src/components/atomic/CountBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="inline-block bg-wp-background-400 dark:bg-wp-background-200 px-1.5 py-0.5 rounded-full min-w-5 font-bold text-center text-wp-text-100 text-xs leading-4"
+    class="inline-block bg-wp-code-100 dark:bg-wp-background-200 px-1.5 py-0.5 rounded-full min-w-5 font-bold text-center text-wp-text-100 text-xs leading-4"
   >
     {{ value }}
   </span>
