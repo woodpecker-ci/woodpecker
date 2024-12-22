0ac10862fea46f3dcb85f48edfb61c8e0a3acc9c
pat-s <patrick.schratz@gmail.com>
2024-12-22 09:46:17 +0100

    add darker bg color for hover

--- web/src/components/atomic/CountBadge.vue
+++ web/src/components/atomic/CountBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="bg-wp-code-inline-100 text-wp-text-code-inline-100 inline-block px-1.5 py-0.5 rounded-full min-w-5 font-bold text-center text-xs leading-4"
+    class="bg-wp-code-inline-100 text-wp-text-code-inline-100 hover:bg-wp-code-inline-200 inline-block px-1.5 py-0.5 rounded-full min-w-5 font-bold text-center text-xs leading-4"
   >
     {{ value }}
   </span>
