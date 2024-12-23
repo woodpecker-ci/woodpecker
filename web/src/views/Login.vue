76c5952ca7c33d20e3095d26b1d45c0c5577cc6d
pat-s <patrick.schratz@gmail.com>
2024-12-23 22:58:17 +0100

    fix login div

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,26 +1,26 @@
 <template>
-  <main class="flex flex-col w-full h-full justify-center items-center">
+  <main class="flex flex-col justify-center items-center w-full h-full">
     <Error v-if="errorMessage" class="w-full md:w-3xl">
       <span class="whitespace-pre">{{ errorMessage }}</span>
-      <span v-if="errorDescription" class="whitespace-pre mt-1">{{ errorDescription }}</span>
+      <span v-if="errorDescription" class="mt-1 whitespace-pre">{{ errorDescription }}</span>
       <a
         v-if="errorUri"
         :href="errorUri"
         target="_blank"
-        class="text-wp-link-100 hover:text-wp-link-200 cursor-pointer mt-1"
+        class="mt-1 text-wp-link-100 hover:text-wp-link-200 cursor-pointer"
       >
         <span>{{ errorUri }}</span>
       </a>
     </Error>
 
     <div
-      class="flex flex-col w-full overflow-hidden bg-wp-background-100 shadow border border-wp-background-400 dark:bg-wp-background-200 md:m-8 md:rounded-md md:flex-row md:w-3xl md:h-sm"
+      class="flex md:flex-row flex-col border-wp-background-400 bg-wp-background-100 dark:bg-wp-background-200 shadow md:m-8 border md:rounded-md w-full md:max-w-3xl md:h-96 overflow-hidden"
     >
-      <div class="flex justify-center items-center bg-wp-primary-200 dark:bg-wp-primary-300 min-h-48 md:w-3/5">
-        <WoodpeckerLogo preserveAspectRatio="xMinYMin slice" class="w-30 h-30 md:w-48 md:h-48" />
+      <div class="flex justify-center items-center bg-wp-primary-200 dark:bg-wp-primary-300 md:w-3/5 min-h-48">
+        <WoodpeckerLogo preserveAspectRatio="xMinYMin slice" class="w-30 md:w-48 h-30 md:h-48" />
       </div>
-      <div class="flex justify-center items-center flex-col md:w-2/5 min-h-48 gap-4 text-center">
-        <h1 class="text-xl text-wp-text-100">{{ $t('welcome') }}</h1>
+      <div class="flex flex-col justify-center items-center gap-4 md:w-2/5 min-h-48 text-center">
+        <h1 class="text-wp-text-100 text-xl">{{ $t('welcome') }}</h1>
         <div class="flex flex-col gap-2">
           <Button
             v-for="forge in forges"
