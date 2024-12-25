7cd4b910b0308a7f6f1a9d761b7e77456edbe4b5
pat-s <patrick.schratz@gmail.com>
2024-12-25 22:43:41 +0100

    wrap

--- web/windi.config.ts
+++ web/windi.config.ts
@@ -82,14 +82,14 @@ export default defineConfig({
           300: tinycolor(customColors['wp-primary'][600]).desaturate(10).toString(),
         },
         'int-wp-control-error': {
-          100: customColors['wp-state-error'][100].desaturate(25).toString(),
+          100: tinycolor(customColors['wp-state-error'][100]).desaturate(25).toString(),
           200: colors.red[600],
           300: colors.red[700],
           400: colors.red[800],
           500: colors.red[900],
         },
         'int-wp-control-error-dark': {
-          100: customColors['wp-state-error'][100].desaturate(25).toString(),
+          100: tinycolor(customColors['wp-state-error'][100]).desaturate(25).toString(),
           200: tinycolor(colors.red[600]).desaturate(25).toString(),
           300: tinycolor(colors.red[700]).desaturate(25).toString(),
           400: tinycolor(colors.red[800]).desaturate(25).toString(),
@@ -97,7 +97,7 @@ export default defineConfig({
         },
 
         'int-wp-state-error': {
-          100: customColors['wp-state-error'][100].desaturate(25).toString(),
+          100: tinycolor(customColors['wp-state-error'][100]).desaturate(25).toString(),
         },
         'int-wp-state-neutral': {
           100: colors.gray[600],
