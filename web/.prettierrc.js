f140beb1e930878eaceef2b2d4b75b5a4d431fc7
pat-s <patrick.schratz@gmail.com>
2024-12-26 22:33:05 +0100

    remove unused config options

--- web/.prettierrc.js
+++ web/.prettierrc.js
@@ -6,11 +6,4 @@ const config = JSON.parse(await readFile(new URL('../.prettierrc.json', import.m
 export default {
   ...config,
   plugins: ['prettier-plugin-tailwindcss'],
-  importOrder: [
-    '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
-    '', // Empty string will match any import not matched by other special words or groups.
-    '^(#|@|~|\\$)(/.*)$',
-    '',
-    '^[./]',
-  ],
 };
