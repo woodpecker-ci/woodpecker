ac35601ba051d2283e24093c8175df8d3647efd5
pat-s <patrick.schratz@gmail.com>
2024-12-27 00:06:26 +0100

    prettier

--- web/src/components/atomic/Button.vue
+++ web/src/components/atomic/Button.vue
@@ -8,7 +8,8 @@
         color === 'gray',
       'bg-wp-control-ok-100 hover:bg-wp-control-ok-200 border-wp-control-ok-300 text-white': color === 'green',
       'bg-wp-control-info-100 hover:bg-wp-control-info-200 border-wp-control-info-300 text-white': color === 'blue',
-      'bg-wp-state-error-100 dark:bg-wp-state-error-200 hover:bg-wp-state-error-300 border-wp-state-error-300 text-white': color === 'red',
+      'bg-wp-state-error-100 dark:bg-wp-state-error-200 hover:bg-wp-state-error-300 border-wp-state-error-300 text-white':
+        color === 'red',
       ...passedClasses,
     }"
     :title="title"
