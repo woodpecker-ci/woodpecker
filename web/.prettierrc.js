356cc04cf2dcc9f62c6b8727f3b34225227cb8b9
pat-s <patrick.schratz@gmail.com>
2024-12-26 22:29:19 +0100

    add prettier tailwind sort plugin

--- web/.prettierrc.js
+++ web/.prettierrc.js
@@ -5,7 +5,7 @@ const config = JSON.parse(await readFile(new URL('../.prettierrc.json', import.m
 
 export default {
   ...config,
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  plugins: ['prettier-plugin-tailwindcss'],
   importOrder: [
     '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
     '', // Empty string will match any import not matched by other special words or groups.
