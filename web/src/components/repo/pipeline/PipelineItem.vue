60e9767cbdaeb53da9fe12e932a6d6280afcef71
qwerty287 <qwerty287@posteo.de>
2024-12-27 17:56:19 +0200

    add number back

--- web/src/components/repo/pipeline/PipelineItem.vue
+++ web/src/components/repo/pipeline/PipelineItem.vue
@@ -1,82 +1,85 @@
 <template>
   <ListItem v-if="pipeline" class="p-0 w-full">
-      <div class="flex items-center col-span-4">
-        <div
-          class="h-full w-2"
-          :class="{
-            'bg-wp-state-warn-100': pipeline.status === 'pending',
-            'bg-wp-state-error-100': pipelineStatusColors[pipeline.status] === 'red',
-            'bg-wp-state-neutral-100': pipelineStatusColors[pipeline.status] === 'gray',
-            'bg-wp-state-ok-100': pipelineStatusColors[pipeline.status] === 'green',
-            'bg-wp-state-info-100': pipelineStatusColors[pipeline.status] === 'blue',
-          }"
-        />
-        <div class="w-12 flex justify-center items-center h-full">
-          <PipelineRunningIcon v-if="pipeline.status === 'started' || pipeline.status === 'running'" />
-          <PipelineStatusIcon v-else :status="pipeline.status" />
+    <div class="flex items-center col-span-4">
+      <div
+        class="h-full w-2"
+        :class="{
+          'bg-wp-state-warn-100': pipeline.status === 'pending',
+          'bg-wp-state-error-100': pipelineStatusColors[pipeline.status] === 'red',
+          'bg-wp-state-neutral-100': pipelineStatusColors[pipeline.status] === 'gray',
+          'bg-wp-state-ok-100': pipelineStatusColors[pipeline.status] === 'green',
+          'bg-wp-state-info-100': pipelineStatusColors[pipeline.status] === 'blue',
+        }"
+      />
+      <div class="w-12 flex justify-center items-center h-full">
+        <PipelineRunningIcon v-if="pipeline.status === 'started' || pipeline.status === 'running'" />
+        <PipelineStatusIcon v-else :status="pipeline.status" />
+      </div>
+
+      <div class="flex px-2 mr-2 flex-grow min-w-0 <md:flex-wrap w-full flex-col items-center min-w-0 py-4 gap-2">
+        <div class="w-full flex items-center gap-2">
+          <span :title="pipelineEventTitle" class="text-wp-text-100">
+            <Icon v-if="pipeline.event === 'pull_request'" name="pull-request" />
+            <Icon v-else-if="pipeline.event === 'pull_request_closed'" name="pull-request-closed" />
+            <Icon v-else-if="pipeline.event === 'deployment'" name="deployment" />
+            <Icon v-else-if="pipeline.event === 'tag' || pipeline.event === 'release'" name="tag" />
+            <Icon v-else-if="pipeline.event === 'cron'" name="push" />
+            <Icon v-else-if="pipeline.event === 'manual'" name="manual-pipeline" />
+            <Icon v-else name="push" />
+          </span>
+          <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
+          <span class="text-wp-text-alt-100 text-lg">#{{ pipeline.number }}</span>
+          <span class="text-wp-text-100 text-lg whitespace-nowrap overflow-hidden overflow-ellipsis" :title="message">
+            {{ shortMessage }}
+          </span>
         </div>
 
-        <div class="flex py-2 px-2 mr-2 flex-grow min-w-0 <md:flex-wrap">
-          <div class="w-full md:w-auto flex flex-col items-center min-w-0 py-2 gap-2">
-            <div class="w-full flex items-center gap-2">
-              <span :title="pipelineEventTitle" class="text-wp-text-100">
-                <Icon v-if="pipeline.event === 'pull_request'" name="pull-request" />
-                <Icon v-else-if="pipeline.event === 'pull_request_closed'" name="pull-request-closed" />
-                <Icon v-else-if="pipeline.event === 'deployment'" name="deployment" />
-                <Icon v-else-if="pipeline.event === 'tag' || pipeline.event === 'release'" name="tag" />
-                <Icon v-else-if="pipeline.event === 'cron'" name="push" />
-                <Icon v-else-if="pipeline.event === 'manual'" name="manual-pipeline" />
-                <Icon v-else name="push" />
+        <div class="flex w-full gap-2">
+          <div class="flex items-center w-22 text-wp-text-100">
+            <Icon name="commit" />
+            <span class="truncate">{{ pipeline.commit.slice(0, 7) }}</span>
+          </div>
+
+          <div class="flex items-center min-w-0 text-wp-text-100">
+            <Icon
+              v-if="pipeline.event === 'pull_request' || pipeline.event === 'pull_request_closed'"
+              name="pull-request"
+            />
+            <Icon v-else-if="pipeline.event === 'tag' || pipeline.event === 'release'" name="tag" />
+            <Icon v-else name="push" />
+
+            <span class="ml-1 truncate">
+              {{ prettyRef }}
+              <!-- eslint-disable @intlify/vue-i18n/no-raw-text-->
+              <span
+                v-if="pipeline.event === 'pull_request' || pipeline.event === 'pull_request_closed'"
+                :title="prTitleWithDescription"
+              >
+                ({{ prTitle }})
               </span>
-              <span class="text-wp-text-100 text-lg whitespace-nowrap overflow-hidden overflow-ellipsis" :title="message">
-                {{ shortMessage }}
-              </span>
-            </div>
-
-            <div class="flex w-full gap-2">
-              <div class="flex items-center w-22 text-wp-text-100">
-                <Icon name="commit" />
-                <span class="truncate">{{ pipeline.commit.slice(0, 7) }}</span>
-              </div>
-
-              <div class="flex items-center min-w-0 text-wp-text-100">
-                <Icon v-if="pipeline.event === 'pull_request' || pipeline.event === 'pull_request_closed'" name="pull-request" />
-                <Icon v-else-if="pipeline.event === 'tag' || pipeline.event === 'release'" name="tag" />
-                <Icon v-else name="push" />
-
-                <span class="ml-1 truncate">
-                  {{ prettyRef }}
-                  <!-- eslint-disable @intlify/vue-i18n/no-raw-text-->
-                  <span
-                    v-if="pipeline.event === 'pull_request' || pipeline.event === 'pull_request_closed'"
-                    :title="prTitleWithDescription"
-                  >
-                    ({{ prTitle }})
-                  </span>
-                  <!-- eslint-enable @intlify/vue-i18n/no-raw-text-->
-                </span>
-              </div>
-            </div>
+              <!-- eslint-enable @intlify/vue-i18n/no-raw-text-->
+            </span>
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="<md:hidden flex ml-auto mr-6 items-center">
-        <div class="flex flex-col w-full w-48 py-2 gap-x-4 gap-y-2 flex-shrink-0 text-wp-text-100 justify-center h-full">
-          <div class="flex space-x-2 min-w-0" :title="$t('repo.pipeline.duration')">
-            <Icon name="duration" />
-            <span class="truncate">{{ duration }}</span>
-          </div>
-
-          <div class="flex space-x-2 min-w-0" :title="$t('repo.pipeline.created', { created })">
-            <Icon name="since" />
-            <span class="truncate">{{ since }}</span>
-          </div>
+    <div class="<md:hidden flex ml-auto mr-6 items-center">
+      <div class="flex flex-col w-full w-48 py-2 gap-x-4 gap-y-2 flex-shrink-0 text-wp-text-100 justify-center h-full">
+        <div class="flex space-x-2 min-w-0" :title="$t('repo.pipeline.duration')">
+          <Icon name="duration" />
+          <span class="truncate">{{ duration }}</span>
         </div>
 
-        <Icon v-if="pipeline.event === 'cron'" name="stopwatch" class="text-wp-text-100 w-8" />
-        <img v-else class="rounded-md w-8" :src="pipeline.author_avatar" :title="pipeline.author" />
+        <div class="flex space-x-2 min-w-0" :title="$t('repo.pipeline.created', { created })">
+          <Icon name="since" />
+          <span class="truncate">{{ since }}</span>
+        </div>
       </div>
+
+      <Icon v-if="pipeline.event === 'cron'" name="stopwatch" class="text-wp-text-100 w-8" />
+      <img v-else class="rounded-md w-8" :src="pipeline.author_avatar" :title="pipeline.author" />
+    </div>
   </ListItem>
 </template>
 
