3e681f462cab4d91af6cdf8bfcacab9490fa910a
qwerty287 <qwerty287@posteo.de>
2024-12-27 12:49:23 +0200

    start with migration

--- server/forge/bitbucketdatacenter/convert_test.go
+++ server/forge/bitbucketdatacenter/convert_test.go
@@ -18,289 +18,299 @@ import (
 	"testing"
 	"time"
 
-	"github.com/franela/goblin"
 	bb "github.com/neticdk/go-bitbucket/bitbucket"
+	"github.com/stretchr/testify/assert"
 
 	"go.woodpecker-ci.org/woodpecker/v3/server/model"
 )
 
-//nolint:misspell
-func TestHelper(t *testing.T) {
-	g := goblin.Goblin(t)
-	g.Describe("Bitbucket Server converter", func() {
-		g.It("should convert status", func() {
-			tests := []struct {
-				from model.StatusValue
-				to   bb.BuildStatusState
-			}{
-				{
-					from: model.StatusPending,
-					to:   bb.BuildStatusStateInProgress,
-				},
-				{
-					from: model.StatusRunning,
-					to:   bb.BuildStatusStateInProgress,
-				},
-				{
-					from: model.StatusSuccess,
-					to:   bb.BuildStatusStateSuccessful,
-				},
-				{
-					from: model.StatusValue("other"),
-					to:   bb.BuildStatusStateFailed,
-				},
-			}
-			for _, tt := range tests {
-				to := convertStatus(tt.from)
-				g.Assert(to).Equal(tt.to)
-			}
-		})
+func Test_convertStatus(t *testing.T) {
+	tests := []struct {
+		from model.StatusValue
+		to   bb.BuildStatusState
+	}{
+		{
+			from: model.StatusPending,
+			to:   bb.BuildStatusStateInProgress,
+		},
+		{
+			from: model.StatusRunning,
+			to:   bb.BuildStatusStateInProgress,
+		},
+		{
+			from: model.StatusSuccess,
+			to:   bb.BuildStatusStateSuccessful,
+		},
+		{
+			from: model.StatusValue("other"),
+			to:   bb.BuildStatusStateFailed,
+		},
+	}
+	for _, tt := range tests {
+		to := convertStatus(tt.from)
+		assert.Equal(t, tt.to, to)
+	}
+}
 
-		g.It("should convert repository", func() {
-			from := &bb.Repository{
-				ID:   uint64(1234),
-				Slug: "REPO",
-				Project: &bb.Project{
-					Key: "PRJ",
+func Test_convertRepo(t *testing.T) {
+	from := &bb.Repository{
+		ID:   uint64(1234),
+		Slug: "REPO",
+		Project: &bb.Project{
+			Key: "PRJ",
+		},
+		Links: map[string][]bb.Link{
+			"clone": {
+				{
+					Name: "http",
+					Href: "https://user@git.domain/clone",
 				},
-				Links: map[string][]bb.Link{
-					"clone": {
-						{
-							Name: "http",
-							Href: "https://user@git.domain/clone",
-						},
-					},
-					"self": {
-						{
-							Href: "https://git.domain/self",
-						},
-					},
+			},
+			"self": {
+				{
+					Href: "https://git.domain/self",
 				},
-			}
-			perm := &model.Perm{}
-			to := convertRepo(from, perm, "main")
-			g.Assert(to.ForgeRemoteID).Equal(model.ForgeRemoteID("1234"))
-			g.Assert(to.Name).Equal("REPO")
-			g.Assert(to.Owner).Equal("PRJ")
-			g.Assert(to.Branch).Equal("main")
-			g.Assert(to.SCMKind).Equal(model.RepoGit)
-			g.Assert(to.FullName).Equal("PRJ/REPO")
-			g.Assert(to.Perm).Equal(perm)
-			g.Assert(to.Clone).Equal("https://git.domain/clone")
-		})
+			},
+		},
+	}
+	perm := &model.Perm{}
+	to := convertRepo(from, perm, "main")
 
-		g.It("should convert repository push event", func() {
-			now := time.Now()
-			tests := []struct {
-				from *bb.RepositoryPushEvent
-				to   *model.Pipeline
-			}{
-				{
-					from: &bb.RepositoryPushEvent{},
-					to:   nil,
-				},
-				{
-					from: &bb.RepositoryPushEvent{
-						Changes: []bb.RepositoryPushEventChange{
-							{
-								FromHash: "1234567890abcdef",
-								ToHash:   "0000000000000000000000000000000000000000",
-							},
-						},
-					},
-					to: nil,
-				},
-				{
-					from: &bb.RepositoryPushEvent{
-						Changes: []bb.RepositoryPushEventChange{
-							{
-								FromHash: "0000000000000000000000000000000000000000",
-								ToHash:   "1234567890abcdef",
-								Type:     bb.RepositoryPushEventChangeTypeDelete,
-							},
-						},
-					},
-					to: nil,
-				},
-				{
-					from: &bb.RepositoryPushEvent{
-						Event: bb.Event{
-							Date: bb.ISOTime(now),
-							Actor: bb.User{
-								Name:  "John Doe",
-								Email: "john.doe@mail.com",
-								Slug:  "john.doe_mail.com",
-							},
-						},
-						Repository: bb.Repository{
-							Slug: "REPO",
-							Project: &bb.Project{
-								Key: "PRJ",
-							},
-						},
-						Changes: []bb.RepositoryPushEventChange{
-							{
-								Ref: bb.RepositoryPushEventRef{
-									ID:        "refs/head/branch",
-									DisplayID: "branch",
-								},
-								RefId:  "refs/head/branch",
-								ToHash: "1234567890abcdef",
-							},
-						},
-					},
-					to: &model.Pipeline{
-						Commit:    "1234567890abcdef",
-						Branch:    "branch",
-						Message:   "",
-						Avatar:    "https://base.url/users/john.doe_mail.com/avatar.png",
-						Author:    "John Doe",
-						Email:     "john.doe@mail.com",
-						Timestamp: now.UTC().Unix(),
-						Ref:       "refs/head/branch",
-						ForgeURL:  "https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef",
-						Event:     model.EventPush,
-					},
-				},
-			}
-			for _, tt := range tests {
-				to := convertRepositoryPushEvent(tt.from, "https://base.url")
-				g.Assert(to).Equal(tt.to)
-			}
-		})
+	assert.Equal(t, &model.Repo{
+		ForgeRemoteID: model.ForgeRemoteID("1234"),
+		Name:          "REPO",
+		Owner:         "PRJ",
+		Branch:        "main",
+		SCMKind:       model.RepoGit,
+		FullName:      "PRJ/REPO",
+		Perm:          perm,
+		Clone:         "https://git.domain/clone",
+		ForgeURL:      "https://git.domain/self",
+		PREnabled:     true,
+		IsSCMPrivate:  true,
+	}, to)
+}
 
-		g.It("should convert pull request event", func() {
-			now := time.Now()
-			from := &bb.PullRequestEvent{
+func Test_convertRepositoryPushEvent(t *testing.T) {
+	now := time.Now()
+	tests := []struct {
+		from *bb.RepositoryPushEvent
+		to   *model.Pipeline
+	}{
+		{
+			from: &bb.RepositoryPushEvent{},
+			to:   nil,
+		},
+		{
+			from: &bb.RepositoryPushEvent{
+				Changes: []bb.RepositoryPushEventChange{
+					{
+						FromHash: "1234567890abcdef",
+						ToHash:   "0000000000000000000000000000000000000000",
+					},
+				},
+			},
+			to: nil,
+		},
+		{
+			from: &bb.RepositoryPushEvent{
+				Changes: []bb.RepositoryPushEventChange{
+					{
+						FromHash: "0000000000000000000000000000000000000000",
+						ToHash:   "1234567890abcdef",
+						Type:     bb.RepositoryPushEventChangeTypeDelete,
+					},
+				},
+			},
+			to: nil,
+		},
+		{
+			from: &bb.RepositoryPushEvent{
 				Event: bb.Event{
-					Date:     bb.ISOTime(now),
-					EventKey: bb.EventKeyPullRequestFrom,
+					Date: bb.ISOTime(now),
 					Actor: bb.User{
 						Name:  "John Doe",
 						Email: "john.doe@mail.com",
 						Slug:  "john.doe_mail.com",
 					},
 				},
-				PullRequest: bb.PullRequest{
-					ID:    123,
-					Title: "my title",
-					Source: bb.PullRequestRef{
-						ID:        "refs/head/branch",
-						DisplayID: "branch",
-						Latest:    "1234567890abcdef",
-						Repository: bb.Repository{
-							Slug: "REPO",
-							Project: &bb.Project{
-								Key: "PRJ",
-							},
-						},
-					},
-					Target: bb.PullRequestRef{
-						ID:        "refs/head/main",
-						DisplayID: "main",
-						Latest:    "abcdef1234567890",
-						Repository: bb.Repository{
-							Slug: "REPO",
-							Project: &bb.Project{
-								Key: "PRJ",
-							},
-						},
+				Repository: bb.Repository{
+					Slug: "REPO",
+					Project: &bb.Project{
+						Key: "PRJ",
 					},
 				},
-			}
-			to := convertPullRequestEvent(from, "https://base.url")
-			g.Assert(to.Commit).Equal("1234567890abcdef")
-			g.Assert(to.Branch).Equal("branch")
-			g.Assert(to.Avatar).Equal("https://base.url/users/john.doe_mail.com/avatar.png")
-			g.Assert(to.Author).Equal("John Doe")
-			g.Assert(to.Email).Equal("john.doe@mail.com")
-			g.Assert(to.Timestamp).Equal(now.UTC().Unix())
-			g.Assert(to.Ref).Equal("refs/pull-requests/123/from")
-			g.Assert(to.ForgeURL).Equal("https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef")
-			g.Assert(to.Event).Equal(model.EventPull)
-			g.Assert(to.Refspec).Equal("branch:main")
-		})
+				Changes: []bb.RepositoryPushEventChange{
+					{
+						Ref: bb.RepositoryPushEventRef{
+							ID:        "refs/head/branch",
+							DisplayID: "branch",
+						},
+						RefId:  "refs/head/branch",
+						ToHash: "1234567890abcdef",
+					},
+				},
+			},
+			to: &model.Pipeline{
+				Commit:    "1234567890abcdef",
+				Branch:    "branch",
+				Message:   "",
+				Avatar:    "https://base.url/users/john.doe_mail.com/avatar.png",
+				Author:    "John Doe",
+				Email:     "john.doe@mail.com",
+				Timestamp: now.UTC().Unix(),
+				Ref:       "refs/head/branch",
+				ForgeURL:  "https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef",
+				Event:     model.EventPush,
+			},
+		},
+	}
+	for _, tt := range tests {
+		to := convertRepositoryPushEvent(tt.from, "https://base.url")
+		assert.Equal(t, tt.to, to)
+	}
+}
 
-		g.It("should close pull request", func() {
-			now := time.Now()
-			from := &bb.PullRequestEvent{
-				Event: bb.Event{
-					Date:     bb.ISOTime(now),
-					EventKey: bb.EventKeyPullRequestMerged,
-					Actor: bb.User{
-						Name:  "John Doe",
-						Email: "john.doe@mail.com",
-						Slug:  "john.doe_mail.com",
-					},
-				},
-				PullRequest: bb.PullRequest{
-					ID:    123,
-					Title: "my title",
-					Source: bb.PullRequestRef{
-						ID:        "refs/head/branch",
-						DisplayID: "branch",
-						Latest:    "1234567890abcdef",
-						Repository: bb.Repository{
-							Slug: "REPO",
-							Project: &bb.Project{
-								Key: "PRJ",
-							},
-						},
-					},
-					Target: bb.PullRequestRef{
-						ID:        "refs/head/main",
-						DisplayID: "main",
-						Latest:    "abcdef1234567890",
-						Repository: bb.Repository{
-							Slug: "REPO",
-							Project: &bb.Project{
-								Key: "PRJ",
-							},
-						},
-					},
-				},
-			}
-			to := convertPullRequestEvent(from, "https://base.url")
-			g.Assert(to.Commit).Equal("1234567890abcdef")
-			g.Assert(to.Branch).Equal("branch")
-			g.Assert(to.Avatar).Equal("https://base.url/users/john.doe_mail.com/avatar.png")
-			g.Assert(to.Author).Equal("John Doe")
-			g.Assert(to.Email).Equal("john.doe@mail.com")
-			g.Assert(to.Timestamp).Equal(now.UTC().Unix())
-			g.Assert(to.Ref).Equal("refs/pull-requests/123/from")
-			g.Assert(to.ForgeURL).Equal("https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef")
-			g.Assert(to.Event).Equal(model.EventPullClosed)
-			g.Assert(to.Refspec).Equal("branch:main")
-		})
-
-		g.It("should truncate author", func() {
-			tests := []struct {
-				from string
-				to   string
-			}{
-				{
-					from: "Some Short Author",
-					to:   "Some Short Author",
-				},
-				{
-					from: "Some Very Long Author That May Include Multiple Names Here",
-					to:   "Some Very Long Author That May Includ...",
-				},
-			}
-			for _, tt := range tests {
-				g.Assert(authorLabel(tt.from)).Equal(tt.to)
-			}
-		})
-
-		g.It("should convert user", func() {
-			from := &bb.User{
-				Slug:  "slug",
+func Test_convertPullRequestEvent(t *testing.T) {
+	now := time.Now()
+	from := &bb.PullRequestEvent{
+		Event: bb.Event{
+			Date:     bb.ISOTime(now),
+			EventKey: bb.EventKeyPullRequestFrom,
+			Actor: bb.User{
+				Name:  "John Doe",
 				Email: "john.doe@mail.com",
-			}
-			to := convertUser(from, "https://base.url")
-			g.Assert(to.Login).Equal("slug")
-			g.Assert(to.Avatar).Equal("https://base.url/users/slug/avatar.png")
-			g.Assert(to.Email).Equal("john.doe@mail.com")
-		})
-	})
+				Slug:  "john.doe_mail.com",
+			},
+		},
+		PullRequest: bb.PullRequest{
+			ID:    123,
+			Title: "my title",
+			Source: bb.PullRequestRef{
+				ID:        "refs/head/branch",
+				DisplayID: "branch",
+				Latest:    "1234567890abcdef",
+				Repository: bb.Repository{
+					Slug: "REPO",
+					Project: &bb.Project{
+						Key: "PRJ",
+					},
+				},
+			},
+			Target: bb.PullRequestRef{
+				ID:        "refs/head/main",
+				DisplayID: "main",
+				Latest:    "abcdef1234567890",
+				Repository: bb.Repository{
+					Slug: "REPO",
+					Project: &bb.Project{
+						Key: "PRJ",
+					},
+				},
+			},
+		},
+	}
+	to := convertPullRequestEvent(from, "https://base.url")
+	assert.Equal(t, &model.Pipeline{
+		Commit:    "1234567890abcdef",
+		Branch:    "branch",
+		Avatar:    "https://base.url/users/john.doe_mail.com/avatar.png",
+		Author:    "John Doe",
+		Email:     "john.doe@mail.com",
+		Timestamp: now.UTC().Unix(),
+		Ref:       "refs/pull-requests/123/from",
+		ForgeURL:  "https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef",
+		Event:     model.EventPull,
+		Refspec:   "branch:main",
+		Title:     "my title",
+	}, to)
+}
+
+func Test_convertPullRequestCloseEvent(t *testing.T) {
+	now := time.Now()
+	from := &bb.PullRequestEvent{
+		Event: bb.Event{
+			Date:     bb.ISOTime(now),
+			EventKey: bb.EventKeyPullRequestMerged,
+			Actor: bb.User{
+				Name:  "John Doe",
+				Email: "john.doe@mail.com",
+				Slug:  "john.doe_mail.com",
+			},
+		},
+		PullRequest: bb.PullRequest{
+			ID:    123,
+			Title: "my title",
+			Source: bb.PullRequestRef{
+				ID:        "refs/head/branch",
+				DisplayID: "branch",
+				Latest:    "1234567890abcdef",
+				Repository: bb.Repository{
+					Slug: "REPO",
+					Project: &bb.Project{
+						Key: "PRJ",
+					},
+				},
+			},
+			Target: bb.PullRequestRef{
+				ID:        "refs/head/main",
+				DisplayID: "main",
+				Latest:    "abcdef1234567890",
+				Repository: bb.Repository{
+					Slug: "REPO",
+					Project: &bb.Project{
+						Key: "PRJ",
+					},
+				},
+			},
+		},
+	}
+	to := convertPullRequestEvent(from, "https://base.url")
+	assert.Equal(t, &model.Pipeline{
+		Commit:    "1234567890abcdef",
+		Branch:    "branch",
+		Avatar:    "https://base.url/users/john.doe_mail.com/avatar.png",
+		Author:    "John Doe",
+		Email:     "john.doe@mail.com",
+		Timestamp: now.UTC().Unix(),
+		Ref:       "refs/pull-requests/123/from",
+		ForgeURL:  "https://base.url/projects/PRJ/repos/REPO/commits/1234567890abcdef",
+		Event:     model.EventPullClosed,
+		Refspec:   "branch:main",
+		Title:     "my title",
+	}, to)
+}
+
+func Test_authorLabel(t *testing.T) {
+	tests := []struct {
+		from string
+		to   string
+	}{
+		{
+			from: "Some Short Author",
+			to:   "Some Short Author",
+		},
+		{
+			from: "Some Very Long Author That May Include Multiple Names Here",
+			to:   "Some Very Long Author That May Includ...",
+		},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, tt.to, authorLabel(tt.from))
+	}
+}
+
+func Test_convertUser(t *testing.T) {
+	from := &bb.User{
+		Slug:  "slug",
+		Email: "john.doe@mail.com",
+		ID:    1,
+	}
+	to := convertUser(from, "https://base.url")
+	assert.Equal(t, &model.User{
+		Login:         "slug",
+		Avatar:        "https://base.url/users/slug/avatar.png",
+		Email:         "john.doe@mail.com",
+		ForgeRemoteID: "1",
+	}, to)
 }
