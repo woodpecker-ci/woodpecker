c6e406e1e0f2d4ee9b8f934504cf490901947434
pat-s <patrick.schratz@gmail.com>
2024-12-28 00:09:12 +0100

    underline instead of chevron

--- web/src/components/layout/scaffold/Tabs.vue
+++ web/src/components/layout/scaffold/Tabs.vue
@@ -1,22 +1,15 @@
 <template>
-  <div class="flex flex-wrap mt-2 md:gap-4">
+  <div class="flex flex-wrap md:gap-4 mt-2">
     <router-link
       v-for="tab in tabs"
       :key="tab.title"
-      v-slot="{ isActive, isExactActive }"
       :to="tab.to"
-      class="border-transparent w-full py-1 md:w-auto flex cursor-pointer md:border-b-2 text-wp-text-100 items-center"
-      :active-class="tab.matchChildren ? '!border-wp-text-100' : ''"
-      :exact-active-class="tab.matchChildren ? '' : '!border-wp-text-100'"
+      class="flex items-center !border-wp-text-100 py-1 border-transparent md:border-b-2 w-full md:w-auto text-wp-text-100 cursor-pointer"
+      active-class="underline !border-wp-text-100"
+      exact-active-class="underline !border-wp-text-100"
     >
-      <Icon
-        v-if="isExactActive || (isActive && tab.matchChildren)"
-        name="chevron-right"
-        class="md:hidden flex-shrink-0"
-      />
-      <Icon v-else name="blank" class="md:hidden" />
       <span
-        class="flex gap-2 items-center md:justify-center flex-row py-1 w-full min-w-20 dark:hover:bg-wp-background-100 hover:bg-wp-background-200 rounded-md sm:pl-2.5 md:pl-2 sm:pr-2"
+        class="flex flex-row md:justify-center items-center gap-2 dark:hover:bg-wp-background-100 hover:bg-wp-background-200 py-1 sm:pr-2 sm:pl-2.5 md:pl-2 rounded-md w-full min-w-20"
       >
         <Icon v-if="tab.icon" :name="tab.icon" :class="tab.iconClass" class="flex-shrink-0" />
         <span>{{ tab.title }}</span>
