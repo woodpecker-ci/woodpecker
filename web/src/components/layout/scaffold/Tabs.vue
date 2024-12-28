51df10b8072ab3e98ba98d47c73ff148c899117c
pat-s <patrick.schratz@gmail.com>
2024-12-29 00:25:52 +0100

    always center

--- web/src/components/layout/scaffold/Tabs.vue
+++ web/src/components/layout/scaffold/Tabs.vue
@@ -5,11 +5,11 @@
       :key="tab.title"
       :to="tab.to"
       class="flex items-center px-2 py-1 border-transparent md:border-b-2 w-auto w-full md:w-auto text-wp-text-100 cursor-pointer"
-      :active-class="tab.matchChildren ? '!border-wp-text-100 dark:wp-background-100 bg-wp-background-300' : ''"
-      :exact-active-class="tab.matchChildren ? '' : '!border-wp-text-100 dark:wp-background-100 bg-wp-background-300'"
+      :active-class="tab.matchChildren ? '!border-wp-text-100 dark:wp-background-100 bg-wp-background-300 px-2 w-auto' : ''"
+      :exact-active-class="tab.matchChildren ? '' : '!border-wp-text-100 dark:wp-background-100 bg-wp-background-300 px-2 w-auto'"
     >
       <span
-        class="flex flex-row md:justify-center items-center gap-2 dark:hover:bg-wp-background-100 hover:bg-wp-background-200 py-1 rounded-md w-full min-w-20"
+        class="flex flex-row justify-center items-center gap-2 dark:hover:bg-wp-background-100 hover:bg-wp-background-200 py-1 rounded-md w-full min-w-20"
       >
         <Icon v-if="tab.icon" :name="tab.icon" :class="tab.iconClass" class="flex-shrink-0" />
         <span>{{ tab.title }}</span>
