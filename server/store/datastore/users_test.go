147db46f5f965e614087b5cf71196045a3ef0432
Patrick Schratz <patrick.schratz@gmail.com>
2025-01-10 18:12:21 +0100

    Don't fail user creation if org exists (#4687)
    (cherry picked from commit 628c0e8faa1c1d4299e053b666500c2dbb383e37)

--- server/store/datastore/users_test.go
+++ server/store/datastore/users_test.go
@@ -253,3 +253,42 @@ func TestUsers(t *testing.T) {
 		})
 	})
 }
+
+func TestCreateUserWithExistingOrg(t *testing.T) {
+	store, closer := newTestStore(t, new(model.User), new(model.Org), new(model.Perm))
+	defer closer()
+
+	existingOrg := &model.Org{
+		ForgeID: 1,
+		IsUser:  true,
+		Name:    "existingorg",
+		Private: false,
+	}
+
+	err := store.OrgCreate(existingOrg)
+	assert.NoError(t, err)
+	assert.EqualValues(t, "existingorg", existingOrg.Name)
+
+	// Create a new user with the same name as the existing organization
+	newUser := &model.User{
+		Login: "existingOrg",
+		Hash:  "A",
+	}
+	err = store.CreateUser(newUser)
+	assert.NoError(t, err)
+
+	updatedOrg, err := store.OrgGet(existingOrg.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "existingorg", updatedOrg.Name)
+
+	newUser2 := &model.User{
+		Login: "new-user",
+		Hash:  "B",
+	}
+	err = store.CreateUser(newUser2)
+	assert.NoError(t, err)
+
+	newOrg, err := store.OrgFindByName("new-user")
+	assert.NoError(t, err)
+	assert.Equal(t, "new-user", newOrg.Name)
+}
