f61d88aafd9f960e89856e3ba2c097eca9f966bc
Robert Kaussow <mail@thegeeklab.de>
2025-01-12 15:37:30 +0100

    Fix broken z-index (#4715)

--- web/src/components/layout/Popup.vue
+++ web/src/components/layout/Popup.vue
@@ -2,13 +2,13 @@
   <!-- overlay -->
   <div
     v-if="open"
-    class="z-500 fixed bottom-0 left-0 right-0 top-0 bg-gray-900 opacity-80 print:hidden"
+    class="fixed bottom-0 left-0 right-0 top-0 z-40 bg-gray-900 opacity-80 print:hidden"
     @click="$emit('close')"
   />
   <!-- overlay end -->
   <div
     v-if="open"
-    class="max-w-1/3 max-w-4/5 md:max-h-3/5 z-1000 fixed left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform print:hidden"
+    class="max-w-1/3 max-w-4/5 md:max-h-3/5 fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform print:hidden"
   >
     <div class="shadow-all m-auto flex h-auto flex-col">
       <slot />
