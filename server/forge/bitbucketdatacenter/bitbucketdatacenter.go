50239f0bb90d47febe4b34bac52ec1c96fbd12f9
tal <tal@localhost>
2025-01-13 16:09:35 +0200

    fix(bitbucketdatacenter.go): Remove pipeline number from status key

--- server/forge/bitbucketdatacenter/bitbucketdatacenter.go
+++ server/forge/bitbucketdatacenter/bitbucketdatacenter.go
@@ -313,7 +313,7 @@ func (c *client) Status(ctx context.Context, u *model.User, repo *model.Repo, pi
 	status := &bb.BuildStatus{
 		State:       convertStatus(pipeline.Status),
 		URL:         common.GetPipelineStatusURL(repo, pipeline, workflow),
-		Key:         fmt.Sprintf("%s-%d", common.GetPipelineStatusContext(repo, pipeline, workflow), pipeline.Number),
+		Key:         common.GetPipelineStatusContext(repo, pipeline, workflow),
 		Description: common.GetPipelineStatusDescription(pipeline.Status),
 		Ref:         pipeline.Ref,
 	}
