16f246929d36fc4b61f9abe4926ed92301b67b5f
Patrick Schratz <patrick.schratz@gmail.com>
2025-01-23 18:15:42 +0100

    Update server/store/datastore/org.go
    Co-authored-by: Anbraten <6918444+anbraten@users.noreply.github.com>

--- server/store/datastore/org.go
+++ server/store/datastore/org.go
@@ -81,7 +81,7 @@ func (s storage) OrgFindByName(name string) (*model.Org, error) {
 func (s storage) orgFindByName(sess *xorm.Session, name string) (*model.Org, error) {
 	// sanitize
 	org := new(model.Org)
-	return org, wrapGet(sess.Where("name = ?", name).Get(org))
+	return org, wrapGet(sess.Where("LOWER(name) = ?", strings.ToLower(name)).Get(org))
 }
 
 func (s storage) OrgRepoList(org *model.Org, p *model.ListOptions) ([]*model.Repo, error) {
