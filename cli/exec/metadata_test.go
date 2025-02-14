818d66d47afb3065375cabe53337ebd56dc7a7c2
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2025-02-14 11:09:07 +0200

    Replace deprecated linter (#4843)

--- cli/exec/metadata_test.go
+++ cli/exec/metadata_test.go
@@ -84,7 +84,7 @@ func TestMetadataFromContext(t *testing.T) {
 	})
 
 	t.Run("InvalidFile", func(t *testing.T) {
-		tempFile, err := os.CreateTemp("", "invalid.json")
+		tempFile, err := os.CreateTemp(t.TempDir(), "invalid.json")
 		require.NoError(t, err)
 		t.Cleanup(func() { os.Remove(tempFile.Name()) })
 
@@ -132,7 +132,7 @@ func TestMetadataFromContext(t *testing.T) {
 
 func createTempFile(t *testing.T, content any) string {
 	t.Helper()
-	tempFile, err := os.CreateTemp("", "metadata.json")
+	tempFile, err := os.CreateTemp(t.TempDir(), "metadata.json")
 	require.NoError(t, err)
 	t.Cleanup(func() { os.Remove(tempFile.Name()) })
 
