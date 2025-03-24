e7e123b1408e1d7087c5ac1ad0e2c58d8d876304
Robert Kaussow <mail@thegeeklab.de>
2025-03-24 21:00:02 +0100

    Fix queue badge list

--- web/src/components/atomic/Badge.vue
+++ web/src/components/atomic/Badge.vue
@@ -1,17 +1,14 @@
 <template>
-  <span class="inline-flex text-xs font-medium">
+  <span class="inline-flex items-center text-xs font-medium">
     <span
-      class="border-wp-state-neutral-100 bg-wp-state-neutral-100 flex items-center rounded-l-full border-2 py-0.5 pr-1 pl-2 text-gray-300"
+      class="border-wp-state-neutral-100 bg-wp-state-neutral-100 rounded-l-full border-1 py-0.5 pr-1 pl-2 whitespace-nowrap text-gray-300"
       :class="{
         'rounded-r-full pr-2': value === undefined,
       }"
     >
       {{ label }}
     </span>
-    <span
-      v-if="value !== undefined"
-      class="border-wp-state-neutral-100 flex items-center rounded-r-full border-2 py-0.5 pr-2 pl-1"
-    >
+    <span v-if="value !== undefined" class="border-wp-state-neutral-100 rounded-r-full border-1 py-0.5 pr-2 pl-1">
       {{ value }}
     </span>
   </span>
