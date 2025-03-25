ae36ffa5c0dffbeb6b8959de6da907c36c114024
Robert Kaussow <mail@thegeeklab.de>
2025-03-25 01:02:42 +0100

    cleanup

--- web/src/components/atomic/Badge.vue
+++ web/src/components/atomic/Badge.vue
@@ -3,14 +3,10 @@
     <span
       v-if="label !== undefined"
       class="border-wp-state-neutral-100 bg-wp-state-neutral-100 rounded-l-full border-1 py-0.5 pr-1 pl-2 whitespace-nowrap text-gray-300"
-      :class="{
-        'rounded-r-full pr-2': value === undefined,
-      }"
     >
       {{ label }}
     </span>
     <span
-      v-if="value !== undefined"
       class="border-wp-state-neutral-100 rounded-r-full border-1 py-0.5 pr-2 pl-1 whitespace-nowrap"
       :class="{
         'rounded-l-full pl-2': label === undefined,
@@ -24,6 +20,6 @@
 <script lang="ts" setup>
 defineProps<{
   label?: string;
-  value?: string | number;
+  value: string | number;
 }>();
 </script>
