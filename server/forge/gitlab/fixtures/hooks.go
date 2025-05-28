3664473eab94aeca2cf0293fc35a99a2d5104c7a
m.huber <m.huber@kithara.com>
2025-05-28 19:38:06 +0200

    gitlab: call it fixtures also

--- server/forge/gitlab/fixtures/hooks.go
+++ server/forge/gitlab/fixtures/hooks.go
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testdata
+package fixtures
 
 import (
 	"net/http"
 	"net/url"
+
+	_ "embed"
 )
 
 var (
--- server/forge/gitlab/fixtures/testdata.go
+++ server/forge/gitlab/fixtures/testdata.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testdata
+package fixtures
 
 import (
 	"net/http"
