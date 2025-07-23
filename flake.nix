590ad89578d63f61924bdbd510fbd24c639c792b
m.huber <m.huber@kithara.com>
2025-07-23 14:57:37 +0200

    define go version once

--- flake.nix
+++ flake.nix
@@ -12,37 +12,45 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # generic
-            gnumake
-            gnutar
-            zip
-            tree
+        devShells.default =
+          with pkgs;
+          let
+            go = go_1_24;
+          in
+          pkgs.mkShell {
+            buildInputs = [
+              # generic
+              gnumake
+              gnutar
+              gzip
+              zip
+              tree
 
-            # frontend
-            nodejs_24
-            pnpm
-            nodePackages.typescript
-            nodePackages.typescript-language-server
+              # frontend
+              nodejs_24
+              pnpm
+              nodePackages.typescript
+              nodePackages.typescript-language-server
 
-            # backend
-            go_1_24
-            glibc.static
-            gofumpt
-            golangci-lint
-            go-mockery
-            protobuf
-            sqlite
-            go-swag # for generate-openapi
-            addlicense
-            protoc-gen-go
-            protoc-gen-go-grpc
-            gcc
-          ];
-          CFLAGS = "-I${pkgs.glibc.dev}/include";
-          LDFLAGS = "-L${pkgs.glibc}/lib";
-        };
+              # backend
+              go
+              glibc.static
+              gofumpt
+              golangci-lint
+              go-mockery
+              protobuf
+              sqlite
+              go-swag # for generate-openapi
+              addlicense
+              protoc-gen-go
+              protoc-gen-go-grpc
+              gcc
+            ];
+            CFLAGS = "-I${pkgs.glibc.dev}/include";
+            LDFLAGS = "-L${pkgs.glibc}/lib";
+            GO = "${go}/bin/go";
+            GOROOT = "${go}/share/go";
+          };
       }
     );
 }
