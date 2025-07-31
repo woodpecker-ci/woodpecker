8f935b13d717181e16c76abeb949283e2e177e53
Anbraten <anton@ju60.de>
2025-07-31 18:00:30 +0200

    undo

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -104,6 +104,10 @@ onMounted(async () => {
 useWPTitle(computed(() => [i18n.t('login')]));
 
 function getHostFromUrl(forge: Forge) {
+  if (!forge.url && !forge.oauth_host) {
+    return forge.type.charAt(0).toUpperCase() + forge.type.slice(1);
+  }
+
   const url = new URL(forge.oauth_host ?? forge.url);
   return url.hostname;
 }
