54c205d2f0d5616f1dbd8190a3ef4eefe7c76d7d
Harri Avellan <harri@xavellan.tech>
2025-09-06 13:12:38 +0300

    feat: expose listing available organizations via woodpecker-go / CLI (#5481)
    Co-authored-by: qwerty287 <80460567+qwerty287@users.noreply.github.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ cli/admin/org/org_list.go
@@ -0,0 +1,71 @@
+package org
+
+import (
+	"context"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/urfave/cli/v3"
+
+	"go.woodpecker-ci.org/woodpecker/v3/cli/common"
+	"go.woodpecker-ci.org/woodpecker/v3/cli/internal"
+	"go.woodpecker-ci.org/woodpecker/v3/woodpecker-go/woodpecker"
+)
+
+var Command = &cli.Command{
+	Name:  "org",
+	Usage: "manage organizations",
+	Commands: []*cli.Command{
+		orgListCmd,
+	},
+}
+
+var orgListCmd = &cli.Command{
+	Name:      "ls",
+	Usage:     "list organizations",
+	ArgsUsage: "",
+	Action:    orgList,
+	Flags: []cli.Flag{
+		common.FormatFlag(tmplOrgList, true),
+	},
+}
+
+func orgList(ctx context.Context, c *cli.Command) error {
+	format := c.String("format") + "\n"
+
+	client, err := internal.NewClient(ctx, c)
+	if err != nil {
+		return err
+	}
+
+	opt := woodpecker.ListOptions{}
+
+	list, err := client.OrgList(opt)
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := template.New("_").Funcs(orgFuncMap).Parse(format)
+	if err != nil {
+		return err
+	}
+
+	for _, org := range list {
+		if err := tmpl.Execute(os.Stdout, org); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Template for org list items.
+var tmplOrgList = "\x1b[33m{{ .Name }} \x1b[0m" + `
+Organization ID: {{ .ID }}
+`
+
+var orgFuncMap = template.FuncMap{
+	"list": func(s []string) string {
+		return strings.Join(s, ", ")
+	},
+}
