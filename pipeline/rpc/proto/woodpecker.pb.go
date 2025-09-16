aae742e5c81ccbfe954c07bef5187e80084d42f8
Robert Kaussow <mail@thegeeklab.de>
2025-09-16 14:18:23 +0200

    gen

--- pipeline/rpc/proto/woodpecker.pb.go
+++ pipeline/rpc/proto/woodpecker.pb.go
@@ -22,11 +22,12 @@
 package proto
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
