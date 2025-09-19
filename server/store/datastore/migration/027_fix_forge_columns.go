3fdf128e8a58ae70751c26c53b12669eb0461b58
Robert Kaussow <mail@thegeeklab.de>
2025-09-19 20:15:45 +0200

    cleanup

--- server/store/datastore/migration/027_fix_forge_columns.go
+++ server/store/datastore/migration/027_fix_forge_columns.go
@@ -1,8 +1,20 @@
+// Copyright 2024 Woodpecker Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package migration
 
 import (
-	"fmt"
-
 	"src.techknowlogick.com/xormigrate"
 	"xorm.io/xorm"
 )
@@ -10,15 +22,14 @@ import (
 var fixForgeColumns = xormigrate.Migration{
 	ID: "fix-forge-columns",
 	MigrateSession: func(sess *xorm.Session) (err error) {
-		// Define old forge structure with old column names
 		type forge struct {
 			OAuthClientID     string `xorm:"VARCHAR(250) 'o_auth_client_i_d'"`
 			OAuthClientSecret string `xorm:"VARCHAR(250) 'o_auth_client_secret'"`
 		}
 
-		// ensure old columns exist before renaming
+		// Ensure columns to rename exist
 		if err := sess.Sync(new(forge)); err != nil {
-			return fmt.Errorf("sync old forge model failed: %w", err)
+			return err
 		}
 
 		// Rename old columns to new names
