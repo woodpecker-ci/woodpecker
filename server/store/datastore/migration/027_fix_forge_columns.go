fc9070e94b9cc19f1a8f291115bd87725d84882d
Robert Kaussow <mail@thegeeklab.de>
2025-09-19 20:10:33 +0200

    Fix forge oauth columns migration

--- server/store/datastore/migration/027_fix_forge_columns.go
+++ server/store/datastore/migration/027_fix_forge_columns.go
@@ -1,6 +1,8 @@
 package migration
 
 import (
+	"fmt"
+
 	"src.techknowlogick.com/xormigrate"
 	"xorm.io/xorm"
 )
@@ -8,6 +10,18 @@ import (
 var fixForgeColumns = xormigrate.Migration{
 	ID: "fix-forge-columns",
 	MigrateSession: func(sess *xorm.Session) (err error) {
+		// Define old forge structure with old column names
+		type forge struct {
+			OAuthClientID     string `xorm:"VARCHAR(250) 'o_auth_client_i_d'"`
+			OAuthClientSecret string `xorm:"VARCHAR(250) 'o_auth_client_secret'"`
+		}
+
+		// ensure old columns exist before renaming
+		if err := sess.Sync(new(forge)); err != nil {
+			return fmt.Errorf("sync old forge model failed: %w", err)
+		}
+
+		// Rename old columns to new names
 		if err := renameColumn(sess, "forges", "o_auth_client_i_d", "oauth_client_id"); err != nil {
 			return err
 		}
