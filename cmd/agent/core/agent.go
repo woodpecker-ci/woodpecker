f03490d68ef342fb18f7cccccf2089b615f030d1
qwerty287 <qwerty287@posteo.de>
2025-11-06 17:00:33 +0100

    Fix version string in log

--- cmd/agent/core/agent.go
+++ cmd/agent/core/agent.go
@@ -65,7 +65,7 @@ var (
 )
 
 func run(ctx context.Context, c *cli.Command, backends []types.Backend) error {
-	log.Info().Str("version", version.Version).Msg("Starting Woodpecker agent")
+	log.Info().Str("version", version.String()).Msg("Starting Woodpecker agent")
 
 	agentCtx, ctxCancel := context.WithCancelCause(ctx)
 	stopAgentFunc = func(err error) {
