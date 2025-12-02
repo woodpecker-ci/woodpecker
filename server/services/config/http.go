ce0145a59ad7a44a520a34b360cf3e7f48e0f866
LUKIEYF <103362370+LUKIEYF@users.noreply.github.com>
2025-12-02 10:28:17 +0800

    Update server/services/config/http.go
    Co-authored-by: qwerty287 <80460567+qwerty287@users.noreply.github.com>

--- server/services/config/http.go
+++ server/services/config/http.go
@@ -66,7 +66,7 @@ func (h *http) Fetch(ctx context.Context, forge forge.Forge, user *model.User, r
 	}
 
 	status, err := h.client.Send(ctx, net_http.MethodPost, h.endpoint, body, response)
-	if err != nil && status != 204 {
+ 	if err != nil && status != net_http.StatusNoContent {
 		return nil, fmt.Errorf("failed to fetch config via http from endpoint %s (status: %d): %w", h.endpoint, status, err)
 	}
 
