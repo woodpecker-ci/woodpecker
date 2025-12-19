46dd05cf79c341b6452aee8569d9adf4e5b53b06
qwerty287 <80460567+qwerty287@users.noreply.github.com>
2025-12-19 17:55:25 +0100

    Aktualisieren von RepoManualPipeline.vue

--- web/src/views/repo/RepoManualPipeline.vue
+++ web/src/views/repo/RepoManualPipeline.vue
@@ -102,7 +102,7 @@ async function triggerManualPipeline() {
       name: 'repo',
     });
 
-    notifications.notify({ type: 'warn', title: i18n.t('repo.manual_pipeline.no_workflows') });
+    notifications.notify({ type: 'warn', title: i18n.t('repo.manual_pipeline.no_manual_workflows') });
   } else {
     await router.push({
       name: 'repo-pipeline',
