d1b7e35ca857f183e76171f2ab72841fbed3daf9
6543 <6543@obermui.de>
2026-01-19 18:44:18 +0100

    Improve service syntax related docs and tests nits (#5991)

--- pipeline/frontend/yaml/linter/linter_test.go
+++ pipeline/frontend/yaml/linter/linter_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"go.woodpecker-ci.org/woodpecker/v3/pipeline/errors"
 	"go.woodpecker-ci.org/woodpecker/v3/pipeline/frontend/yaml"
@@ -185,7 +186,7 @@ func TestLintErrors(t *testing.T) {
 
 	for _, test := range testdata {
 		conf, err := yaml.ParseString(test.from)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		lerr := linter.New().Lint([]*linter.WorkflowConfig{{
 			File:      test.from,
