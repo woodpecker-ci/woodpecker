61fa4b660887834cbfc2c3c6f48e96419b0f2d90
qwerty287 <qwerty287@posteo.de>
2026-03-01 09:33:40 +0100

    correctly merge statuses

--- server/pipeline/status.go
+++ server/pipeline/status.go
@@ -29,7 +29,7 @@ var statusPriorityOrder = []model.StatusValue{
 
 	// skipped and killed cannot appear together with running/pending.
 	model.StatusKilled,
-	model.StatusCancelled, // TODO if has both cancelled: final status should be killed (if not both are cancelled)
+	model.StatusCancelled,
 
 	// running states
 	model.StatusRunning,
@@ -54,5 +54,16 @@ func buildPriorityMap() map[model.StatusValue]int {
 }
 
 func MergeStatusValues(s, t model.StatusValue) model.StatusValue {
+	// both are skipped due to cancellation -> cancelled
+	if s == model.StatusCancelled && t == model.StatusCancelled {
+		return model.StatusCancelled
+	}
+	// if only one was skipped -> use killed as the workflow/pipeline was running once already
+	if s == model.StatusCancelled {
+		s = model.StatusKilled
+	}
+	if t == model.StatusCancelled {
+		t = model.StatusKilled
+	}
 	return statusPriorityOrder[min(priorityMap[s], priorityMap[t])]
 }
--- server/pipeline/status_test.go
+++ server/pipeline/status_test.go
@@ -31,7 +31,7 @@ func TestStatusValueMerge(t *testing.T) {
 		{
 			s: model.StatusSuccess,
 			t: model.StatusSkipped,
-			e: model.StatusSkipped,
+			e: model.StatusSuccess,
 		},
 		{
 			s: model.StatusSuccess,
@@ -67,6 +67,21 @@ func TestStatusValueMerge(t *testing.T) {
 			s: model.StatusSkipped,
 			t: model.StatusSkipped,
 			e: model.StatusSkipped,
+		},
+		{
+			s: model.StatusSkipped,
+			t: model.StatusCancelled,
+			e: model.StatusCancelled,
+		},
+		{
+			s: model.StatusSuccess,
+			t: model.StatusCancelled,
+			e: model.StatusKilled,
+		},
+		{
+			s: model.StatusFailure,
+			t: model.StatusCancelled,
+			e: model.StatusKilled,
 		},
 	}
 	for _, tt := range tests {
