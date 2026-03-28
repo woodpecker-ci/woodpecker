a4fd480df889bf66dee86ae7db4b157afce323d9
6543 <6543@obermui.de>
2026-03-28 16:49:29 +0100

    Apply suggestion from @6543

--- pipeline/frontend/builder/utils.go
+++ pipeline/frontend/builder/utils.go
@@ -1,5 +1,4 @@
 // Copyright 2025 Woodpecker Authors
-// Copyright 2018 Drone.IO Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
