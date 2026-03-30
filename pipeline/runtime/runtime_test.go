3d7d0d04607b4cd17c622608d02ca68e5fc899c3
6543 <6543@obermui.de>
2026-03-30 15:22:59 +0200

    see if this is flaky?!?

--- pipeline/runtime/runtime_test.go
+++ pipeline/runtime/runtime_test.go
@@ -197,7 +197,7 @@ func TestWorkflowWithServiceStep(t *testing.T) {
 	if assert.Len(t, traces, 5) {
 		assert.EqualValues(t, backend_types.State{}, traces[0].CurrStepState)
 		assert.Greater(t, traces[2].CurrStepState.Started, int64(0))
-		assert.EqualValues(t, backend_types.State{Started: traces[2].CurrStepState.Started, Exited: false}, traces[2].CurrStepState)
+		assert.EqualValues(t, backend_types.State{Started: traces[2].CurrStepState.Started, Exited: true}, traces[2].CurrStepState)
 		assert.EqualValues(t, backend_types.State{}, traces[3].CurrStepState)
 		assert.Greater(t, traces[4].CurrStepState.Started, int64(0))
 		assert.EqualValues(t, backend_types.State{Started: traces[4].CurrStepState.Started, Exited: true}, traces[4].CurrStepState)
