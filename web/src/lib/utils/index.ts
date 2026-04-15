ed4a7dec72f063fa9af374e94b402ab061a3f4b8
6543 <6543@obermui.de>
2026-04-15 10:06:17 +0200

    unwrap obj to clone it (#6409)

--- web/src/lib/utils/index.ts
+++ web/src/lib/utils/index.ts
@@ -1,7 +1,13 @@
+import { toRaw } from 'vue';
+
 export function debounce<T extends unknown[]>(fn: (...args: T) => void, delay: number): (...args: T) => void {
   let timer: ReturnType<typeof setTimeout>;
   return (...args: T) => {
     clearTimeout(timer);
     timer = setTimeout(fn, delay, ...args);
   };
+}
+
+export function deepClone<T>(value: T): T {
+  return JSON.parse(JSON.stringify(toRaw(value))) as T;
 }
