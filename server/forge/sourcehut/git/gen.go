d1268ea969d19afe18950b0135eb438db284d942
6543 <6543@obermui.de>
2026-05-01 23:16:28 +0200

    sourcehut: codegen

--- server/forge/sourcehut/git/gen.go
+++ server/forge/sourcehut/git/gen.go
@@ -1,3 +1,7 @@
 package git
 
+import (
+	_ "git.sr.ht/~emersion/gqlclient"
+)
+
 //go:generate go run git.sr.ht/~emersion/gqlclient/cmd/gqlclientgen -s schema.graphqls -q queries.graphql -o gql.go -n git
