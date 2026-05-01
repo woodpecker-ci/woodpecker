79c3a3efeb11efc481244e08a7f81fe32fccc966
6543 <6543@obermui.de>
2026-05-01 23:12:23 +0200

    make codegen work in nix

--- server/forge/sourcehut/meta/gen.go
+++ server/forge/sourcehut/meta/gen.go
@@ -1,3 +1,7 @@
 package meta
 
+import (
+	_ "git.sr.ht/~emersion/gqlclient"
+)
+
 //go:generate go run git.sr.ht/~emersion/gqlclient/cmd/gqlclientgen -s schema.graphqls -q queries.graphql -o gql.go -n meta
