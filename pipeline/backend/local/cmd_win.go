14ed5dcbe3e9508c80c5b23b6c253ce069bac2c7
6543 <6543@obermui.de>
2026-06-10 12:17:52 +0200

    docu golan upstream issue too
    docu golan upstream issue too

--- pipeline/backend/local/cmd_win.go
+++ pipeline/backend/local/cmd_win.go
@@ -26,7 +26,7 @@ func newCmd(ctx context.Context, binary string, args ...string) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, binary, args...)
 
 	// Non perfect workaround till std exec supports JOB_OBJECT
-	// https://github.com/woodpecker-ci/woodpecker/issues/6717
+	// https://github.com/woodpecker-ci/woodpecker/issues/6717 & https://github.com/golang/go/issues/79927
 	cmd.Cancel = func() error {
 		return exec.Command("taskkill", "/F", "/T", "/PID",
 			strconv.Itoa(cmd.Process.Pid)).Run()
