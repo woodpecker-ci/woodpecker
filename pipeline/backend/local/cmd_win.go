e887e97a21fa363227df3e938f154d69fd0349ba
6543 <6543@obermui.de>
2026-06-10 04:26:52 +0200

    local backend: on windows use proccess tree-walk as workaround to kill
    not recommended as it could race and leave sub childs of dead parents dangling, but the best we have atm

--- pipeline/backend/local/cmd_win.go
+++ pipeline/backend/local/cmd_win.go
@@ -24,5 +24,12 @@ import (
 func newCmd(ctx context.Context, binary string, args ...string) *exec.Cmd {
 	cmd := exec.CommandContext(ctx, binary, args...)
 
+	// Non perfect workaround till std exec supports JOB_OBJECT
+	// https://github.com/woodpecker-ci/woodpecker/issues/6717
+	cmd.Cancel = func() error {
+		return exec.Command("taskkill", "/F", "/T", "/PID",
+			strconv.Itoa(cmd.Process.Pid)).Run()
+	}
+
 	return cmd
 }
