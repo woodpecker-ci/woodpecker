8d70dc1aeb4a7f857eced6a2e18f405bb96b3d0c
6543 <6543@obermui.de>
2026-08-17 23:57:34 +0200

    Apply suggestion from @6543

--- pipeline/frontend/yaml/linter/linter_test.go
+++ pipeline/frontend/yaml/linter/linter_test.go
@@ -221,10 +221,6 @@ func TestLintErrors(t *testing.T) {
 			from: "{steps: { build: { image: golang } }, services: [ { name: database, image: mysql }, { name: database, image: postgres } ] }",
 			want: "Service names must be unique, `database` is used more than once",
 		},
-		{
-			from: "steps:\n  build:\n    image: golang\nservices:\n  database:\n    image: mysql\n  database:\n    image: postgres\n",
-			want: "Service names must be unique, `database` is used more than once",
-		},
 	}
 
 	for _, test := range testdata {
